9c1a9b1ef4264442f6df32696e58a04a3993dc39
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-04-02 22:45:49 +0000

    Fixed the actionclass to use the new cubic spline class

--- src/ActionClass.h
+++ src/ActionClass.h
@@ -16,43 +16,55 @@ u_{kj}(q;\tau)z^{2j}s^{2(k-j)} */
 
 class PairActionClass
 {
-	Array<CubicSpline,2> ukj; //(level,f(k,j) )
-	double calcUrrptau(double s,double q,double z,int level);
-	
-	
-	  
+
+private:
+  Array<double,1> tempukjArray;
+
+public:
+  /// This stores the coefficients of the expansion specified above.
+  /// The array index is over the levels.  You call it with the q
+  /// value and a temporary array to get all of the values in that
+  /// column. 
+  Array<MultiCubicSpline,1> ukj; //(level )
+  double calcUrrptau(double s,double q,double z,int level);
 
 
+
+  
+  
+  
+  
 };
 
 double PairActionClass::calcUrrptau(double s,double q,double z,int level)
 {
-	double sum=0;
-	double r=0.5*(q+z);
-	double rprime=0.5*(q-z);
-	sum=sum+(ukj(level,0))(r)+(ukj(level,0))(rprime);//this is the endpoint action
-	zsquared=z*z;
-	ssquared=s*s;
-	ssquaredinverse=1/ssquared;
-	Sto2k=2;
-		for (int k=1;k<=n;k++){
-			Zto2j=1;
-			currS=Sto2k;
-			for (int j=0;j<=k;j++){
-				
-				cof=(ukj(level,k*(k+1)/2+j))(q); //indexing into the 2darray
-				sum=sum+cof*zto2j*currS;
-				
-				
-				zto2j*=zsquared;
-				currS=currS*ssquaredinverse;				
-			}				
-			Sto2k=Sto2k*ssquared;
-		}
-	
-	
+  double sum=0;
+  double r=0.5*(q+z);
+  double rprime=0.5*(q-z);
+  sum=sum+(ukj(level,0))(r)+(ukj(level,0))(rprime);//this is the endpoint action
+  zsquared=z*z;
+  ssquared=s*s;
+  ssquaredinverse=1/ssquared;
+  Sto2k=2;
+  (ukj(level))(q,tempukjArray); 
+  for (int k=1;k<=n;k++){
+    Zto2j=1;
+    currS=Sto2k;
 
+    for (int j=0;j<=k;j++){
 
+      cof=tempukjArray(k*(k+1)/2+j); //indexing into the 2darray
+      sum=sum+cof*zto2j*currS;
+      
+      
+      zto2j*=zsquared;
+      currS=currS*ssquaredinverse;				
+    }				
+    Sto2k=Sto2k*ssquared;
+  }
+  
+  
+  
 }
 
 /*! This is the class that controls all of the actions and is in
