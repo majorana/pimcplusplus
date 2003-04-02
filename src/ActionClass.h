8fdd3a8e154d41953816bbc9dfafd347323e0136
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-04-02 03:26:15 +0000

    *** empty log message ***

--- src/ActionClass.h
+++ src/ActionClass.h
@@ -4,9 +4,53 @@
 #include "CubicSpline.h"
 
 
+class SavedPairActionClass
+{
+
+
+
+};
+
+/*! $[\frac{u_0(r;\tau)+u_0(r';\tau)}{2}+\sum_{k=1}^n \sum_{j=1}^k
+u_{kj}(q;\tau)z^{2j}s^{2(k-j)} */
+
 class PairActionClass
 {
-  
+	Array<CubicSpline,2> ukj; //(level,f(k,j) )
+	double calcUrrptau(double s,double q,double z,int level);
+	
+	
+	  
+
+
+};
+
+double PairActionClass::calcUrrptau(double s,double q,double z,int level)
+{
+	double sum=0;
+	double r=0.5*(q+z);
+	double rprime=0.5*(q-z);
+	sum=sum+(ukj(level,0))(r)+(ukj(level,0))(rprime);//this is the endpoint action
+	zsquared=z*z;
+	ssquared=s*s;
+	ssquaredinverse=1/ssquared;
+	Sto2k=2;
+		for (int k=1;k<=n;k++){
+			Zto2j=1;
+			currS=Sto2k;
+			for (int j=0;j<=k;j++){
+				
+				cof=(ukj(level,k*(k+1)/2+j))(q); //indexing into the 2darray
+				sum=sum+cof*zto2j*currS;
+				
+				
+				zto2j*=zsquared;
+				currS=currS*ssquaredinverse;				
+			}				
+			Sto2k=Sto2k*ssquared;
+		}
+	
+	
 
 
 }
--- src/PathClass.h
+++ src/PathClass.h
@@ -9,10 +9,10 @@
 class PathClass
 {
 private:
-  Array<dVec, 2> PositionsA, PositionsB;
-  Array<int, 2> TimeStampA, TimeStampB;
-  Array<int, 2> *WriteTimeStamp1, *WriteTimeStamp2;
-  Array<dVec, 2> *Write1, *Write2;
+
+	MirroredArrayClass<dVec> Positions;
+	MirroredArrayClass<int> TimeStamp;
+	
 public:
   /// Holds the MC step number each particle/timeslice was last updated.
 
@@ -20,64 +20,46 @@ public:
   /// Operator to access by value.
   inline dVec operator()(int Ptcl, int TimeSlice) const
   {
-    return PositionsA(Ptcl, TimeSlice);
+    return Positions(Ptcl, TimeSlice);
   }
 
   /// Write access function.
   inline void SetPos (int Ptcl, int TimeSlice, const dVec &NewPos)
   {
-    (*Write1)(Ptcl, TimeSlice) = NewPos;
-    (*Write2)(Ptcl, TimeSlice) = NewPos;
-    (*WriteTimeStamp1)(Ptcl, TimeSlice) = GetCurrentTimeStamp();
-    (*WriteTimeStamp2)(Ptcl, TimeSlice) = GetCurrentTimeStamp();
+	
+		Positions.Set(Ptcl,TimeSlice,NewPos)
+		TimeStamp.Set(Ptcl,TimeSlice,GetCurrentTimeStamp());
   }
 
-  inline TimeStamp (int Ptcl, int TimeSlice)
+  inline GetTimeStamp (int Ptcl, int TimeSlice)
   {
-    return TimeStampA(Ptcl, TimeSlice);
+    return TimeStamp(Ptcl, TimeSlice);
   }
 
   inline SetMode(ModeType Mode)
   {
-    if (ModeType == MOVEMODE) 
-      {
-	Write1 = &PositionsA;
-	Write2 = &PositionsA;
-      }
-    else if (ModeType == OBSERVABLEMODE)
-      {
-	Write1 = &PositionsA;
-	Write2 = &PositionsB;
-      }
-    else
-      {
-	abort ("Undefined mode type!");
-      }
+		Positions.SetMode(Mode);
+		TimeStamp.SetMode(Mode);
+
+
   }
   /// In case of acceptance, this is called to copy the new path over
   /// the backup copy.  StartSlice and EndSlice are inclusive.  This
   /// copies from A to B.
   void AcceptCopy (const Array<int,1> &Ptcls, int StartSlice, int EndSlice)
   {
-    for (int i=0; i<Ptcls.size(); i++)
-      for (int j=StartSlice; j<=EndSlice; j++)
-	{
-	  PositionsB(i,j) = PositionsA(i,j);
-	  TimeStampB(i,j) = TimeStampA(i,j);
-	}
+		Positions.AcceptCopy(Ptcls,StartSlice,EndSlice);
+		TimeStamp.AcceptCopy(Ptcls,StartSlice,EndSlice);
   }
   /// In case of rejection, this is called to copy the new path over
   /// the backup copy.  StartSlice and EndSlice are inclusive.  This
   /// copies from B to A.
   void RejectCopy (const Array<int,1> &Ptcls, int StartSlice, int EndSlice)
   {
-    for (int i=0; i<Ptcls.size(); i++)
-      for (int j=StartSlice; j<=EndSlice; j++)
-	{
-	  PositionsA(i,j) = PositionsB(i,j);
-	  TimeStampA(i,j) = TimeStampB(i,j);
+  	Positions.RejectCopy(Ptcls,StartSlice,EndSlice);
+		TimeStamp.RejectCopy(Ptcls,StartSlice,EndSlice);
+	
 	}
-  }
 }
 
 
