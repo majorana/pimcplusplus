4da4e9a00fc9dc4313c4e9bfef1bbb5c1d679515
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2003-06-30 21:45:08 +0000

    Slightly changed interface for OpenSection.  It now takes an optional section number parameter, specifying which of sections with the same name to open.

--- IO/InputOutput.h
+++ IO/InputOutput.h
@@ -6,6 +6,9 @@
 //#include "InputOutputASCII.h"
 
 #include <stack>
+
+/// In the file name format name.extn, returns the extension.
+/// Actually returns everything after the trailing.
 inline string Extension (string fileName)
 {
   string extn;
@@ -36,12 +39,23 @@ class InputSectionClass
 private:
   InputTreeClass *CurrentSection;
 public:
+
+  /// Opens the file reference by fileName and reads the contents into
+  /// the tree in CurrentSection.  Creates a new object based on the
+  /// extnesion of the filename.  For ".h5", it creates an
+  /// InputTreeHDF5Class.  For ".xml" it creaes an InputTreeXMLClass.
+  /// After creating the object, it calls the objects virtual OpenFile
+  /// function, reading the contents of the file into the tree.
   bool OpenFile (string fileName)
   {
     bool success;
     string extn = Extension (fileName);
     if (extn == "h5") 
       CurrentSection = new InputTreeHDF5Class;
+    else {
+      cerr << "Unrecognized extension " << extn << endl;
+      exit(1);
+    }
     //else if (extn == "xml")
     //CurrentSection = new InputTreeXMLClass;
     //else
@@ -53,36 +67,57 @@ public:
     return (success);
   }
 
+  /// Calls CurrentSections virtual PrintTree() function.  This is for
+  /// debugging purposes.  It spits out a hierarchy of the sections
+  /// and variable names.
+  void PrintTree()
+  {
+    CurrentSection->PrintTree();
+  }
+
+  /// Calls CurrentSections close file and then deletes the
+  /// CurrentSection.  
   void CloseFile ()
   {
     CurrentSection->CloseFile();
     delete (CurrentSection);
   }
 
-  inline bool OpenSection (string name)
+  /// Opens the num'th section with the given name.  The default
+  /// value for num is 0.
+  inline bool OpenSection (string name, int num=0)
   {
     InputTreeClass *newSection;
-    bool success = CurrentSection->FindSection(name, newSection);
+    CurrentSection->Rewind();
+    bool success;
+    for (int i=0; i<=num; i++)
+      success = CurrentSection->FindSection(name, newSection, false);
     if (success)
       CurrentSection=newSection;
     return success;
   }
   
+  /// Closes the current section.  That is, CurrentSection becomes
+  /// CurrentSection's parent.
   inline void CloseSection ()
   {
     assert (CurrentSection->Parent != NULL);
     CurrentSection = CurrentSection->Parent;
   }
 
+  /// Template function which reads a variable in the present section
+  /// into the passed-by-reference T variable.
   template<class T>
   bool ReadVar(string name, T &var)
   {
-    CurrentSection->ReadVar(name, var);
+    return (CurrentSection->ReadVar(name, var));
   }
 
-  inline int CountSections(string fileName)
+  /// Returns the number of subsections within the present section
+  /// which have the name name.
+  inline int CountSections(string name)
   {
-    CurrentSection->CountSections(fileName);
+    return (CurrentSection->CountSections(name));
   }
 };
 
