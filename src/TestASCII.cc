2d1d04d11fd28b97c4d18851b1be5dc16e85459a
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-07-07 21:12:20 +0000

    Added the Test ASCII file

---
+++ src/TestASCII.cc
@@ -0,0 +1,11 @@
+#include "Common/IO/InputOutput.h"
+#include "Common/IO/InputOutputASCII.h"
+#include <iostream>
+
+int  main()
+{
+  InputSectionASCIIClass IO;
+  IO.OpenFile("inputFile", NULL);
+  
+
+}
