5c3a657cb455d8ddf5deabc0848191414a426523
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-07-17 20:55:22 +0000

    The HDF reader and writer (we believe) is now currently working. Added a flush.

--- src/TestHDF5.cc
+++ src/TestHDF5.cc
@@ -6,25 +6,29 @@ void TestHDF5Output()
   IOSectionClass HDF5out;
 
   Array<double,1> v(5);
-  Array<double,2> m(3,3);
+  Array<double,3> m(3,3,1);
   v = 1.0, 2.0, 3.0, 4.0, 5.0;
   m = 
     1.0, 2.0, 3.0,
-    4.0, 5.0, 5.0,
+    4.0, 5.0, 6.0,
     7.0, 8.0, 9.0;
-  Array<double,1> mplus;
+  Array<double,2> mplus(3,1);
   mplus = 10.0, 11.0, 12.0;
   Array<string,3> types(2,4,1);
   types = "Electron", "Proton", "Kenon", "Bryon", "Nonon", "Brainon",
     "Stepon", "Offon";
-
+  Array<string,2> typeAdd(4,1);
+  typeAdd="on", "off", "in", "out";
   HDF5out.NewFile ("HDF5test2.h5");
   HDF5out.WriteVar ("v", v);
   for (int i=0; i<1; i++)
     HDF5out.AppendVar ("v", 6.0+i);
   HDF5out.WriteVar ("m", m);
   HDF5out.AppendVar ("m", mplus);
+  HDF5out.ReadVar("m", m);
+  cerr << "m = " << m << endl;
   HDF5out.WriteVar ("types", types);
+  HDF5out.AppendVar("types",typeAdd);
   HDF5out.NewSection ("Action");
   HDF5out.WriteVar ("tau", 0.1);
   HDF5out.WriteVar ("tau2", 0.1);
@@ -33,6 +37,7 @@ void TestHDF5Output()
   HDF5out.NewSection ("PairAction");
   HDF5out.WriteVar ("Types", types);
   HDF5out.WriteVar ("mass", 1.0);
+  HDF5out.FlushFile();
   HDF5out.CloseSection();
   HDF5out.CloseSection();
   HDF5out.NewSection("Action");
