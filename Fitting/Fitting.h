6cbc23b97416359f61d2c4db00016212f896ea39
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2003-08-06 23:11:41 +0000

    Moved definitions into Fitting.cc.

---
+++ Fitting/Fitting.h
@@ -0,0 +1,15 @@
+#ifndef FITTING_H
+#define FITTING_H
+
+#include "../Blitz.h"
+#include "../MatrixOps/MatrixOps.h"
+
+/// LitFit performs a least-squares fit to data given in y with the
+/// errors given by sigma.  It performs a fit to a function of the
+/// form \f[ y_{\text{fit}}(x) \approx \sum_{j=0}^M a_j F_j(x) \f]. 
+/// \f$ F_{ij} = F_j(x_i) \f$.  
+void LinFit (Array<double,1> &y, Array<double,1> &sigma,    // inputs
+	     Array<double,2> &F,                     // input
+	     Array<double,1> &a, Array<double,1> &errors); // outputs
+
+#endif
