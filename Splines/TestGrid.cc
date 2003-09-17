476978fc79995db5fe79836a27fc6054bccf1aac
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2003-09-17 16:53:38 +0000

    Adding a file for testing grids.

---
+++ Splines/TestGrid.cc
@@ -0,0 +1,56 @@
+#include "Grid.h"
+
+void TestOptimal()
+{
+  OptimalGrid rgrid(600, 15.0, 0.0039);
+  double delta = rgrid(1) - rgrid(0);
+
+  for (int i=0; i<rgrid.NumPoints; i++) {
+    double r = rgrid(i) + 0.1*delta;
+    int j = rgrid.ReverseMap(r);
+    if (i != j)
+      cerr << "Error in Optimal ReverseMap!\n"
+	   << " i = " << i << "j = " << j << endl;
+  }
+}
+
+
+void TestOptimal2()
+{
+  OptimalGrid2 rgrid(1.0e-6, 15, 10.0, 300);
+  double delta = rgrid(1) - rgrid(0);
+
+  for (int i=0; i<rgrid.NumPoints; i++) {
+    double r = rgrid(i) + 0.1*delta;
+    int j = rgrid.ReverseMap(r);
+    if (i != j)
+      cerr << "Error in Optimal2 ReverseMap!\n"
+	   << " i = " << i << "j = " << j << endl;
+  }
+}
+
+
+void TestCluster()
+{
+  ClusterGrid rgrid(1.0e-6, 15, 1.0/10.0, 300);
+  double delta = rgrid(1) - rgrid(0);
+
+  for (int i=0; i<rgrid.NumPoints; i++) {
+    double r = rgrid(i) + 0.1*delta;
+    int j = rgrid.ReverseMap(r);
+    if (i != j)
+      cerr << "Error in Cluster ReverseMap!\n"
+	   << " i = " << i << "j = " << j << endl;
+  }
+  for (int i=0; i<rgrid.NumPoints; i++)
+    fprintf (stdout, "%1.16e\n", rgrid(i));
+}
+
+
+
+main()
+{
+  TestOptimal();
+  TestOptimal2();
+  TestCluster();
+}
