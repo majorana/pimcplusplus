d93332348f70db27b463fef9ed81e31de4cee205
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2003-09-26 20:14:37 +0000

    Added a missing CloseSection().

--- PairAction/PAFit.h
+++ PairAction/PAFit.h
@@ -4,5 +4,24 @@
 #include "PAszFit.h"
 #include "PAcoulombFit.h"
 
+inline PairActionFitClass *ReadFit (IOSectionClass &in)
+{
+  assert (in.OpenSection("Fits"));
+  string type;
+  assert (in.ReadVar("Type", type));
+  in.CloseSection (); // "Fits"
+  PairActionFitClass *fit;
+  if (type == "szfit")
+    fit = new PAszFitClass;
+  else if (type == "coulombfit")
+    fit = new PAcoulombFitClass;
+  else {
+    cerr << "Unrecognize pair action fit type \"" 
+	 << type << "\".  Exitting.\n";
+    exit(1);
+  }
+  return (fit);
+}
+
 
 #endif
