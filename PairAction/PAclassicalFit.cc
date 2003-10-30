0f3d084fb1737c3ef4f4fbf8045a251824ffada8
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2003-10-30 21:51:01 +0000

    Added this for interaction between classical particles.

---
+++ PairAction/PAclassicalFit.cc
@@ -0,0 +1,82 @@
+#include "PAFit.h"
+#include "../Splines/BicubicSpline.h"
+
+/// The following routines are used only if we are creating fits, not
+/// using them.
+#ifdef MAKE_FIT
+void PAclassicalFitClass::ReadParams(IOSectionClass &inSection)
+{
+  UsePBC = inSection.ReadVar ("Box", Box);
+}
+
+void PAclassicalFitClass::WriteBetaIndependentInfo (IOSectionClass &outSection)
+{ }
+
+
+void PAclassicalFitClass::AddFit (Rho &rho)
+{
+}
+
+
+void PAclassicalFitClass::Error(Rho &rho, double &Uerror, double &dUerror)
+{
+  Uerror = 0.0;
+  dUerror = 0.0;
+}
+
+
+void PAclassicalFitClass::WriteFits (IOSectionClass &outSection)
+{
+}
+#endif
+
+
+double PAclassicalFitClass::U(double q, double z, double s2, int level)
+{
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+
+  double r = q+0.5*z;
+  double rp = q-0.5*z;
+  return (0.5*beta*(Potential->V(r)+Potential->V(rp)));
+}
+
+double PAclassicalFitClass::dU(double q, double z, double s2, int level)
+{
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+
+  double r = q+0.5*z;
+  double rp = q-0.5*z;
+  return (0.5*(Potential->V(r)+Potential->V(rp)));
+}
+
+
+
+bool PAclassicalFitClass::Read (IOSectionClass &in,
+				double smallestBeta, int NumBetas)
+{
+  SmallestBeta = smallestBeta;
+  // Read Particles;
+  assert(in.OpenSection("Fits"));
+  assert(in.OpenSection("Particle1"));
+  Particle1.Read(in);
+  in.CloseSection();
+  assert(in.OpenSection("Particle2"));
+  Particle2.Read(in);
+  in.CloseSection();
+  lambda = Particle1.lambda + Particle2.lambda;
+  assert (lambda == 0.0);
+
+  // Read Potential;
+  assert(in.OpenSection("Potential"));
+  Potential = ReadPH(in);
+  in.CloseSection();
+  in.CloseSection();
+  return true;
+}
+
+
+
