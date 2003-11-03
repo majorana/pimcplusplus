dd1d04dfe660f59f24b05f7cfe2898ea393e52e0
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-11-03 19:06:17 +0000

    Wrote code to visualize paths.

--- src/analysis/analyze.py
+++ src/analysis/analyze.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
-from tables import *
+import tables
+from visual import *
+#import Numeric
+h5file = tables.openFile("../Observables2.h5");
+data = h5file.root.Paths_2.Path.read()
+names=h5file.root.Paths_2.SpeciesNames.read()
 
-h5file = openFile("../Observables2.h5");
-data = h5file.root.Energies_0.TotalEnergy.read()
+path1=curve(color=color.yellow,radius=0.5)
+path2=curve(color=color.blue,radius=0.5)
+for pathNum in range(0,len(data)):
+    rate(1)
+    ball1 = sphere (pos=data[pathNum,2,0], radius=0.1, color=color.red)
+    ball2 = sphere (pos=data[pathNum,3,0], radius=0.1, color=color.red)
+    path1.visible=0
+    path2.visible=0
+    path1=curve(color=color.yellow)
+    path2=curve(color=color.blue)
+    for slice in range(0,len(data[pathNum][0])):
+        path1.append(pos=data[pathNum][0][slice],radius=0.1)
+        path2.append(pos=data[pathNum][1][slice],radius=0.1)
+    scene.autoscale=0
+            
+                
 
-print data
+
