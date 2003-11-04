fa4b5fd72143d85ab75be59b64ee92eb04a540e4
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2003-11-04 00:31:21 +0000

    Changes some stuff.

--- src/analysis/analyze.py
+++ src/analysis/analyze.py
@@ -2,23 +2,28 @@
 import tables
 from visual import *
 #import Numeric
-h5file = tables.openFile("../Observables2.h5");
+h5file = tables.openFile("../H2_bservables.h5");
 data = h5file.root.Paths_2.Path.read()
 names=h5file.root.Paths_2.SpeciesNames.read()
 
-path1=curve(color=color.yellow,radius=0.5)
-path2=curve(color=color.blue,radius=0.5)
+E = h5file.root.Energies_0.TotalEnergy.read()
+Emean = sum(E)/len(E)
+print Emean
+
+
+path1=curve(color=color.yellow,radius=5)
+path2=curve(color=color.blue,radius=5)
 for pathNum in range(0,len(data)):
     rate(1)
-    ball1 = sphere (pos=data[pathNum,2,0], radius=0.1, color=color.red)
-    ball2 = sphere (pos=data[pathNum,3,0], radius=0.1, color=color.red)
+    ball1 = sphere (pos=data[pathNum,2,0], radius=0.2, color=color.red)
+    ball2 = sphere (pos=data[pathNum,3,0], radius=0.2, color=color.red)
     path1.visible=0
     path2.visible=0
-    path1=curve(color=color.yellow)
-    path2=curve(color=color.blue)
+    path1=curve(color=color.yellow,radius=0.0)
+    path2=curve(color=color.blue,radius=0.0)
     for slice in range(0,len(data[pathNum][0])):
-        path1.append(pos=data[pathNum][0][slice],radius=0.1)
-        path2.append(pos=data[pathNum][1][slice],radius=0.1)
+        path1.append(pos=data[pathNum][0][slice])
+        path2.append(pos=data[pathNum][1][slice])
     scene.autoscale=0
             
                 
