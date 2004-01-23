3b2ea59b98aa888c104ecdcaf45b4524cc00bfe6
bclark <bclark@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-01-23 00:06:00 +0000

    Adding to repository.

---
+++ PairAction/PAzeroFit.h
@@ -0,0 +1,28 @@
+#ifndef PA_ZERO_FIT_H
+#define PA_ZERO_FIT_H
+#include "PAFitBase.h"
+
+
+class PAzeroFitClass : public PairActionFitClass
+{
+public:
+  #ifdef MAKE_FIT
+    void ReadParams  (IOSectionClass &inSection) {};
+    void WriteBetaIndependentInfo (IOSectionClass &outSection) {};
+    /// Returns weighter RMS error
+    void Error (Rho &rho, double &Uerror, double &dUerror) {};
+    void AddFit (Rho &rho) {};
+    void WriteFits(IOSectionClass &outSection) {};
+    void ReadInput (IOSectionClass &inSection) {};
+  #endif
+  bool Read  (IOSectionClass &inSection, double lowestBeta,
+	      int NumBetas);
+  double U(double q, double z, double s2, int level);
+  double dU(double q, double z, double s2, int level);
+  PAzeroFitClass()
+  { 
+
+  }
+};
+
+#endif
