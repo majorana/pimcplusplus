b99d81dc751777a4e6f5af697d4da092ee866a4a
bclark <bclark@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-02-13 00:46:00 +0000

    Fixed permutations a little although they still appear to be broken.  Began writing block moves of permutations and bisections

--- Random/TestRandom.cc
+++ Random/TestRandom.cc
@@ -2,7 +2,9 @@
 
 int main(int argc, char** argv)
 {
+#ifdef USE_MPI
   MPI_Init (&argc, &argv);
+#endif
   CommunicatorClass comm;
 
   comm.SetWorld();
@@ -10,7 +12,9 @@ int main(int argc, char** argv)
   rand.Init();
   cerr << "Proc " << comm.MyProc() << "  local random num = " << rand.Local() << endl;
   cerr << "Proc " << comm.MyProc() << " common random num = " << rand.Common() << endl;
+#ifdef USE_MPI
   MPI_Finalize();
+#endif
 }
 
   
