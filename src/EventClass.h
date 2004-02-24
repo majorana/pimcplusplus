77bfc54f8112b692024b60d88de2a6c28ddf444e
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2004-02-24 17:35:21 +0000

    Adding to repository.

---
+++ src/EventClass.h
@@ -0,0 +1,15 @@
+#ifndef EVENT_CLASS_H
+#define EVENT_CLASS_H
+
+#include "Common/IO/InputOutput.h"
+
+
+class EventClass
+{
+ public:
+  virtual void DoEvent()=0;
+  virtual void Read(IOSectionClass& IO)=0;
+
+};
+
+#endif
---
+++ src/Main.cc
@@ -0,0 +1,20 @@
+#include "PIMCClass.h"
+
+
+main(int argc, char **argv)
+{
+#ifdef USE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+  if (argc < 2) {
+    cout << "Usage:\n";
+    cout << "pimc++ myfile.in\n"; 
+ }
+  else {
+    IOSectionClass in;
+    assert (in.OpenFile(argv[1]));
+    PIMCClass PIMC;
+    PIMC.Read(in);
+    PIMC.Run();
+  }
+}
