6b62febc76011549080eac99ecd59f6fb8c47922
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-03-11 21:14:59 +0000

    *** empty log message ***

--- PH/ScreenedPot.cc
+++ PH/ScreenedPot.cc
@@ -34,7 +34,7 @@ double ScreenedPot::d2Adr2 (double r)
 double ScreenedPot::V (double r)
 {
   double VHXC;
-  VHXC = (r < HXC.grid->End) ? HXC(r) : Charge/r;
+  VHXC = (r <= HXC.grid->End) ? HXC(r) : Charge/r;
   return (VHXC + BarePot->V(r));
 }
 
@@ -42,14 +42,14 @@ double ScreenedPot::V (double r)
 double ScreenedPot::dVdr (double r)
 {
   double dVHXC;
-  dVHXC = (r < HXC.grid->End) ? HXC.Deriv(r) : -Charge/(r*r);
+  dVHXC = (r <= HXC.grid->End) ? HXC.Deriv(r) : -Charge/(r*r);
   return (dVHXC + BarePot->dVdr(r));
 }
 
 double ScreenedPot::d2Vdr2(double r)
 {
   double d2VHXC;
-  d2VHXC = (r < HXC.grid->End) ? HXC.Deriv2(r) : 2.0*Charge/(r*r*r);
+  d2VHXC = (r <= HXC.grid->End) ? HXC.Deriv2(r) : 2.0*Charge/(r*r*r);
   return (d2VHXC + BarePot->d2Vdr2(r));
 }
 
