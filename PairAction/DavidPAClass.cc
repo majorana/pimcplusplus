4df75ddcdb8b41d8e0b193327f446e898585bb9a
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-04-07 15:24:51 +0000

    Adding to repository.

---
+++ PairAction/DavidPAClass.cc
@@ -0,0 +1,54 @@
+#include "DavidPAClass.h"
+
+#ifdef MAKE_FIT
+
+void DavidPAClass::ReadParams(IOSectionClass &in)
+{
+
+}
+
+void DavidPAClass::WriteBetaIndependentInfo (IOSectionClass &out)
+{
+
+}
+
+void DavidPAClass::Error (Rho &rho, double &Uerror, double &dUerror)
+{
+
+}
+
+void DavidPAClass::AddFit (Rho &rho)
+{
+
+}
+
+void DavidPAClass::WriteFits(IOSectionClass &out)
+{
+
+
+}
+#endif
+
+double DavidPAClass::U (double q, double z, double s2, int level)
+{
+  double s=sqrt(s2);
+  double uTemp;
+  double duTemp;
+  double vTemp;
+  calcUsqz(s,q,z,level,uTemp,duTemp,vTemp);
+  return uTemp;
+
+}
+
+double DavidPAClass::dU(double q, double z, double s2, int level)
+{
+  double s=sqrt(s2);
+  double uTemp;
+  double duTemp;
+  double vTemp;
+  calcUsqz(s,q,z,level,uTemp,duTemp,vTemp);
+  //  cerr<<"my vtemp is "<<vTemp<<endl;
+  return duTemp;
+
+
+}
