81bf115dbc45db8bc7ffddac4bf5f530322ee541
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2004-04-29 16:32:07 +0000

    Removed the distance table. Fixed things that were broken with how it was being put in the box and maybe have the Helium pair correlation function working
    Fixe dsome things with the DavidPAClass

--- src/analysis/free.py
+++ src/analysis/free.py
@@ -7,7 +7,7 @@ h5file = tables.openFile(sys.argv[1])
 #data = h5file.root.Paths_2.Path.read()
 #names=h5file.root.Paths_2.SpeciesNames.read()
 
-E = h5file.root.Energies_0.TotalEnergy.read()
+E = h5file.root.Energies_1.TotalEnergy.read()
 (mean,var,error,kappa)= stats.Stats(E)
 print 'Mean:  ', mean
 print 'Var:   ', var
