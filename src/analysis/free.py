afae7ca93b7ed658db0bde7b2440c1bfc54e1924
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2004-05-21 16:21:15 +0000

    Current version of the code with non-working energy for David's DM file Added code to analyze other forms of the energy

--- src/analysis/free.py
+++ src/analysis/free.py
@@ -8,11 +8,29 @@ h5file = tables.openFile(sys.argv[1])
 #names=h5file.root.Paths_2.SpeciesNames.read()
 
 E = h5file.root.Energies_0.TotalEnergy.read()
+PE= h5file.root.Energies_0.PotentialEnergy.read()
+SE= h5file.root.Energies_0.SpringEnergy.read()
+UE=h5file.root.Energies_0.DBetaEnergy.read()
 (mean,var,error,kappa)= stats.Stats(E)
+(meanP,varP,errorP,kappaP)=stats.Stats(PE)
+(meanS,varS,errorS,kappaS)=stats.Stats(SE)
+(meanU,varU,errorU,kappaU)=stats.Stats(UE)
 print 'Mean:  ', mean
 print 'Var:   ', var
 print 'Error: ', error
 print 'kappa  ', kappa
+print '========'
+print 'Mean P: ', meanP
+print 'Error P:', errorP
+print '========'
+print 'Mean S: ', meanS/8.0
+print 'Error S:', errorS/8.0
+print '========'
+print 'Mean U: ', (meanU-meanP)/8.0
+print 'Error U:', (errorU+errorP)/8.0
+print 'Mean U: ', (meanU)/8.0
+print 'Error U:', (errorU)/8.0
+
 ## quit
 ## Emean = sum(E)/len(E)
 ## print Emean
