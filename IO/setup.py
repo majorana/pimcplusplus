bab97e6b360d9ceb9dbcc935d92343dfb6aae6c5
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-07-21 20:27:51 +0000

    Adding necessar library references.

--- IO/setup.py
+++ IO/setup.py
@@ -6,7 +6,10 @@ module1 = Extension('IOSection',
                                'InputOutputXML.cc' ],\
                     include_dirs=['/home/common/lib/blitz-0.6-GCC/',\
                                   '/home/common/lib/hdf5/include',
-                                  '/usr/include/libxml2'])
+                                  '/usr/include/libxml2'],
+                    library_dirs=['/home/common/lib/blitz-0.6-GCC/lib',\
+                                  '/home/common/lib/hdf5/lib/'],
+                    libraries =  ['blitz', 'xml2', 'hdf5'])
 
 setup (name = 'PackageName',
        version = '1.0',
