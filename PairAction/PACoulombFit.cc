36f6b7f3c3193300554a2f83c0819899000238ca
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-08-09 21:19:21 +0000

    Adding to repository.

---
+++ PairAction/PACoulombFit.cc
@@ -0,0 +1,173 @@
+#include "PAFit.h"
+
+/// The following routines are used only if we are creating fits, not
+/// using them.
+#ifdef MAKE_FIT
+void PACoulombFitClass::ReadParams(IOSectionClass &inSection)
+{
+}
+
+void PACoulombFitClass::WriteBetaIndependentInfo (IOSectionClass &outSection)
+{ }
+
+
+void PACoulombFitClass::AddFit (Rho &rho)
+{
+}
+
+
+void PACoulombFitClass::Error(Rho &rho, double &Uerror, double &dUerror)
+{
+  Uerror = 0.0;
+  dUerror = 0.0;
+}
+
+
+void PACoulombFitClass::WriteFits (IOSectionClass &outSection)
+{
+}
+#endif
+
+
+double PACoulombFitClass::U(double q, double z, double s2, int level)
+{
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+  double r = q + 0.5*z;
+  double rp = q - 0.5*z;
+  double V = Z1Z2*(1.0/r + 1.0/rp);
+  return (beta*V);
+}
+
+double PACoulombFitClass::dU(double q, double z, double s2, int level)
+{
+  double r = q + 0.5*z;
+  double rp = q - 0.5*z;
+  double V = Z1Z2*(1.0/r + 1.0/rp);
+  return (V);
+}
+
+
+
+bool PACoulombFitClass::Read (IOSectionClass &in,
+				double smallestBeta, int numBetas)
+{
+  SmallestBeta = smallestBeta;
+  NumBetas=numBetas;
+  // Read Particles;
+  assert(in.OpenSection("Fits"));
+  assert(in.OpenSection("Particle1"));
+  Particle1.Read(in);
+  in.CloseSection();
+  assert(in.OpenSection("Particle2"));
+  Particle2.Read(in);
+  in.CloseSection();
+  lambda = Particle1.lambda + Particle2.lambda;
+  assert (lambda == 0.0);
+
+  // Read Potential;
+  in.OpenSection("Potential");
+  if (!in.ReadVar ("Z1Z2", Z1Z2))
+    Z1Z2 = 0.0;
+  in.CloseSection();
+  in.CloseSection();
+  return true;
+}
+
+
+
+/////////////////////////
+/// Long-ranged stuff ///
+/////////////////////////
+bool PACoulombFitClass::IsLongRange()
+{
+  return (Z1Z2 != 0.0);
+}
+
+
+/// The diagonal action only -- used for long-range breakup
+double PACoulombFitClass::Udiag(double q, int level)
+{  
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+  return beta*Z1Z2/q;
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::Udiag_p(double q, int level) 
+{  
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+  return -beta*Z1Z2/(q*q);
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::Udiag_pp(double q, int level) 
+{  
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+  return 2.0*beta*Z1Z2/(q*q*q);
+}
+
+/// The beta-derivative of the diagonal action
+double PACoulombFitClass::dUdiag    (double q, int level) 
+{
+  return Z1Z2/q;
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::dUdiag_p  (double q, int level) 
+{
+  return -Z1Z2/q*q;
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::dUdiag_pp (double q, int level) 
+{
+  return 2.0*Z1Z2/(q*q*q);
+}
+
+/// The potential to which this action corresponds.
+double PACoulombFitClass::V  (double r) 
+{
+  return Z1Z2/r;
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::Vp (double r)
+{
+  return -Z1Z2/(r*r);
+}
+
+/// The q-derivative of the above
+double PACoulombFitClass::Vpp(double r) 
+{
+  return 2.0*Z1Z2/(r*r*r);
+}
+
+void PACoulombFitClass::Setrc(double rc)
+{
+  rcut = rc;
+}
+
+double PACoulombFitClass::Xk_U(double k, int level)
+{
+  double beta = SmallestBeta;
+  for (int i=0; i<level; i++)
+    beta *= 2.0;
+  return -beta*4.0*M_PI/(k*k)*cos(k*rcut);
+}
+
+double PACoulombFitClass::Xk_dU(double k, int level)
+{
+  return -4.0*M_PI/(k*k)*cos(k*rcut);
+}
+
+double PACoulombFitClass::Xk_V(double k)
+{
+  return -4.0*M_PI/(k*k)*cos(k*rcut);
+}
---
+++ PairAction/PACoulombFit.h
@@ -0,0 +1,66 @@
+#ifndef PA_COULOMB_FIT_H
+#define PA_COULOMB_FIT_H
+#include "PAFitBase.h"
+
+class PACoulombFitClass : public PairActionFitClass
+{
+private:
+  double Vlong_k (double boxVol, double k, int level);
+  double Vlong (double q, int level);
+  // Real space cutoff parameter;
+  double alpha;
+  // The cutoff radius
+  double rcut;  
+public:
+#ifdef MAKE_FIT
+  void ReadParams  (IOSectionClass &inSection);
+  void WriteBetaIndependentInfo (IOSectionClass &outSection);
+  /// Returns weighter RMS error
+  void Error (Rho &rho, double &Uerror, double &dUerror);
+  void AddFit (Rho &rho);
+  void WriteFits(IOSectionClass &outSection);
+  void ReadInput (IOSectionClass &inSection);
+#endif
+  bool Read  (IOSectionClass &inSection, double lowestBeta,
+	      int NumBetas);
+  double U(double q, double z, double s2, int level);
+  double dU(double q, double z, double s2, int level);
+
+  /////////////////////////
+  /// Long-ranged stuff ///
+  /////////////////////////
+  /// The diagonal action only -- used for long-range breakup
+  double Udiag(double q, int level);
+  /// The q-derivative of the above
+  double Udiag_p(double q, int level);
+  /// The q-derivative of the above
+  double Udiag_pp(double q, int level);
+  /// The beta-derivative of the diagonal action
+  double dUdiag    (double q, int level);
+  /// The q-derivative of the above
+  double dUdiag_p  (double q, int level);
+  /// The q-derivative of the above
+  double dUdiag_pp (double q, int level);
+  /// The potential to which this action corresponds.
+  double V  (double r);
+  /// The q-derivative of the above
+  double Vp (double r);
+  /// The q-derivative of the above
+  double Vpp(double r);
+  /// Set the cutoff radius for the long-range breakup
+  void Setrc (double rc);
+  /// Modified fourier transform.
+  double Xk_U  (double k, int level);
+  double Xk_dU (double k, int level);
+  double Xk_V  (double k);
+
+  bool IsLongRange();
+  //  void DoBreakup(const dVec &box, const Array<dVec,1> &kVecs);
+
+  PACoulombFitClass()
+  { 
+
+  }
+};
+
+#endif
