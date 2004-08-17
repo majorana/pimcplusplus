75bcfa7146d992fc5aa7a9ea2407fb3351118400
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2004-08-17 19:29:02 +0000

    Visual Analysis exits more sanely and almost to the point where it will put things into box and whatnot.

--- src/analysis/VisualAnalysis.py
+++ src/analysis/VisualAnalysis.py
@@ -7,7 +7,7 @@ from matplotlib.matlab import *
 from HTMLgen import *
 import povexport
 from visual import *
-
+from visual.controls import *
 
 def GetPaths(infile):
      paths=infile.ReadVar("Path")
@@ -25,36 +25,44 @@ def InitVisualPaths(pathData):
 
 
 
-def ErasePaths(pathData,visualPath,visualBall,mcTime):
-     maxMCTime=len(pathData)
-     if mcTime>=maxMCTime:
-          mcTime=maxMCTime-1
-     numPaths=len(pathData[mcTime])
-     for pathNum in range(0,numPaths):
+def ErasePaths(visualPath,visualBall):
+     for pathNum in range(0,len(visualPath)):
           visualBall[pathNum].visible=0
           visualPath[pathNum].visible=0
 
 
+box=zeros(3)+0.0
+box[0]=24.0
+box[1]=24.0
+box[2]=24.0
 
-def PlotPaths(pathData,visualPath,visualBall,mcTime):
+def inBox(pathData,mcTime,pathNum,slice):
+    myArray=zeros(len(pathData[mcTime][pathNum][slice]))+0.0
+    for counter in range(0,len(pathData[mcTime][pathNum][slice])):
+        myArray[counter]=pathData[mcTime][pathNum][slice][counter] % box[counter]
+    return myArray
+
+def Id(pathData,mcTime,pathNum,slice):
+    return pathData[mcTime][pathNum][slice]
+
+def PlotPaths(pathData,visualPath,visualBall,mcTime,ballTime):
      maxMCTime=len(pathData)
      if mcTime>=maxMCTime:
           mcTime=maxMCTime-1
      numPaths=len(pathData[mcTime])
      for pathNum in range(0,numPaths):
           visualPath[pathNum]=(curve(color=color.blue,radius=2.2))
-          visualBall[pathNum].visible=0
-          visualBall[pathNum]=sphere(pos=(pathData[mcTime,pathNum,0]), radius=2.1, color=color.red)
-          visualBall[pathNum].visible=1
-#          print pathNum,numPaths
+          visualBall[pathNum]=sphere(pos=(Id(pathData,mcTime,pathNum,ballTime)), radius=2.1, color=color.red)
+#          visualBall[pathNum]=sphere(pos=(pathData[mcTime,pathNum,ballTime]), radius=2.1, color=color.red)
           numSlices=len(pathData[mcTime][pathNum])
           for slice in range(0,numSlices):
-              visualPath[pathNum].append(pos=(pathData[mcTime][pathNum][slice]))
-              visualPath[pathNum].visible=1
+              visualPath[pathNum].append(pos=(Id(pathData,mcTime,pathNum,slice)))
+#              visualPath[pathNum].append(pos=(pathData[mcTime][pathNum][slice]))
 
 
 
 
+scene.exit=1
 infile=IOSectionClass()
 infile.OpenFile(sys.argv[1])
 fileString=sys.argv[1]
@@ -66,29 +74,61 @@ os.chdir(dirName)
 infile.OpenSection("PathDump")
 pathData=GetPaths(infile)
 infile.CloseSection()
+
+##c = controls() # Create controls window
+
+# Create a button in the controls window:
+
+##b = button( pos=(0,0), width=60, height=60, text='Quit', action=lambda: quit() )
+
+def quit():
+    print "I quit!"
+    bedone=true
+    
 (visualPath,visualBall)=InitVisualPaths(pathData)
+
+
 maxmcTime=len(pathData)-1
 
-
-
+maxBallTime=len(pathData[0][0])-1
 mcTime=0
-PlotPaths(pathData,visualPath,visualBall,mcTime)
-while (1==1):
-    s=scene.kb.getkey()
-    if s=="left":
-        ErasePaths(pathData,visualPath,visualBall,mcTime)
-        mcTime=mcTime-1
-        if mcTime<0:
-            mcTime=0
-    elif s=="right":
-        ErasePaths(pathData,visualPath,visualBall,mcTime)
-        mcTime=mcTime+1
-        if mcTime>maxmcTime:
-            mcTime=maxmcTime
-    elif s=="q":
-        exit()
-    print "The current mctime is ",mcTime
-    PlotPaths(pathData,visualPath,visualBall,mcTime)
+ballTime=0
+PlotPaths(pathData,visualPath,visualBall,mcTime,ballTime)
+bedone=false
+
+
+    
+
+#    c.interact()
+#    if (scene.kb.keys):
+while (not(bedone)):
+    if (scene.kb.keys):
+        s=scene.kb.getkey()
+        if s=="left":
+            ErasePaths(visualPath,visualBall)
+            mcTime=mcTime-1
+            if mcTime<0:
+                mcTime=0
+        elif s=="right":
+            ErasePaths(visualPath,visualBall)
+            mcTime=mcTime+1
+            if mcTime>maxmcTime:
+                mcTime=maxmcTime
+        elif s=="r":
+            ErasePaths(visualPath,visualBall)
+            ballTime=ballTime+1
+            if ballTime>maxBallTime:
+                ballTime=maxBallTime
+        elif s=="l":
+            ErasePaths(visualPath,visualBall)
+            ballTime=ballTime-1
+            if ballTime<0:
+                ballTime=0
+        elif s=="q":
+            bedone=true
+        print "The current mctime is ",mcTime
+        PlotPaths(pathData,visualPath,visualBall,mcTime,ballTime)
+scene.visible=false
 
 ## elif s=="r":
 ##         ballslice=ballslice+1
