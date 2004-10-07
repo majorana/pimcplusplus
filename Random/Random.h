2751c6937f65142510e0f9196d1a0b692a62ad6e
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2004-10-07 20:17:54 +0000

    Added global random gaussian scalar and vectors.

--- Random/Random.h
+++ Random/Random.h
@@ -26,25 +26,24 @@ public:
   /* normal random variate generator */
   // Returns a random number distributed according to
   // P(x) = 1/sqrt(2*pi * sigma) * exp ((x - mean)^2 /(2*sigma^2))
-  inline double LocalGaussian(double mean, double sigma);                                      
-    
-  /// Produces a guassian random vector with radius that has variance
-  /// sigma 
+  inline double LocalGaussian(double sigma);
+  
+  /// Produces a guassian random vector with radius that has STD sigma
   inline void LocalGaussianVec(double sigma, Vec3 &c)
-  {
-    c(0)=LocalGaussian(0.0,sigma);
-    c(1)=LocalGaussian(0.0,sigma);
-    c(2)=LocalGaussian(0.0,sigma);    
-  }
+  { c(0)=LocalGaussian(sigma); c(1)=LocalGaussian(sigma); c(2)=LocalGaussian(sigma); }
   
-  /// Produces a guassian random vector with radius that has variance
-  /// sigma 
+  /// Produces a guassian random vector with radius that has STD sigma 
   inline void LocalGaussianVec(double sigma,Vec2 &c)
-  {
-    c(0)=LocalGaussian(0.0,sigma);
-    c(1)=LocalGaussian(0.0,sigma);
-  }
+  { c(0)=LocalGaussian(sigma); c(1)=LocalGaussian(sigma); }
   
+  inline double CommonGaussian(double sigma);
+  /// Produces a guassian random vector with radius that has STD sigma
+  inline void CommonGaussianVec(double sigma, Vec3 &c)
+  { c(0)=CommonGaussian(sigma); c(1)=CommonGaussian(sigma); c(2)=CommonGaussian(sigma); }
+  
+  /// Produces a guassian random vector with radius that has STD sigma 
+  inline void CommonGaussianVec(double sigma,Vec2 &c)
+  { c(0)=CommonGaussian(sigma); c(1)=CommonGaussian(sigma); }
   
   void Init()
   {
@@ -61,7 +60,7 @@ public:
     CloneNumber=myProc/procsPerClone;
     int commID=numProcs+CloneNumber;
     LocalStream = init_sprng(SPRNG_DEFAULT, myProc,numProcs+NumClones,seed,SPRNG_DEFAULT);
-    CommonStream = init_sprng(SPRNG_DEFAULT, commID,numProcs+NumClones,seed,SPRNG_DEFAULT);    
+    CommonStream = init_sprng(SPRNG_DEFAULT, commID,numProcs+NumClones,seed,SPRNG_DEFAULT);
   }
 
 
@@ -71,33 +70,57 @@ public:
   }
 };
 
-inline double RandomClass::LocalGaussian(double mean, double sigma)
+inline double RandomClass::LocalGaussian(double sigma)
 {                                      
   double V1, V2, S, X1;
   static double X2;
   static int OneLeft = 0;
   double temp;
 
-  if (OneLeft)                   // Use second number from last computation
-    {
-      X1 = X2;
-      OneLeft = 0;
-    }
-  else
-    {
-      do
-        {
-          V1 = 2.0 * Local() - 1.0;
-          V2 = 2.0 * Local() - 1.0;
-          S = V1 * V1 + V2 * V2;
-        } while ( S >= 1.0 );
-      
-      temp = sqrt( (-2.0 * log((double) S ) ) / S );
-      X1 = V1 * temp;
-      X2 = V2 * temp;
-      OneLeft = 1;
-    }  
-  return( mean + X1 * sigma );
+  if (OneLeft) {                   // Use second number from last computation
+    X1 = X2;
+    OneLeft = 0;
+  }
+  else {
+    do {
+      V1 = 2.0 * Local() - 1.0;
+      V2 = 2.0 * Local() - 1.0;
+      S = V1 * V1 + V2 * V2;
+    } while ( S >= 1.0 );
+    
+    temp = sqrt( (-2.0 * log((double) S ) ) / S );
+    X1 = V1 * temp;
+    X2 = V2 * temp;
+    OneLeft = 1;
+  }  
+  return( X1 * sigma );
+}
+
+
+inline double RandomClass::CommonGaussian(double sigma)
+{                                      
+  double V1, V2, S, X1;
+  static double X2;
+  static int OneLeft = 0;
+  double temp;
+
+  if (OneLeft) {                   // Use second number from last computation
+    X1 = X2;
+    OneLeft = 0;
+  }
+  else {
+    do {
+      V1 = 2.0 * Common() - 1.0;
+      V2 = 2.0 * Common() - 1.0;
+      S = V1 * V1 + V2 * V2;
+    } while ( S >= 1.0 );
+    
+    temp = sqrt( (-2.0 * log((double) S ) ) / S );
+    X1 = V1 * temp;
+    X2 = V2 * temp;
+    OneLeft = 1;
+  }  
+  return( X1 * sigma );
 }
 
 #endif
