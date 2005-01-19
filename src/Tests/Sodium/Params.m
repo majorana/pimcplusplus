3fc83aa7f16323b2adaf9f1600759f1178fae4a2
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-01-19 22:58:51 +0000

    Adding to repository.

---
+++ src/Tests/Sodium/Params.m
@@ -0,0 +1,14 @@
+T = 2210;  % kelvin
+rs = 5.90; % bohr
+N = 16;    % particles
+kB = 1.3807e-23 % Joules/Kelvin
+kB = kB * 6.24150974e18;  % eV/Kelvin
+kB = kB / 27.211383;       % hartree/Kelvin
+kBT = T * kB;
+
+beta = 1.0/kBT
+
+V = 4*pi/3 * rs^3 * N
+L = V^(1/3)
+tau = 0.125 % inverse hartrees
+M = beta/tau
