c580da5b0d96b3af692f3914178ed99c956c3d59
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-02-08 19:23:05 +0000

    Added new move, but will get rid of it soon

---
+++ src/Moves/StructureReject.cc
@@ -0,0 +1,36 @@
+#include "StructureReject.h"
+
+
+double StructureRejectStageClass::Sample(int &slice1,int &slice2,
+				      Array<int,1> &changedParticles)
+{
+  //do nothing for now
+}
+
+
+
+
+void StructureRejectStageClass::Read (IOSectionClass &in)
+{
+  assert(in.ReadVar("MaxValue",MaxValue));
+  StructureFactor.Read(in);
+}
+
+bool StructureRejectStageClass::Attempt (int &slice1, int &slice2, 
+			       Array<int,1> &activeParticles, 
+			       double &prevActionChange)
+{
+  bool toReturn=false;
+  //  double tcounts=(double)StructureFactor.TotalCounts;
+  //  double nslices=(double)PathData.Path.NumTimeSlices();
+  double norm=PathData.Path.NumParticles();
+  StructureFactor.Calculate();
+  for (int counter=0;counter<StructureFactor.Sk.size();counter++){
+    cerr<<StructureFactor.Sk(counter)/norm<<endl;
+    if (StructureFactor.Sk(counter)/norm>4){
+      toReturn=true;
+    }
+  }
+  StructureFactor.Clear();
+  return toReturn;
+}
---
+++ src/Moves/StructureReject.h
@@ -0,0 +1,31 @@
+#ifndef STRUCTURE_REJECT_STAGE_CLASS_H
+#define STRUCTURE_REJECT_STAGE_CLASS_H
+
+#include "MultiStage.h"
+#include "../Observables/StructureFactor.h"
+
+class StructureRejectStageClass : public LocalStageClass
+{
+private:
+  int Species1;
+  int Species2;
+  double MaxValue;
+  StructureFactorClass StructureFactor;
+public:
+  void Read(IOSectionClass &in);
+  double Sample (int &slice1,int &slice2,
+		 Array<int,1> &changedParticles); 
+  bool Attempt(int &slice1, int &slice2,
+	       Array<int,1> &activeParticles,
+	       double &prevActionChange);
+
+  StructureRejectStageClass (PathDataClass &pathData, IOSectionClass &in) 
+    : LocalStageClass(pathData),
+    StructureFactor(pathData,in)
+  {
+    // do nothing for now
+  }
+
+};
+
+#endif
