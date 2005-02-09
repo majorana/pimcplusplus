286219ba1cdaffda815483bf78a6019d63354af4
gergely <gergely@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-02-09 17:21:41 +0000

    Actually adding modified energy observables and dummy pair actions.

---
+++ src/Observables/ObservableModifiedEnergy.cc
@@ -0,0 +1,265 @@
+#include "ObservableModifiedEnergy.h"
+
+
+// Fix to include final link between link M and 0
+void ModifiedEnergyClass::Accumulate()
+{
+  TimesCalled++;
+  if (TimesCalled % DumpFreq==0)
+    WriteBlock();
+
+  if ((TimesCalled % Freq)!=0){
+    return;
+  }
+  //Move the join to the end so we don't have to worry about permutations
+  PathData.MoveJoin(PathData.NumTimeSlices()-1);
+  
+  NumSamples++;
+
+  double kinetic, dUShort, dULong, node, vShort, vLong, tip5p;
+  PathData.Actions.Energy (kinetic, dUShort, dULong, node, vShort, vLong);
+
+  int slice1 = 0;
+  int slice2 = PathData.Path.TotalNumSlices;
+  tip5p = PathData.Actions.TIP5PWater.d_dBeta(slice1,slice2,0);
+  
+  TotalSum   += kinetic + dUShort + dULong + node + tip5p;
+  KineticSum += kinetic;
+  dUShortSum += dUShort;
+  dULongSum  += dULong;
+  NodeSum    += node;
+  VShortSum  += vShort;
+  VLongSum   += vLong;
+  TIP5PSum   += tip5p;
+}
+
+
+// // Fix to include final link between link M and 0
+// void EnergyClass::Accumulate()
+// {
+//   TimesCalled++;
+//   if (TimesCalled % DumpFreq==0)
+//     WriteBlock();
+
+//   if ((TimesCalled % Freq)!=0){
+//     return;
+//   }
+//   //Move the join to the end so we don't have to worry about permutations
+//   PathData.MoveJoin(PathData.NumTimeSlices()-1);
+//   // Loop over all links
+//   int numLinks = PathData.NumTimeSlices()-1; 
+  
+//   NumSamples++;
+
+//   /// CHECK code
+//   double Echeck = 0.0;
+//   double spring, dU, V = 0.0;
+//   spring = dU = 0.0;
+//   for (int slice=0; slice<numLinks; slice++) {
+//     double sp, du,v;
+//     PathData.Action.Energy(slice, 0, sp, du);
+//     v=PathData.Action.PotentialEnergy(slice);
+//     dU += du; //*PathData.Path.Weight;
+//     spring += sp; //*PathData.Path.Weight;
+//     V += v; //*PathData.Path.Weight;
+//   }
+
+//   double node = 0.0;
+//   for (int species=0; species<PathData.Path.NumSpecies(); species++)
+//     if (PathData.Actions.NodalActions(species) != NULL)
+//       node += PathData.Actions.NodalActions(species)->d_dBeta(0,numLinks,0);
+//   Echeck = spring + dU + node;
+//   ESum += Echeck;
+//   VSum += V;
+//   SSum += spring;
+//   FSum += dU;
+//   NodeSum += node;
+// }
+
+
+void ModifiedEnergyClass::ShiftData (int NumTimeSlices)
+{
+  // Do nothing
+}
+
+void ModifiedEnergyClass::WriteBlock()
+{
+  int nslices=PathData.Path.TotalNumSlices;
+  double norm = 1.0/((double)NumSamples*(double)nslices);
+  
+  TotalVar.Write(PathData.Path.Communicator.Sum(TotalSum)*norm);
+  KineticVar.Write(PathData.Path.Communicator.Sum(KineticSum)*norm);
+//  dUShortVar.Write(PathData.Path.Communicator.Sum(dUShortSum)*norm);
+//  dULongVar.Write(PathData.Path.Communicator.Sum(dULongSum)*norm);
+//  NodeVar.Write(PathData.Path.Communicator.Sum(NodeSum)*norm);
+//  VShortVar.Write(PathData.Path.Communicator.Sum(VShortSum)*norm);
+//  VLongVar.Write(PathData.Path.Communicator.Sum(VLongSum)*norm);
+  TIP5PVar.Write(PathData.Path.Communicator.Sum(TIP5PSum)*norm);
+
+  cerr << "Total " << TotalSum*norm << " and per molecule: " << TotalSum*norm/PathData.Path.numMol << endl;
+  cerr << "Kinetic " << KineticSum*norm << " and per molecule: " << KineticSum*norm/PathData.Path.numMol << endl;
+  cerr << "TIP5P " << TIP5PSum*norm << " and per molecule: " << TIP5PSum*norm/PathData.Path.numMol << endl;
+  
+  TotalSum   = 0.0;
+  KineticSum = 0.0;
+  dUShortSum = 0.0;
+  dULongSum  = 0.0;
+  NodeSum    = 0.0;
+  VShortSum  = 0.0;
+  VLongSum   = 0.0;
+  TIP5PSum   = 0.0;
+  NumSamples = 0;
+}
+
+//   double totSum;
+//   double totNumSamples;
+  
+//   double myAvg = ESum/(double)NumSamples; //everybody should have the same number of samples for this to be happy
+//   double myVAvg= VSum/(double)NumSamples;
+//   double mySAvg= SSum/(double)NumSamples;
+//   double myFAvg= FSum/(double)NumSamples;
+//   double myNodeAvg = NodeSum/(double)NumSamples;
+//   double avg = PathData.Path.Communicator.Sum(myAvg);
+//   double vavg =PathData.Path.Communicator.Sum(myVAvg);
+//   double savg =PathData.Path.Communicator.Sum(mySAvg);
+//   double favg =PathData.Path.Communicator.Sum(myFAvg);
+//   double NodeAvg =PathData.Path.Communicator.Sum(myNodeAvg);
+//   avg  = avg/(double)PathData.Path.TotalNumSlices;
+//   vavg =vavg/(double)PathData.Path.TotalNumSlices;
+//   savg =savg/(double)PathData.Path.TotalNumSlices;
+//   favg =favg/(double)PathData.Path.TotalNumSlices;
+//   NodeAvg = NodeAvg/(double)(PathData.Path.TotalNumSlices);
+//   // Only processor 0 writes.
+//   if (PathData.Path.Communicator.MyProc()==0) {
+//     cerr << "myAvg = " << myAvg << endl;
+//     cerr << "avg = " << avg << endl;
+//     cerr << "Pot avg = " << vavg << endl;
+//     cerr << "S avg = " << savg << endl;
+//     cerr << "U avg = " <<favg <<endl;
+//     cerr << "NodeAvg = " <<NodeAvg <<endl;
+//     if (FirstTime) {
+//       FirstTime = false;
+//       WriteInfo();
+//       IOSection.WriteVar("Type","Scalar");
+//       Array<double,1> dummy(1);
+//       dummy(0)=avg;
+//       IOSection.WriteVar ("TotalEnergy", dummy);
+//       dummy(0)=vavg;
+//       IOSection.WriteVar ("PotentialEnergy",dummy);
+//       dummy(0)=savg;
+//       IOSection.WriteVar ("SpringEnergy",dummy);
+//       dummy(0)=favg;
+//       IOSection.WriteVar ("DBetaEnergy",dummy);
+//       dummy(0)=NodeAvg;
+//       IOSection.WriteVar ("NodeEnergy",dummy);
+//       IOVar = IOSection.GetVarPtr("TotalEnergy");
+//       IOVVar= IOSection.GetVarPtr("PotentialEnergy");
+//       IOSVar= IOSection.GetVarPtr("SpringEnergy");
+//       IOUVar= IOSection.GetVarPtr("DBetaEnergy");
+//       IONodeVar= IOSection.GetVarPtr("NodeEnergy");
+//     }
+//     else {
+//       IOVar->Append(avg);
+//       IOVVar->Append(vavg);
+//       IOSVar->Append(savg);
+//       IOUVar->Append(favg);
+//       IONodeVar->Append(NodeAvg);
+//       IOSection.FlushFile();
+//     }
+//   }
+//   ESum       = 0.0;
+//   VSum       = 0.0;
+//   SSum       = 0.0;
+//   FSum       = 0.0;
+//   NodeSum    = 0.0;
+//  NumSamples = 0;
+//}
+
+void ModifiedEnergyClass::Read(IOSectionClass &in)
+{  
+  ObservableClass::Read(in);
+  assert(in.ReadVar("freq",Freq));
+  assert(in.ReadVar("dumpFreq",DumpFreq));
+  if (PathData.Path.Communicator.MyProc()==0){
+    WriteInfo();
+    IOSection.WriteVar("Type","Scalar");
+  }
+}
+
+
+////Code for energy sign class
+
+
+// Fix to include final link between link M and 0
+/*
+void EnergySignClass::Accumulate()
+{
+  TimesCalled++;
+  if (TimesCalled % DumpFreq==0)
+    WriteBlock();
+
+  if ((TimesCalled % Freq)!=0){
+    return;
+  }
+  //Move the join to the end so we don't have to worry about permutations
+  PathData.MoveJoin(PathData.NumTimeSlices()-1);
+  
+  NumSamples++;
+
+  double kinetic, dUShort, dULong, node, vShort, vLong;
+  PathData.Actions.Energy (kinetic, dUShort, dULong, node, vShort, vLong);
+  
+  TotalSum   += (kinetic + dUShort + dULong + node)*PathData.Path.Weight;
+  KineticSum += kinetic * PathData.Path.Weight;
+  dUShortSum += dUShort * PathData.Path.Weight;
+  dULongSum  += dULong * PathData.Path.Weight;
+  NodeSum    += node * PathData.Path.Weight;
+  VShortSum  += vShort * PathData.Path.Weight;
+  VLongSum   += vLong * PathData.Path.Weight;
+}
+
+
+
+void EnergySignClass::ShiftData (int NumTimeSlices)
+{
+  // Do nothing
+}
+
+void EnergySignClass::WriteBlock()
+{
+  int nslices=PathData.Path.TotalNumSlices;
+  double norm = 1.0/((double)NumSamples*(double)nslices);
+  
+  TotalVar.Write(PathData.Path.Communicator.Sum(TotalSum)*norm);
+  KineticVar.Write(PathData.Path.Communicator.Sum(KineticSum)*norm);
+  dUShortVar.Write(PathData.Path.Communicator.Sum(dUShortSum)*norm);
+  dULongVar.Write(PathData.Path.Communicator.Sum(dULongSum)*norm);
+  NodeVar.Write(PathData.Path.Communicator.Sum(NodeSum)*norm);
+  VShortVar.Write(PathData.Path.Communicator.Sum(VShortSum)*norm);
+  VLongVar.Write(PathData.Path.Communicator.Sum(VLongSum)*norm);
+
+  if (PathData.Path.Communicator.MyProc()==0)
+    IOSection.FlushFile();
+  
+  TotalSum   = 0.0;
+  KineticSum = 0.0;
+  dUShortSum = 0.0;
+  dULongSum  = 0.0;
+  NodeSum    = 0.0;
+  VShortSum  = 0.0;
+  VLongSum   = 0.0;
+  NumSamples = 0;
+}
+
+
+void EnergySignClass::Read(IOSectionClass &in)
+{  
+  ObservableClass::Read(in);
+  assert(in.ReadVar("freq",Freq));
+  assert(in.ReadVar("dumpFreq",DumpFreq));
+  if (PathData.Path.Communicator.MyProc()==0){
+    WriteInfo();
+    IOSection.WriteVar("Type","Scalar");
+  }
+}
+*/
---
+++ src/Observables/ObservableModifiedEnergy.h
@@ -0,0 +1,91 @@
+#ifndef OBSERVABLE_MODIFIED_ENERGY_H
+#define OBSERVABLE_MODIFIED_ENERGY_H
+
+#include "ObservableBase.h"
+
+class ModifiedEnergyClass : public ObservableClass
+{
+
+private:
+  double TotalSum, KineticSum, dUShortSum, dULongSum, NodeSum, VShortSum, VLongSum, TIP5PSum;
+
+  ObservableDouble TotalVar, KineticVar, dUShortVar, dULongVar, NodeVar,
+    VShortVar, VLongVar, TIP5PVar;
+
+  int NumSamples;
+  int TimesCalled;
+  int Freq;
+  int DumpFreq;
+public:
+  void Accumulate();
+  void WriteBlock();
+  void ShiftData(int numTimeSlices);
+  void Read(IOSectionClass& in);
+  ModifiedEnergyClass(PathDataClass &myPathData, IOSectionClass &ioSection)
+    : ObservableClass(myPathData, ioSection) , 
+      TotalVar  ("Total",  IOSection,myPathData.Path.Communicator),
+      KineticVar("Kinetic",IOSection,myPathData.Path.Communicator), 
+      dUShortVar("dUShort",IOSection,myPathData.Path.Communicator), 
+      dULongVar ("dULong", IOSection,myPathData.Path.Communicator), 
+      NodeVar   ("Node",   IOSection,myPathData.Path.Communicator), 
+      VShortVar ("VShort",IOSection,myPathData.Path.Communicator), 
+      VLongVar  ("VLong",IOSection,myPathData.Path.Communicator),
+      TIP5PVar  ("TIP5P",IOSection,myPathData.Path.Communicator)
+  {
+    TotalSum   = 0.0;
+    KineticSum = 0.0;
+    dUShortSum = 0.0;
+    dULongSum  = 0.0;
+    NodeSum    = 0.0;
+    VShortSum  = 0.0;
+    VLongSum   = 0.0;
+    TIP5PSum = 0.0;
+    NumSamples = 0;
+    TimesCalled=0;
+  }
+};
+
+/*
+class EnergySignClass : public ObservableClass
+{
+
+private:
+  double TotalSum, KineticSum, dUShortSum, dULongSum, NodeSum, 
+    VShortSum, VLongSum;
+
+  ObservableDouble TotalVar, KineticVar, dUShortVar, dULongVar, NodeVar,
+    VShortVar, VLongVar;
+
+  int NumSamples;
+  int TimesCalled;
+  int Freq;
+  int DumpFreq;
+public:
+  void Accumulate();
+  void WriteBlock();
+  void ShiftData(int numTimeSlices);
+  void Read(IOSectionClass& in);
+  EnergySignClass(PathDataClass &myPathData, IOSectionClass &ioSection)
+    : ObservableClass(myPathData, ioSection) , 
+      TotalVar  ("Total",  IOSection,myPathData.Path.Communicator),
+      KineticVar("Kinetic",IOSection,myPathData.Path.Communicator), 
+      dUShortVar("dUShort",IOSection,myPathData.Path.Communicator), 
+      dULongVar ("dULong", IOSection,myPathData.Path.Communicator), 
+      NodeVar   ("Node",   IOSection,myPathData.Path.Communicator), 
+      VShortVar ("VShort",IOSection,myPathData.Path.Communicator), 
+      VLongVar  ("VLong",IOSection,myPathData.Path.Communicator)
+  {
+    TotalSum   = 0.0;
+    KineticSum = 0.0;
+    dUShortSum = 0.0;
+    dULongSum  = 0.0;
+    NodeSum    = 0.0;
+    VShortSum  = 0.0;
+    VLongSum   = 0.0;
+    NumSamples = 0;
+    TimesCalled=0;
+  }
+};
+*/
+
+#endif 
