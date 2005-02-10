23f2c6599473a4efc943cfcb2e43d68bd937a59f
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-02-10 19:10:11 +0000

    Action that is large if teh structure factor is greater then some number

---
+++ src/Actions/StructureReject.h
@@ -0,0 +1,21 @@
+#ifndef STRUCTURE_REJECT_H
+#define STRUCTURE_REJECT_H
+
+#include "ActionBase.h"
+
+class StructureRejectClass : public ActionBaseClass
+{
+  Array<double,1> Sk;
+  int Species1;
+  int Species2;
+  int TotalCounts;
+public:
+  void Read (IOSectionClass &in);
+  double Action (int slice1, int slice2, 
+		 const Array<int,1> &activeParticles, int level);
+  double d_dBeta (int slice1, int slice2, int level);
+  StructureRejectClass (PathDataClass &pathData);
+};
+
+
+#endif
