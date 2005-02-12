0bfb20fcf322f8db4095fed14d592402139b02e7
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-02-12 02:55:19 +0000

    Committing these to the repository.  Made path dump work (in theory) with time slice parallelism and we now record the total permutation cycle.

--- src/Observables/PathDump.cc
+++ src/Observables/PathDump.cc
@@ -17,45 +17,125 @@ void PathDumpClass::Read(IOSectionClass &in)
 
 void PathDumpClass::WriteBlock()
 {
-  //Move the join to the end so we don't have to worry about permutations
-  PathData.MoveJoin(PathData.NumTimeSlices()-1);
+//   PathClass &Path = PathData.Path;
+//   int first0, last0;
+//   Path.SliceRange (0, first0, last0);
+//   // First, let's get the reference slice to processor 0's first slice
+//   while (Path.GetRefSlice() >= (last0-1)) {
+//     PathData.MoveJoin((last0-1));
+//     PathData.ShiftData (-(last0-1));
+//     PathData.Join = 0;
+//   }
+//   PathData.MoveJoin(Path.GetRefSlice());
+//   PathData.ShiftData (-Path.GetRefSlice());
+//   assert (Path.GetRefSlice() == 0);
+//   PathData.Join = 0;
+//   // Now the reference slice should be at proc0's slice 0
 
+//   //Move the join to the end so we don't have to worry about permutations
+//   PathData.MoveJoin(PathData.NumTimeSlices()-1);
+  PathClass &Path = PathData.Path;
+  int refSave = PathData.Path.GetRefSlice();
+  PathData.MoveRefSlice(0);
+
+  int start, end, numProcs, myProc;
+  numProcs = Path.Communicator.NumProcs();
+  myProc   = Path.Communicator.MyProc();
+
+  Array<double,3> pathArray;
+  int totalSlices = Path.TotalNumSlices;
+
+  Path.SliceRange(numProcs-1, start,end);
+  int maxShift = end-start+1;
+  int slicesLeft = totalSlices;
+  int offset = 0;
   int numPtcls = PathData.NumParticles();
-  int numTimeSlices = PathData.NumTimeSlices();
-  if (FirstTime){
-    FirstTime=false;
+
+  if (myProc == 0)
+    pathArray.resize(numPtcls,totalSlices,NDIM);
+  while (slicesLeft > maxShift) {
+    // First copy
+    PathData.MoveJoin(maxShift);
+    if (myProc == 0)
+      for (int i=0; i<maxShift; i++)
+	for (int ptcl=0; ptcl < numPtcls; ptcl++)
+	  for (int dim=0; dim<NDIM; dim++)
+	    pathArray(ptcl, i+offset, dim) = Path(i,ptcl)[dim];
+    // Now shift
+    PathData.ShiftData(-maxShift);
+    PathData.Join = 0;
+    slicesLeft -= maxShift;
+    offset += maxShift;
+  }
+  // Move join out of the way
+  PathData.MoveJoin (Path.NumTimeSlices()-1);
+  // Copy last part
+  if (myProc == 0)
+    for (int i=0; i<slicesLeft; i++)
+      for (int ptcl=0; ptcl < numPtcls; ptcl++)
+	for (int dim=0; dim<NDIM; dim++)
+	  pathArray(ptcl, i+offset, dim) = Path(i,ptcl)[dim];
+  
+  // Reset path to original position
+  PathData.MoveRefSlice (refSave);
+
+  Array<int,1> permVec(numPtcls);
+  Path.TotalPermutation(permVec);
+
+  // Now write
+  PathVar.Write (pathArray);
+  PermVar.Write (permVec);
+
+
+  if (FirstTime && (myProc == 0)) {
     WriteInfo();
     Array<string,1> speciesNames(numPtcls);
-    for (int speciesIndex=0;speciesIndex<PathData.NumSpecies();speciesIndex++){
-      SpeciesClass &species = PathData.Path.Species(speciesIndex);
+    for (int spIndex=0;spIndex<PathData.NumSpecies();spIndex++){
+      SpeciesClass &species = PathData.Path.Species(spIndex);
       for (int ptcl=species.FirstPtcl; ptcl<=species.LastPtcl; ptcl++)
-      	speciesNames(ptcl)=species.Name;
+	speciesNames(ptcl)=species.Name;
     }
     IOSection.WriteVar("SpeciesNames", speciesNames);
     IOSection.WriteVar("Type","Path");
-    Array<double,4> pathArray(1,numPtcls,numTimeSlices,NDIM);
-    for (int ptcl=0;ptcl<numPtcls;ptcl++){
-      for (int slice=0;slice<numTimeSlices;slice++){
-	for (int dim=0;dim<NDIM;dim++){
-	  pathArray(0,ptcl,slice,dim)=PathData(slice,ptcl)[dim];
-	}
-      }
-    }
+  }
+  FirstTime = false;
+  IOSection.FlushFile();
+//   int numPtcls = PathData.NumParticles();
+//   int numTimeSlices = PathData.NumTimeSlices();
+//   if (FirstTime){
+//     FirstTime=false;
+//     WriteInfo();
+//     Array<string,1> speciesNames(numPtcls);
+//     for (int speciesIndex=0;speciesIndex<PathData.NumSpecies();speciesIndex++){
+//       SpeciesClass &species = PathData.Path.Species(speciesIndex);
+//       for (int ptcl=species.FirstPtcl; ptcl<=species.LastPtcl; ptcl++)
+//       	speciesNames(ptcl)=species.Name;
+//     }
+//     IOSection.WriteVar("SpeciesNames", speciesNames);
+//     IOSection.WriteVar("Type","Path");
+//     Array<double,4> pathArray(1,numPtcls,numTimeSlices,NDIM);
+//     for (int ptcl=0;ptcl<numPtcls;ptcl++){
+//       for (int slice=0;slice<numTimeSlices;slice++){
+// 	for (int dim=0;dim<NDIM;dim++){
+// 	  pathArray(0,ptcl,slice,dim)=PathData(slice,ptcl)[dim];
+// 	}
+//       }
+//     }
 
-    // Write the first path here
-    IOSection.WriteVar("Path",pathArray);
-    // Now get the pointer to it
-    IOVar = IOSection.GetVarPtr("Path");
-  }
-  else {
-    // Append the new path here.
-    Array<double,3> pathArray(numPtcls,numTimeSlices,NDIM);
-    for (int ptcl=0;ptcl<numPtcls;ptcl++)
-      for (int slice=0;slice<numTimeSlices;slice++)
-	for (int dim=0;dim<NDIM;dim++)
-	  pathArray(ptcl,slice,dim)=PathData(slice,ptcl)[dim];
-    IOVar->Append(pathArray);
-    IOSection.FlushFile();
-  }
+//     // Write the first path here
+//     IOSection.WriteVar("Path",pathArray);
+//     // Now get the pointer to it
+//     IOVar = IOSection.GetVarPtr("Path");
+//   }
+//   else {
+//     // Append the new path here.
+//     Array<double,3> pathArray(numPtcls,numTimeSlices,NDIM);
+//     for (int ptcl=0;ptcl<numPtcls;ptcl++)
+//       for (int slice=0;slice<numTimeSlices;slice++)
+// 	for (int dim=0;dim<NDIM;dim++)
+// 	  pathArray(ptcl,slice,dim)=PathData(slice,ptcl)[dim];
+//     IOVar->Append(pathArray);
+//     IOSection.FlushFile();
+//   }
 }
 
--- src/Observables/PathDump.h
+++ src/Observables/PathDump.h
@@ -7,14 +7,19 @@
 class PathDumpClass : public ObservableClass
 {
 private:
+  ObservableVecDouble3 PathVar;
+  ObservableVecInt1 PermVar;
 public:
   int TimesCalled;
   void Accumulate();
   void WriteBlock();
   void Read(IOSectionClass& IO);
   int DumpFreq;
-  PathDumpClass(PathDataClass &myPathData, IOSectionClass &ioSection)
-    : ObservableClass(myPathData, ioSection)  { 
+  PathDumpClass(PathDataClass &myPathData, IOSectionClass &ioSection) :
+    ObservableClass(myPathData, ioSection),
+    PathVar ("Path", IOSection, myPathData.Path.Communicator),
+    PermVar ("Permutation", IOSection, myPathData.Path.Communicator)
+  { 
     Name="PathDump";
     TimesCalled=0;
   }
