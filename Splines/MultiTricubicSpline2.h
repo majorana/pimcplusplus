146edf3dfd99cbc60f1fe1a7ea81f2a6dc9a5249
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2005-03-12 04:35:20 +0000

    Adding MultiTricubicSpline2.  It's faster by using templates.

--- Splines/MultiTricubicSpline2.h
+++ Splines/MultiTricubicSpline2.h
@@ -1,5 +1,5 @@
-#ifndef MULTI_TRICUBIC_SPLINE_H
-#define MULTI_TRICUBIC_SPLINE_H
+#ifndef MULTI_TRICUBIC_SPLINE2_H
+#define MULTI_TRICUBIC_SPLINE2_H
 
 #include "Grid.h"
 #include <cmath>
@@ -17,6 +17,7 @@
 /// 7) d3F/dxdydz
 
 
+template <int N> 
 class MultiTricubicSpline
 {
   inline double p1(double t)
@@ -47,24 +48,25 @@ class MultiTricubicSpline
   // dim:     Dimension to calculate derivative w.r.t
   // source:  Function to differentiate
   // dest:    where to put result
-  void UpdateX (int source, int dest, int n);
-  void UpdateY (int source, int dest, int n);
-  void UpdateZ (int source, int dest, int n);
+  inline void UpdateX (int source, int dest, int n);
+  inline void UpdateY (int source, int dest, int n);
+  inline void UpdateZ (int source, int dest, int n);
   bool UpToDate;
-  Array<double,1> bSum, cSum;
+  TinyVector<double,N> bSum, cSum;
 public:
   // Indices are (ix,iy,iz,[0-7],spline#)
-  Array<double,5> F;
+  Array<TinyMatrix<double,8,N>,3> F;
 
-  int Nx, Ny, Nz, N;
+  int Nx, Ny, Nz;
   Grid *Xgrid, *Ygrid, *Zgrid;
   TinyVector<Grid*,3> Grids;
-  void Update();
+  inline void Update();
   inline double operator()(int ix, int iy, int iz, int n) const
   { return (F(ix,iy,iz,0,n)); }
   inline double& operator()(int ix, int iy, int iz, int n) 
   { UpToDate=false; return (F(ix,iy,iz,0,n)); }
-  inline void operator()(double x, double y, double z, Array<double,1> &val);
+  inline void operator()(double x, double y, double z, 
+			 TinyVector<double,N> &val); 
 //   inline double d_dx      (double x, double y, double z); 
 //   inline double d_dy      (double x, double y, double z); 
 //   inline double d_dz      (double x, double y, double z); 
@@ -77,25 +79,58 @@ public:
 //   inline TinyVector<double,3> Grad (double x, double y, double z);
 //   inline double Laplacian (double x, double y, double z);
 
-  MultiTricubicSpline(Grid *xgrid, Grid *ygrid, Grid *zgrid, int num)
+  MultiTricubicSpline(Grid *xgrid, Grid *ygrid, Grid *zgrid)
   {
     Xgrid = xgrid; Nx = xgrid->NumPoints;
     Ygrid = ygrid; Ny = ygrid->NumPoints;
     Zgrid = zgrid; Nz = zgrid->NumPoints;
-    N = num;
 
-    F.resize(Nx,Ny,Nz, 8, num);
-    bSum.resize(num);
-    cSum.resize(num);
+    F.resize(Nx,Ny,Nz);
     UpToDate = false;
   }
   
   /// Copy constructor
-  inline MultiTricubicSpline (const MultiTricubicSpline &a);
-
+  inline MultiTricubicSpline (const MultiTricubicSpline &a)
+  {
+    F.resize(a.F.shape());
+    F = a.F;
+    Nx = a.Nx;
+    Ny = a.Ny;
+    Nz = a.Nz;
+    Xgrid = a.Xgrid;
+    Ygrid = a.Ygrid;
+    Zgrid = a.Zgrid;
+    UpToDate = a.UpToDate;
+  }
   /// Assigment operator -- necessary for array resizeAndPreserve
-  inline MultiTricubicSpline & operator= (MultiTricubicSpline &a);
-  inline MultiTricubicSpline & operator= (MultiTricubicSpline a);
+  inline MultiTricubicSpline & operator= (MultiTricubicSpline &a)
+  {
+    F.resize(a.F.shape());
+    F = a.F;
+    Nx = a.Nx;
+    Ny = a.Ny;
+    Nz = a.Nz;
+    Xgrid = a.Xgrid;
+    Ygrid = a.Ygrid;
+    Zgrid = a.Zgrid;
+    UpToDate = a.UpToDate;
+    return (*this);
+  }
+
+  inline MultiTricubicSpline & operator= (MultiTricubicSpline a)
+  {
+    F.resize(a.F.shape());
+    F = a.F;
+    Nx = a.Nx;
+    Ny = a.Ny;
+    Nz = a.Nz;
+    N  = a.N;
+    Xgrid = a.Xgrid;
+    Ygrid = a.Ygrid;
+    Zgrid = a.Zgrid;
+    UpToDate = a.UpToDate;
+    return (*this);
+  }
 
 
   inline void Init (Grid *xgrid, Grid *ygrid, Grid *zgrid,
@@ -111,62 +146,8 @@ public:
 };
 
 
-inline MultiTricubicSpline::MultiTricubicSpline(const MultiTricubicSpline &a)
-{
-  F.resize(a.F.shape());
-  F = a.F;
-  Nx = a.Nx;
-  Ny = a.Ny;
-  Nz = a.Nz;
-  N  = a.N;
-  bSum.resize(N);
-  cSum.resize(N);
-  Xgrid = a.Xgrid;
-  Ygrid = a.Ygrid;
-  Zgrid = a.Zgrid;
-  UpToDate = a.UpToDate;
-}
-
-
-inline MultiTricubicSpline& MultiTricubicSpline::operator=(MultiTricubicSpline &a)
-{
-  F.resize(a.F.shape());
-  F = a.F;
-  Nx = a.Nx;
-  Ny = a.Ny;
-  Nz = a.Nz;
-  N  = a.N;
-  bSum.resize(N);
-  cSum.resize(N);
-  Xgrid = a.Xgrid;
-  Ygrid = a.Ygrid;
-  Zgrid = a.Zgrid;
-  UpToDate = a.UpToDate;
-  return (*this);
-}
-
-inline 
-MultiTricubicSpline& MultiTricubicSpline::operator=(MultiTricubicSpline a)
-{
-  F.resize(a.F.shape());
-  F = a.F;
-  Nx = a.Nx;
-  Ny = a.Ny;
-  Nz = a.Nz;
-  N  = a.N;
-  bSum.resize(N);
-  cSum.resize(N);
-  Xgrid = a.Xgrid;
-  Ygrid = a.Ygrid;
-  Zgrid = a.Zgrid;
-  UpToDate = a.UpToDate;
-  return (*this);
-}
-
-
-
-inline 
-void MultiTricubicSpline::Init (Grid *xgrid, Grid *ygrid, Grid *zgrid,
+template <int N> void 
+MultiTricubicSpline<N>::Init (Grid *xgrid, Grid *ygrid, Grid *zgrid,
 				const Array<double,4> &init)
 {
   cerr << "Got to MultiTricubicSpline::Init.\n";
@@ -177,18 +158,13 @@ void MultiTricubicSpline::Init (Grid *xgrid, Grid *ygrid, Grid *zgrid,
   assert (init.extent(0) == Nx);
   assert (init.extent(1) == Ny);
   assert (init.extent(2) == Nz);
-  N = init.extent(3);
-  cerr << "N = " << N << endl;
   
-  cSum.resize(N);
-  bSum.resize(N);
-
-  F.resize(Nx,Ny,Nz,8,N);
+  F.resize(Nx,Ny,Nz);
   for (int ix=0; ix<Nx; ix++)
     for (int iy=0; iy<Ny; iy++)
       for (int iz=0; iz<Nz; iz++)
 	for (int i=0; i<N; i++)
-	  F(ix,iy,iz,0,i) = init(ix,iy,iz,i);
+	  F(ix,iy,iz)(0,i) = init(ix,iy,iz,i);
   UpToDate = false;
   cerr << "After up2date.\n";
 }
@@ -196,9 +172,9 @@ void MultiTricubicSpline::Init (Grid *xgrid, Grid *ygrid, Grid *zgrid,
 
 
 
-inline void
-MultiTricubicSpline::operator() (double x, double y, double z,
-				 Array<double,1> &vals)
+template<int N> inline void
+MultiTricubicSpline<N>::operator() (double x, double y, double z,
+				 TinyVector<double,N> &vals)
 {
   if (!UpToDate)
     Update();
@@ -237,46 +213,46 @@ MultiTricubicSpline::operator() (double x, double y, double z,
     bSum = 0.0; 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-        cSum(i) += c0*F(ix,iy,iz  ,0,i);
+        cSum[i] += c0*F(ix,iy,iz)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy,iz+1,0,i);
+	cSum[i] += c1*F(ix,iy,iz+1)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy,iz  ,3,i);
+	cSum[i] += c2*F(ix,iy,iz)(3,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy,iz+1,3,i);
+	cSum[i] += c3*F(ix,iy,iz+1)(3,i);
     bSum += b0*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy+1,iz  ,0,i);
+	cSum[i] += c0*F(ix,iy+1,iz)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy+1,iz+1,0,i);
+	cSum[i] += c1*F(ix,iy+1,iz+1)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy+1,iz  ,3,i);
+	cSum[i] += c2*F(ix,iy+1,iz)(3,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy+1,iz+1,3,i);
+	cSum[i] += c3*F(ix,iy+1,iz+1)(3,i);
     bSum += b1*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy,iz  ,2,i);
+	cSum[i] += c0*F(ix,iy,iz)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy,iz+1,2,i);
+	cSum[i] += c1*F(ix,iy,iz+1)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy,iz  ,6,i);
+	cSum[i] += c2*F(ix,iy,iz)(6,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy,iz+1,6,i);
+	cSum[i] += c3*F(ix,iy,iz+1)(6,i);
     bSum += b2*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy+1,iz  ,2,i);
+	cSum[i] += c0*F(ix,iy+1,iz)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy+1,iz+1,2,i);
+	cSum[i] += c1*F(ix,iy+1,iz+1)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy+1,iz  ,6,i);
+	cSum[i] += c2*F(ix,iy+1,iz)(6,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy+1,iz+1,6,i);
+	cSum[i] += c3*F(ix,iy+1,iz+1)(6,i);
     bSum += b3*cSum;
   vals += a0*bSum;
 
@@ -285,46 +261,46 @@ MultiTricubicSpline::operator() (double x, double y, double z,
     bSum = 0.0; 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy,iz  ,0,i);
+	cSum[i] += c0*F(ix+1,iy,iz)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy,iz+1,0,i);
+	cSum[i] += c1*F(ix+1,iy,iz+1)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy,iz  ,3,i);
+	cSum[i] += c2*F(ix+1,iy,iz)(3,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy,iz+1,3,i);
+	cSum[i] += c3*F(ix+1,iy,iz+1)(3,i);
     bSum += b0*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy+1,iz  ,0,i);
+	cSum[i] += c0*F(ix+1,iy+1,iz)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy+1,iz+1,0,i);
+	cSum[i] += c1*F(ix+1,iy+1,iz+1)(0,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy+1,iz  ,3,i);
+	cSum[i] += c2*F(ix+1,iy+1,iz)(3,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy+1,iz+1,3,i);
+	cSum[i] += c3*F(ix+1,iy+1,iz+1)(3,i);
     bSum += b1*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy,iz  ,2,i);
+	cSum[i] += c0*F(ix+1,iy,iz)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy,iz+1,2,i);
+	cSum[i] += c1*F(ix+1,iy,iz+1)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy,iz  ,6,i);
+	cSum[i] += c2*F(ix+1,iy,iz)(6,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy,iz+1,6,i);
+	cSum[i] += c3*F(ix+1,iy,iz+1)(6,i);
     bSum += b2*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy+1,iz  ,2,i);
+	cSum[i] += c0*F(ix+1,iy+1,iz)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy+1,iz+1,2,i);
+	cSum[i] += c1*F(ix+1,iy+1,iz+1)(2,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy+1,iz  ,6,i);
+	cSum[i] += c2*F(ix+1,iy+1,iz)(6,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy+1,iz+1,6,i);
+	cSum[i] += c3*F(ix+1,iy+1,iz+1)(6,i);
     bSum += b3*cSum;
   vals += a1*bSum;
 
@@ -334,46 +310,46 @@ MultiTricubicSpline::operator() (double x, double y, double z,
     bSum = 0.0; 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy,iz  ,1,i);
+	cSum[i] += c0*F(ix,iy,iz)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy,iz+1,1,i);
+	cSum[i] += c1*F(ix,iy,iz+1)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy,iz  ,5,i);
+	cSum[i] += c2*F(ix,iy,iz)(5,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy,iz+1,5,i);
+	cSum[i] += c3*F(ix,iy,iz+1)(5,i);
     bSum += b0*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy+1,iz  ,1,i);
+	cSum[i] += c0*F(ix,iy+1,iz)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy+1,iz+1,1,i);
+	cSum[i] += c1*F(ix,iy+1,iz+1)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy+1,iz  ,5,i);
+	cSum[i] += c2*F(ix,iy+1,iz)(5,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy+1,iz+1,5,i);
+	cSum[i] += c3*F(ix,iy+1,iz+1)(5,i);
     bSum += b1*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy,iz  ,4,i);
+	cSum[i] += c0*F(ix,iy,iz)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy,iz+1,4,i);
+	cSum[i] += c1*F(ix,iy,iz+1)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy,iz  ,7,i);
+	cSum[i] += c2*F(ix,iy,iz)(7,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy,iz+1,7,i);
+	cSum[i] += c3*F(ix,iy,iz+1)(7,i);
     bSum += b2*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix,iy+1,iz  ,4,i);
+	cSum[i] += c0*F(ix,iy+1,iz)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix,iy+1,iz+1,4,i);
+	cSum[i] += c1*F(ix,iy+1,iz+1)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix,iy+1,iz  ,7,i);
+	cSum[i] += c2*F(ix,iy+1,iz)(7,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix,iy+1,iz+1,7,i);
+	cSum[i] += c3*F(ix,iy+1,iz+1)(7,i);
     bSum += b3*cSum;
   vals += a2*bSum;
 
@@ -384,46 +360,46 @@ MultiTricubicSpline::operator() (double x, double y, double z,
     bSum = 0.0; 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy,iz  ,1,i);
+	cSum[i] += c0*F(ix+1,iy,iz)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy,iz+1,1,i);
+	cSum[i] += c1*F(ix+1,iy,iz+1)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy,iz  ,5,i);
+	cSum[i] += c2*F(ix+1,iy,iz)(5,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy,iz+1,5,i);
+	cSum[i] += c3*F(ix+1,iy,iz+1)(5,i);
     bSum += b0*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy+1,iz  ,1,i);
+	cSum[i] += c0*F(ix+1,iy+1,iz)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy+1,iz+1,1,i);
+	cSum[i] += c1*F(ix+1,iy+1,iz+1)(1,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy+1,iz  ,5,i);
+	cSum[i] += c2*F(ix+1,iy+1,iz)(5,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy+1,iz+1,5,i);
+	cSum[i] += c3*F(ix+1,iy+1,iz+1)(5,i);
     bSum += b1*cSum;
 
       cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy,iz  ,4,i);
+	cSum[i] += c0*F(ix+1,iy,iz)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy,iz+1,4,i);
+	cSum[i] += c1*F(ix+1,iy,iz+1)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy,iz  ,7,i);
+	cSum[i] += c2*F(ix+1,iy,iz)(7,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy,iz+1,7,i);
+	cSum[i] += c3*F(ix+1,iy,iz+1)(7,i);
     bSum += b2*cSum;
 
     cSum = 0.0;
       for (int i=0; i<N; i++)
-	cSum(i) += c0*F(ix+1,iy+1,iz  ,4,i);
+	cSum[i] += c0*F(ix+1,iy+1,iz)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c1*F(ix+1,iy+1,iz+1,4,i);
+	cSum[i] += c1*F(ix+1,iy+1,iz+1)(4,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c2*F(ix+1,iy+1,iz  ,7,i);
+	cSum[i] += c2*F(ix+1,iy+1,iz)(7,i);
       for (int i=0; i<N; i++)
-	cSum(i) += c3*F(ix+1,iy+1,iz+1,7,i);
+	cSum[i] += c3*F(ix+1,iy+1,iz+1)(7,i);
     bSum += b3*cSum;
     vals += a3*bSum;
 
@@ -526,6 +502,183 @@ MultiTricubicSpline::operator() (double x, double y, double z,
 // //   }
 //   return (val);
 }
+
+
+
+
+
+template<int N> void 
+MultiTricubicSpline<N>::Update()
+{
+  for (int n=0; n<N; n++) {
+    // Do dF/dx
+    UpdateX(0, 1, n);
+    // Do dF/dy
+    UpdateY(0, 2, n);
+    // Do dF/dy
+    UpdateZ(0, 3, n);
+    // Do d2F/dxdy
+    UpdateY(1, 4, n);
+    // Do d2F/dxdz
+    UpdateZ(1, 5, n);
+    // Do d2F/dydz
+    UpdateZ(2, 6, n);
+    // Do d3F/dxdydz
+    UpdateZ(4, 7, n);
+  }
+  UpToDate=true;
+}
+
+template<int N> void 
+MultiTricubicSpline<N>::UpdateX(int source, int dest, int n)
+{
+  Grid &x = *Xgrid;
+  Array<double,1> mu(Nx);
+  
+  // Loop over all y and z
+  for (int iy=0; iy<Ny; iy++)
+    for (int iz=0; iz<Nz; iz++) {
+      // Set up tridiagonal set of equations
+      // Initialize RHS of equations
+      F(0,iy,iz)(dest,n) = 
+	1.5*(F(1,iy,iz)(source,n)-F(0,iy,iz)(source,n))/(x(1)-x(0));
+      F(Nx-1,iy,iz)(dest,n) = 
+	1.5*(F(Nx-1,iy,iz)(source,n)-F(Nx-2,iy,iz)(source,n))
+	/(x(Nx-1)-x(Nx-2));
+      mu(0) = 0.5;
+
+      // Solve tri-diagonal set of equations.  First eliminate lower
+      // elements.
+      for (int j=1; j<(Nx-1); j++) {
+	double lambda = 0.5*(x(j+1)-x(j))/(x(j+1)-x(j-1));
+	mu(j) = 0.5 - lambda;
+	F(j,iy,iz)(dest,n) = 
+	  3.0*(lambda*(F(j,iy,iz)(source,n)-
+		       F(j-1,iy,iz)(source,n))/(x(j)-x(j-1))+ 
+	       mu(j)*(F(j+1,iy,iz)(source,n)-F(j,iy,iz)(source,n))
+	       /(x(j+1)-x(j)));
+	double cj = 1.0 - lambda * mu(j-1);
+	F(j,iy,iz)(dest,n) -= lambda * F(j-1,iy,iz)(dest,n);
+	mu(j) /= cj;
+	F(j,iy,iz)(dest,n) /= cj;
+      }
+      
+      // Last element
+      int j = Nx-1;
+      double lambda = 0.5;
+      mu(j) = 0.5 - lambda;
+      F(j,iy,iz)(dest,n) = 
+	3.0*(lambda*(F(j,iy,iz)(source,n)-F(j-1,iy,iz)(source,n))/(x(j)-x(j-1)));
+      double cj = 1.0 - lambda * mu(j-1);
+      F(j,iy,iz)(dest,n) -= lambda * F(j-1,iy,iz)(dest,n);
+      mu(j) /= cj;
+      F(j,iy,iz)(dest,n) /= cj;
+      
+      // Now last d/dx is correct.  We proceed upward, back substituting.
+      for (j=Nx-2; j>=0; j--) {
+	F(j,iy,iz)(dest,n) -= mu(j) * F(j+1,iy,iz)(dest,n);
+      }
+    }      
+}
+
+
+template<int N> void 
+MultiTricubicSpline<N>::UpdateY(int source, int dest, int n)
+{
+  Grid &y = *Ygrid;
+  Array<double,1> mu(Ny);
+  
+  // Loop over all x and z
+  for (int ix=0; ix<Nx; ix++)
+    for (int iz=0; iz<Nz; iz++) {
+      // Set up tridiagonal set of equations
+      // Initialize RHS of equations
+      F(ix,0,iz)(dest,n) = 1.5*(F(ix,1,iz)(source,n)-F(ix,0,iz)(source,n))/(y(1)-y(0));
+      F(ix,Ny-1,iz)(dest,n) = 1.5*(F(ix,Ny-1,iz)(source,n)-F(ix,Ny-2,iz)(source,n))
+	/(y(Ny-1)-y(Ny-2));
+      mu(0) = 0.5;
+
+      // Solve tri-diagonal set of equations.  First eliminate lower
+      // elements.
+      for (int j=1; j<(Ny-1); j++) {
+	double lambda = 0.5*(y(j+1)-y(j))/(y(j+1)-y(j-1));
+	mu(j) = 0.5 - lambda;
+	F(ix,j,iz)(dest,n) = 
+	  3.0*(lambda*(F(ix,j,iz)(source,n)-F(ix,j-1,iz)(source,n))/(y(j)-y(j-1))+ 
+	       mu(j) *(F(ix,j+1,iz)(source,n)-F(ix,j,iz)(source,n))/(y(j+1)-y(j)));
+	double cj = 1.0 - lambda * mu(j-1);
+	F(ix,j,iz)(dest,n) -= lambda * F(ix,j-1,iz)(dest,n);
+	mu(j) /= cj;
+	F(ix,j,iz)(dest,n) /= cj;
+      }
+      
+      // Last element
+      int j = Ny-1;
+      double lambda = 0.5;
+      mu(j) = 0.5 - lambda;
+      F(ix,j,iz)(dest,n) = 
+	3.0*(lambda*(F(ix,j,iz)(source,n)-F(ix,j-1,iz)(source,n))/(y(j)-y(j-1)));
+      double cj = 1.0 - lambda * mu(j-1);
+      F(ix,j,iz)(dest,n) -= lambda * F(ix,j-1,iz)(dest,n);
+      mu(j) /= cj;
+      F(ix,j,iz)(dest,n) /= cj;
+      
+      // Now last d/dx is correct.  We proceed upward, back substituting.
+      for (j=Ny-2; j>=0; j--) {
+	F(ix,j,iz)(dest,n) -= mu(j) * F(ix,j+1,iz)(dest,n);
+      }
+    }      
+}
+
+
+template<int N> void 
+MultiTricubicSpline<N>::UpdateZ(int source, int dest, int n)
+{
+  Grid &z = *Zgrid;
+  Array<double,1> mu(Nz);
+  
+  // Loop over all x and y
+  for (int ix=0; ix<Nx; ix++)
+    for (int iy=0; iy<Ny; iy++) {
+      // Set up tridiagonal set of equations
+      // Initialize RHS of equations
+      F(ix,iy,0)(dest,n) = 1.5*(F(ix,iy,1)(source,n)-F(ix,iy,0)(source,n))/(z(1)-z(0));
+      F(ix,iy,Nz-1)(dest,n) = 1.5*(F(ix,iy,Nz-1)(source,n)-F(ix,iy,Nz-2)(source,n))
+	/(z(Nz-1)-z(Nz-2));
+      mu(0) = 0.5;
+
+      // Solve tri-diagonal set of equations.  First eliminate lower
+      // elements.
+      for (int j=1; j<(Nz-1); j++) {
+	double lambda = 0.5*(z(j+1)-z(j))/(z(j+1)-z(j-1));
+	mu(j) = 0.5 - lambda;
+	F(ix,iy,j)(dest,n) = 
+	  3.0*(lambda*(F(ix,iy,j)(source,n)-F(ix,iy,j-1)(source,n))/(z(j)-z(j-1))+ 
+	       mu(j) *(F(ix,iy,j+1)(source,n)-F(ix,iy,j)(source,n))/(z(j+1)-z(j)));
+	double cj = 1.0 - lambda * mu(j-1);
+	F(ix,iy,j)(dest,n) -= lambda * F(ix,iy,j-1)(dest,n);
+	mu(j) /= cj;
+	F(ix,iy,j)(dest,n) /= cj;
+      }
+      
+      // Last element
+      int j = Nz-1;
+      double lambda = 0.5;
+      mu(j) = 0.5 - lambda;
+      F(ix,iy,j)(dest,n) = 
+	3.0*(lambda*(F(ix,iy,j)(source,n)-F(ix,iy,j-1)(source,n))/(z(j)-z(j-1)));
+      double cj = 1.0 - lambda * mu(j-1);
+      F(ix,iy,j)(dest,n) -= lambda * F(ix,iy,j-1)(dest,n);
+      mu(j) /= cj;
+      F(ix,iy,j)(dest,n) /= cj;
+      
+      // Now last d/dx is correct.  We proceed upward, back substituting.
+      for (j=Nz-2; j>=0; j--) {
+	F(ix,iy,j)(dest,n) -= mu(j) * F(ix,iy,j+1)(dest,n);
+      }
+    }      
+}
+
 
 
 // inline double MultiTricubicSpline::d_dx (double x, double y, double z)
--- Splines/template.make
+++ Splines/template.make
@@ -1,12 +1,12 @@
-SOURCES = CubicSpline.cc Grid.cc BicubicSpline.cc TestBicubic.cc TestGrid.cc TestTricubic.cc MyTricubicSpline.cc TestMyTricubic.cc QuinticSpline.cc TestQuintic.cc DyutimanTest.cc MultiTricubicSpline.cc
+SOURCES = CubicSpline.cc Grid.cc BicubicSpline.cc TestBicubic.cc TestGrid.cc TestTricubic.cc MyTricubicSpline.cc TestMyTricubic.cc QuinticSpline.cc TestQuintic.cc DyutimanTest.cc MultiTricubicSpline.cc TestMultiTricubicSpline.cc TestMultiTricubicSpline2.cc
 
 IOobjs = ../IO/InputOutput.o ../IO/InputOutputHDF5.o ../IO/InputOutputASCII.o  ../IO/InputOutputXML.o
 
 F77Objs = fortran/evtricub.o  fortran/herm3ev.o  fortran/mktricubw.o  fortran/tcspline.o fortran/ibc_ck.o fortran/splinck.o fortran/zonfind.o fortran/tcspeval.o fortran/v_spline.o fortran/bcspline.o fortran/bcspeval.o
 
-Objs:	MyTricubicSpline.o CubicSpline.o QuinticSpline.o DyutimanTest.o MultiTricubicSpline.o
+Objs:	MyTricubicSpline.o CubicSpline.o QuinticSpline.o DyutimanTest.o MultiTricubicSpline.o MultiTricubicSpline2.o
 
-all:	FortranObjs TestBicubic TestGrid  TestMyTricubic Objs TestQuintic DyutimanTest TestMultiTricubicSpline
+all:	FortranObjs TestBicubic TestGrid  TestMyTricubic Objs TestQuintic DyutimanTest TestMultiTricubicSpline TestMultiTricubicSpline2
 
 TestQuintic:	QuinticSpline.o QuinticSplines.o TestQuintic.o
 	$(LD) -o TestQuintic QuinticSpline.o QuinticSplines.o TestQuintic.o $(LIBS)
@@ -19,6 +19,10 @@ TestMyTricubic:	CubicSpline.o Grid.o  TestMyTricubic.o MyTricubicSpline.o
 
 TestMultiTricubicSpline:	Grid.o  TestMultiTricubicSpline.o MyTricubicSpline.o MultiTricubicSpline.o
 	$(LD) -o TestMultiTricubicSpline Grid.o MultiTricubicSpline.o MyTricubicSpline.o TestMultiTricubicSpline.o $(IOobjs) $(LIBS)
+
+
+TestMultiTricubicSpline2:	Grid.o  TestMultiTricubicSpline2.o MyTricubicSpline.o
+	$(LD) -o TestMultiTricubicSpline2 Grid.o MyTricubicSpline.o TestMultiTricubicSpline2.o $(IOobjs) $(LIBS)
 
 
 DyutimanTest:	CubicSpline.o Grid.o  TestMyTricubic.o MyTricubicSpline.o
