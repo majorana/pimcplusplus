42c36542733fb75fb17457191cc77269686f2723
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-06-21 16:35:03 +0000

    Kinetic Action for a sphere h file

---
+++ src/Actions/KineticSphereClass.h
@@ -0,0 +1,23 @@
+#ifndef KINETIC_CLASS_H
+#define KINETIC_CLASS_H
+
+#include "ActionBase.h"
+
+/// The KineticClass calculates the kinetic part of the action.  This
+/// is the "spring term", of the form
+/// \f$ K \equiv \left(4\pi\lambda\tau\right)^{-\frac{ND}{2}} 
+/// \exp\left[-\frac{(R-R')^2}{4\lambda\tau}\right] \f$
+class KineticClass : public ActionBaseClass
+{
+  int NumImages;
+public:
+  void Read (IOSectionClass &in);
+  double Action (int slice1, int slice2, 
+		 const Array<int,1> &activeParticles, int level);
+  double d_dBeta (int slice1, int slice2, int level);
+  inline void SetNumImages (int num) { NumImages = num; }
+  KineticClass (PathDataClass &pathData);
+};
+
+
+#endif
