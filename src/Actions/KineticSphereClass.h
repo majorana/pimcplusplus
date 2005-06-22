afd7b97249e792fce34e5fc858c9d56a571d793f
khairall <khairall@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-06-22 18:37:48 +0000

    Changed kinetic sphere action to be correct

--- src/Actions/KineticSphereClass.h
+++ src/Actions/KineticSphereClass.h
@@ -1,13 +1,13 @@
-#ifndef KINETIC_CLASS_H
-#define KINETIC_CLASS_H
+#ifndef KINETIC_SPHERE_CLASS_H
+#define KINETIC_SPHERE_CLASS_H
 
 #include "ActionBase.h"
 
-/// The KineticClass calculates the kinetic part of the action.  This
+/// The KineticSphereClass calculates the kinetic part of the action.  This
 /// is the "spring term", of the form
 /// \f$ K \equiv \left(4\pi\lambda\tau\right)^{-\frac{ND}{2}} 
 /// \exp\left[-\frac{(R-R')^2}{4\lambda\tau}\right] \f$
-class KineticClass : public ActionBaseClass
+class KineticSphereClass : public ActionBaseClass
 {
   int NumImages;
 public:
@@ -16,7 +16,9 @@ public:
 		 const Array<int,1> &activeParticles, int level);
   double d_dBeta (int slice1, int slice2, int level);
   inline void SetNumImages (int num) { NumImages = num; }
-  KineticClass (PathDataClass &pathData);
+  KineticSphereClass (PathDataClass &pathData);
+  double K(int slice,int nextSlice,int ptcl,int level);
+
 };
 
 
