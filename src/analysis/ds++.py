42a783915b4dfb4902a546f4c65e39547337f058
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-09-02 18:10:35 +0000

    Added statistics box and drawing of mean line.

--- src/analysis/ds++.py
+++ src/analysis/ds++.py
@@ -219,7 +219,7 @@ class scalarClass:
         ax.hold(True)
         xMean = array([0.0, self.y.size()]) 
         yMean = array([self.mean, self.mean])
-        ax.plot(xMean, yMean, 'g')
+        ax.plot(xMean, yMean, 'g', 'LineWidth', 2.0)
         ax.set_title(self.name)
         ax.set_xlabel("block")
         ax.set_ylabel("Energy")
