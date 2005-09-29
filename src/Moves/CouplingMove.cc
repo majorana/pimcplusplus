a7f53247b5e266468e9b23d83de946ac4ce9e4ee
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-09-29 23:50:53 +0000

    Added coupling move

---
+++ src/Moves/CouplingMove.cc
@@ -0,0 +1,43 @@
+#include "CouplingMove.h"
+#include "BisectionStageClass.h"
+#include "CouplingStage.h"
+
+void CouplingMoveClass::WriteRatio()
+{
+  MultiStageClass::WriteRatio();
+  //Do nothing for now
+}
+
+void CouplingMoveClass::MakeMove()
+{
+
+  bool toAccept=true;
+  list<StageClass*>::iterator stageIter=Stages.begin();
+  double prevActionChange=0.0;
+  int stageCount=0;
+  while (stageIter!=Stages.end() && toAccept){
+    //    cerr<<"This is stage "<<stageCount<<endl;
+    toAccept = (*stageIter)->Attempt(Slice1,Slice2,
+				     ActiveParticles,prevActionChange);
+    stageIter++;
+    //    cerr<<endl;
+  }
+  if (toAccept)
+    MultiStageClass::Accept();
+  else 
+    MultiStageClass::Reject();
+}
+
+void CouplingMoveClass::Read(IOSectionClass &in)
+{
+  assert (in.ReadVar ("name", Name));
+  StageClass* coupleStage;
+  coupleStage=new CouplingStageClass(PathData,NumLevels,OutSection);
+  coupleStage->Read(in);
+  coupleStage->Actions.push_back(&PathData.Actions.Kinetic);
+  coupleStage->Actions.push_back(&PathData.Actions.ShortRange);
+  Stages.push_back (coupleStage);
+}
+
+
+
