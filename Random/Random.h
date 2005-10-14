8c9cabd6922f72baab830ef89b3995111b2fc680
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2005-10-14 21:00:02 +0000

    Added a WorldStream for all processors.  Checking new box-muller method.

--- Random/Random.h
+++ Random/Random.h
@@ -6,157 +6,123 @@
 
 class RandomClass
 {
-
 private:
   int NumClones;
   int CloneNumber;
-  int *LocalStream;
-  int *CommonStream;
-   CommunicatorClass &MyComm;
-   int NumCommon, NumLocal;
- public:
-   ///Produces a double that is common among all processors
-   inline double Common()
-   { 
-     double ranNum=sprng(CommonStream);  
-     //    cerr<<"My random number(" << NumCommon << ") is " << ranNum 
-     //	<< " and the stream is " << CommonStream 
-     //	<<" and the proce num is " << MyComm.MyProc();
-     //    cerr << "  NumLocal = " << NumLocal << endl;
-     NumCommon++;
-     return ranNum;
-     //    return sprng(CommonStream);     
-   }
+  int *LocalStream, *CommonStream, *WorldStream;
+  CommunicatorClass &MyComm;
+  int NumCommon, NumLocal, NumWorld;
+public:
+  ///Produces a double that is unique to your processor
+  inline double Local()
+  { NumLocal++;  return sprng(LocalStream);  }
 
-   ///Produces a double that is unique to your processor
-   inline double Local()
-   {   
-     NumLocal++;
-     return sprng(LocalStream); 
-   }
+  ///Produces a double that is common among this clone
+  inline double Common()
+  { NumCommon++; return sprng(CommonStream); }
+    
+  ///Produces a double that is common to all processors
+  inline double World()
+  { NumWorld++;  return sprng(WorldStream);  }
+  
+  ///Produces an int that is unique to your processor between 0 and max
+  inline int LocalInt(int max)
+  { return (int)floor(Local()*max); }
+  inline int CommonInt(int max)
+  { return (int)floor(Common()*max); }
+  inline int WorldInt(int max)
+  { return (int)floor(World()*max); }
+  
+  /// normal random variate generator 
+  /// Returns a random number distributed according to
+  /// P(x) = 1/sqrt(2*pi * sigma) * exp (x^2 /(2*sigma^2))
+  inline double LocalGaussian  (double sigma);
+  inline double LocalGaussian2 (double sigma);
+  inline double CommonGaussian (double sigma);
+  inline double WorldGaussian  (double sigma);
+  
+  /// Produces a guassian random vector with radius that has STD sigma
+  inline void LocalGaussianVec (double sigma, Vec2 &c);
+  inline void CommonGaussianVec(double sigma, Vec2 &c);
+  inline void WorldGaussianVec (double sigma, Vec2 &c);
 
-   ///Produces an int that is unique to your processor between 0 and max
-   inline int LocalInt(int max)
-   { return (int)floor(Local()*max); }
-
-   /* normal random variate generator */
-   // Returns a random number distributed according to
-   // P(x) = 1/sqrt(2*pi * sigma) * exp ((x - mean)^2 /(2*sigma^2))
-   inline double LocalGaussian(double sigma);
-
-
-   /// Produces a guassian random vector with radius that has STD sigma
-   inline void LocalGaussianVec(double sigma, Vec3 &c)
-   { 
-     c(0)=LocalGaussian(sigma); 
-     c(1)=LocalGaussian(sigma); 
-     c(2)=LocalGaussian(sigma); 
-   }
-
-   /// Produces a guassian random vector with radius that has STD sigma 
-   inline void LocalGaussianVec(double sigma,Vec2 &c)
-   { c(0)=LocalGaussian(sigma); c(1)=LocalGaussian(sigma); }
-
-   inline double CommonGaussian(double sigma);
-   /// Produces a guassian random vector with radius that has STD sigma
-   inline void CommonGaussianVec(double sigma, Vec3 &c)
-   { 
-     c(0)=CommonGaussian(sigma); c(1)=CommonGaussian(sigma); c(2)=CommonGaussian(sigma); 
-   }
-
-   /// Produces a guassian random vector with radius that has STD sigma 
-   inline void CommonGaussianVec(double sigma,Vec2 &c)
-   { 
-     c(0)=CommonGaussian(sigma); c(1)=CommonGaussian(sigma); 
-   }
-
-
-   ///Produces an int between 0 and max (not including max) that is
-   ///common among processors  
-   inline int CommonInt(int max)
-   { 
-     return (int)floor(Common()*max); 
-   }
-
-
-   void Init()
-   {
-     int seed = make_sprng_seed();
-     //cerr<<"My seed is "<<seed<<endl;
-     Init (seed);
-   }
-
-   int make_new_seed()
-     {
-
-       time_t tp;
-       struct tm *temp;
-       unsigned int temp2, temp3;
-       static unsigned int temp4 = 0xe0e1;
-
-       time(&tp);
-       temp = localtime(&tp);
-
-       temp2 = (temp->tm_sec<<26)+(temp->tm_min<<20)+(temp->tm_hour<<15)+
-	 (temp->tm_mday<<10)+(temp->tm_mon<<6);
-       temp3 = (temp->tm_year<<13)+(temp->tm_wday<<10)+(temp->tm_yday<<1)+
-	 temp->tm_isdst;
-       temp2 ^= clock()^temp3;
-
-       temp4 = (temp4*0xeeee)%0xffff;
-       temp2 ^= temp4<<16;
-       temp4 = (temp4*0xaeee)%0xffff;
-       temp2 ^= temp4;
-
-       temp2 &= 0x7fffffff;
-
-       return temp2;
-     }
-
-   int InitWithRandomSeed(int numClones=1)
-     {
-       int seed;
-       if (MyComm.MyProc()==0){
-	 seed = make_new_seed();
-	 cerr<<"THE RANDOM SEED IS "<<seed<<endl;
-       }
-       MyComm.Broadcast(0,seed);
-       Init (seed, numClones);
-       return seed;
-     }
-
-
-
+  inline void LocalGaussianVec (double sigma, Vec3 &c);
+  inline void CommonGaussianVec(double sigma, Vec3 &c);
+  inline void WorldGaussianVec (double sigma, Vec3 &c);
+      
+  void Init()
+  {
+    int seed = make_sprng_seed();
+    //cerr<<"My seed is "<<seed<<endl;
+    Init (seed);
+  }
+  
+  int make_new_seed()
+  {
+    
+    time_t tp;
+    struct tm *temp;
+    unsigned int temp2, temp3;
+    static unsigned int temp4 = 0xe0e1;
+    
+    time(&tp);
+    temp = localtime(&tp);
+    
+    temp2 = (temp->tm_sec<<26)+(temp->tm_min<<20)+(temp->tm_hour<<15)+
+      (temp->tm_mday<<10)+(temp->tm_mon<<6);
+    temp3 = (temp->tm_year<<13)+(temp->tm_wday<<10)+(temp->tm_yday<<1)+
+      temp->tm_isdst;
+    temp2 ^= clock()^temp3;
+    
+    temp4 = (temp4*0xeeee)%0xffff;
+    temp2 ^= temp4<<16;
+    temp4 = (temp4*0xaeee)%0xffff;
+    temp2 ^= temp4;
+    
+    temp2 &= 0x7fffffff;
+    
+    return temp2;
+  }
+  
+  int InitWithRandomSeed(int numClones=1)
+  {
+    int seed;
+    if (MyComm.MyProc()==0){
+      seed = make_new_seed();
+      cerr<<"THE RANDOM SEED IS "<<seed<<endl;
+    }
+    MyComm.Broadcast(0,seed);
+    Init (seed, numClones);
+    return seed;
+  }
+  
+  
   void Init(int seed, int numClones=1)
   {
     NumClones=numClones;
     int myProc=MyComm.MyProc();
     int numProcs=MyComm.NumProcs();
     int procsPerClone=numProcs/NumClones;
-    CloneNumber=myProc/procsPerClone;
-    int commID=numProcs+CloneNumber;
-//     cerr<<"my data is "
-// 	<<"my proc: "<<myProc<<" "
-// 	<<"num clones: "<<NumClones<<" "
-// 	<<"num procs: "<<numProcs<<" "
-// 	<<"procs per clone: "<<procsPerClone<<" "
-// 	<<"clone number: "<<CloneNumber<<" "
-// 	<<"commID: "<<commID<<endl;
+    CloneNumber = myProc/procsPerClone;
+    int commID  = numProcs+CloneNumber;
+    int worldID = numProcs+NumClones;
     ///LocalStream is a stream of random numbers unique to the node
     LocalStream = 
-      init_sprng(SPRNG_DEFAULT, myProc,numProcs+NumClones,seed,SPRNG_DEFAULT);
-    ///CommonStream is a stream of random numbers shared between all
-    ///processors that are on the same clone 
+      init_sprng(SPRNG_DEFAULT,myProc,numProcs+NumClones+1,seed,SPRNG_DEFAULT);
+    /// CommonStream is a stream of random numbers shared between all
+    /// processors that are on the same clone 
     CommonStream = 
-      init_sprng(SPRNG_DEFAULT, commID,numProcs+NumClones,seed,SPRNG_DEFAULT);
-
+      init_sprng(SPRNG_DEFAULT,commID,numProcs+NumClones+1,seed,SPRNG_DEFAULT);
+    /// World Stream is a stream of random numbers shared between all
+    /// processors in MPICommWorld
+    WorldStream =  init_sprng(SPRNG_DEFAULT,worldID,numProcs+NumClones+1,
+			      seed,SPRNG_DEFAULT);
   }
-
 
   RandomClass(CommunicatorClass &comm) : 
     MyComm(comm), NumCommon(0), NumLocal(0)
   {
-
+    
   }
 };
 
@@ -187,7 +153,33 @@ inline double RandomClass::LocalGaussian(double sigma)
 }
 
 
-inline double RandomClass::CommonGaussian(double sigma)
+inline double RandomClass::LocalGaussian2(double sigma)
+{                                      
+  double X1;
+  static double X2;
+  static int OneLeft = 0;
+  double temp;
+
+  if (OneLeft) {                   // Use second number from last computation
+    X1 = X2;
+    OneLeft = 0;
+  }
+  else {
+    double v1 = Local();
+    double v2 = Local();
+    double mag = sqrt(-2.0*log(v1));
+    double s, c;
+    sincos(2.0*M_PI*v2, &s, &c);
+    X1 = mag * c;
+    X2 = mag * s;
+    OneLeft = 1;
+  }  
+  return( X1 * sigma );
+}
+
+
+inline double 
+RandomClass::CommonGaussian(double sigma)
 {                      
   double V1, V2, S, X1;
   static double X2;
@@ -212,5 +204,80 @@ inline double RandomClass::CommonGaussian(double sigma)
   }  
   return( X1 * sigma );
 }
+
+inline double 
+RandomClass::WorldGaussian(double sigma)
+{                      
+  double V1, V2, S, X1;
+  static double X2;
+  static int OneLeft = 0;
+  double temp;
+
+  if (OneLeft) {                   // Use second number from last computation
+    X1 = X2;
+    OneLeft = 0;
+  }
+  else {
+    do {
+      V1 = 2.0 * World() - 1.0;
+      V2 = 2.0 * World() - 1.0;
+      S = V1 * V1 + V2 * V2;
+    } while ( S >= 1.0 );
+    
+    temp = sqrt( (-2.0 * log((double) S ) ) / S );
+    X1 = V1 * temp;
+    X2 = V2 * temp;
+    OneLeft = 1;
+  }  
+  return( X1 * sigma );
+}
+
+inline void 
+RandomClass::LocalGaussianVec(double sigma, Vec3 &c)
+{ 
+  c(0)=LocalGaussian(sigma); 
+  c(1)=LocalGaussian(sigma); 
+  c(2)=LocalGaussian(sigma); 
+}
+
+inline void 
+RandomClass::CommonGaussianVec(double sigma, Vec3 &c)
+{ 
+  c(0)=CommonGaussian(sigma); 
+  c(1)=CommonGaussian(sigma); 
+  c(2)=CommonGaussian(sigma); 
+}
+
+inline void 
+RandomClass::WorldGaussianVec(double sigma, Vec3 &c)
+{ 
+  c(0)=WorldGaussian(sigma); 
+  c(1)=WorldGaussian(sigma); 
+  c(2)=WorldGaussian(sigma); 
+}
+
+/// Produces a guassian random vector with radius that has STD sigma 
+inline void 
+RandomClass::LocalGaussianVec(double sigma,Vec2 &c)
+{ 
+  c(0)=LocalGaussian(sigma); 
+  c(1)=LocalGaussian(sigma); 
+}
+ 
+inline void 
+RandomClass::CommonGaussianVec(double sigma,Vec2 &c)
+{ 
+  c(0)=CommonGaussian(sigma); 
+  c(1)=CommonGaussian(sigma); 
+}
+
+inline void 
+RandomClass::WorldGaussianVec(double sigma,Vec2 &c)
+{ 
+  c(0)=WorldGaussian(sigma); 
+  c(1)=WorldGaussian(sigma); 
+}
+
+
 
 #endif
--- Random/TestRandom.cc
+++ Random/TestRandom.cc
@@ -1,4 +1,31 @@
 #include "Random.h"
+#include <time.h>
+
+void SpeedTest()
+{
+  CommunicatorClass comm;
+  comm.SetWorld();
+  RandomClass rand(comm);
+  rand.Init(314159);
+  
+  clock_t start1, end1, start2, end2;
+  start1 = clock();
+  for (int i=0; i<10000000; i++) 
+    double d = rand.LocalGaussian(1.0);
+  end1 = clock();
+
+  start2 = clock();
+  for (int i=0; i<10000000; i++) 
+    double d = rand.LocalGaussian2(1.0);
+  end2 = clock();
+
+  cerr << "LocalGaussian time:   " << 
+    (double)(end1-start1)/(double)CLOCKS_PER_SEC << endl;
+  cerr << "LocalGaussian2 time:  " << 
+    (double)(end2-start2)/(double)CLOCKS_PER_SEC << endl;
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -22,6 +49,7 @@ int main(int argc, char** argv)
   cerr << "Proc " << comm.MyProc() << " common random num = " 
        << rand.Common() << endl;
 
+  SpeedTest();
 #ifdef USE_MPI
   MPI_Finalize();
 #endif
