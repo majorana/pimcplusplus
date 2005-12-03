4821576141d66e8b696f04d1b85e1f24bf1f3feb
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-12-03 02:43:09 +0000

    Added NoPermuteStage.h

---
+++ src/Moves/NoPermuteStage.h
@@ -0,0 +1,31 @@
+#ifndef NO_PERMUTE_STAGE_CLASS_H
+#define NO_PERMUTE_STAGE_CLASS_H
+
+#include "PermuteStage.h"
+#include "MultiStage.h"
+#include "PermuteTableClass.h"
+#include "../Observables/ObservableVar.h"
+
+class NoPermuteStageClass : public PermuteStageClass
+{
+private:
+  int ChooseParticle();
+  int NumLevels;
+  int NumMoves, NumAccepted;
+public:
+  double Sample (int &slice1, int &slice2,
+		 Array<int,1> &activeParticles);
+  bool Attempt (int &slice1, int &slice2, 
+		 Array<int,1> &activeParticles, double &prevActionChange);
+  NoPermuteStageClass (PathDataClass &pathData, int speciesNum, int numLevels,
+		       IOSectionClass &outSection) 
+    : PermuteStageClass(pathData, speciesNum, numLevels,outSection)
+  {
+    // do nothing for now
+  }
+};
+
+
+
+
+#endif
