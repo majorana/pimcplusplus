b49d79d0fca6b5c977c0e4117e5256cd4eae440d
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-12-05 17:57:56 +0000

    Committing Writedata

---
+++ src/WriteData.h
@@ -0,0 +1,31 @@
+#ifndef WRITE_DATA_CLASS_H
+#define WRITE_DATA_CLASS_H
+
+#include "EventClass.h"
+class ObservableClass;
+class MoveClass;
+
+
+/// Note:  LoopClass is not allow to write any output to its out
+/// IOsection. 
+class WriteDataClass : public EventClass 
+{
+protected:
+  list<MoveClass*> &Moves;
+  list<ObservableClass*> &Observables;
+ public:
+  void DoEvent();
+  void Read(IOSectionClass &IO);
+  WriteDataClass(PathDataClass &pathData, IOSectionClass &out,
+	    list<MoveClass*> &moves,
+	    list<ObservableClass*> &observables) : 
+    EventClass(pathData, out),
+    Moves(moves), Observables(observables)
+  {
+    
+  }
+};
+
+
+
+#endif
