8a5c842c1121d608d76d39a4b851b493c734aa8b
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2005-12-13 18:02:44 +0000

    Added disks to close sphere correctly.

---
+++ src/Visual/DiskObject.h
@@ -0,0 +1,27 @@
+#ifndef DISK_OBJECT_H
+#define DISK_OBJECT_H
+
+#include "GLObject.h"
+#include <Common/Blitz.h>
+
+class DiskObject : public GLObject
+{
+protected:
+  Vec3 Color, Pos;
+  int Axis;
+  double Radius;
+  static int DiskListNum, OffScreenListNum;
+  static bool DiskListCreated, OffScreenListCreated;
+  void Set();
+  bool OffScreen;
+public:
+  void SetPos (Vec3 pos);
+  void SetAxis (int axis);
+  void SetRadius (double radius);
+  void SetColor (Vec3 color);
+  void DrawPOV (FILE *fout, string rotString);
+  DiskObject(bool offScreen=false);
+};
+
+
+#endif
