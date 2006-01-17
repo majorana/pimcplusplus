ac98d32ba0ecb6b17869e3fd171667b51ce45621
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-01-17 19:27:18 +0000

    Added force covariance calculation.

--- src/Moves/LangevinMove.cc
+++ src/Moves/LangevinMove.cc
@@ -1,15 +1,192 @@
 #include "LangevinMove.h"
+#include <Common/MatrixOps/MatrixOps.h>
 
+
+/// AccumForces adds the forces for my section of the path to the
+/// deque of forces.
 void
 LangevinMoveClass::AccumForces()
 {
-  PathData.Actions.GetForces(Particles, FShortSum, FLongSum);
+  FShort = dVec(0.0);
+  FLong  = dVec(0.0);
+  PathData.Actions.GetForces(Particles, FShort, FLong);
+  FShortSum += FShort;
+  FLongSum  += FLong;
+  /// Queue the forces for the computation of the variance.  Note that
+  /// the copy is very important since the normal blitz copy constructor
+  /// operator references the array data.
+  FTmp += FShort + FLong;
+  FDeque.push_back(FTmp.copy());
+}
+
+void 
+Stats (Array<double,1> &x, double &mean, double &var, double &kappa)
+{
+  int N = x.size();
+  double ninv = 1.0/(N);
+  double xsum, x2sum;
+  xsum = x2sum = 0.0;
+  for (int i=0; i < N; i++) {
+    xsum  += x(i);
+    x2sum += x(i)*x(i);
+  }
+  mean = xsum * ninv;
+  var = x2sum/(double)(N-1) - mean*mean;
+
+  /// Compute autocorrelation time, kappa
+  kappa = 1.0;
+  bool done = false;
+  int i = 1;
+  while (!done) {
+    double c_i = 0.0;
+    for (int j=0; j<(N-i); j++)
+      c_i += (x(j)-mean) * (x(j+i)-mean);
+    c_i /= (var*(double)(N-i));
+    i++;
+    kappa += c_i;
+    done = (i>=N) || (c_i<=0.0);
+  }
+}
+
+
+
+void
+LangevinMoveClass::CalcCovariance()
+{
+//   if (FDeque.size() < 20) {
+//     A = 0.0;
+//     for (int i=0; i<A.rows(); i++)
+//       A(i,i) = 1.0e-5;
+//     return;
+//   }
+  int N = R.size();
+  double Ninv = 1.0/(double)N;
+
+  /// First, sum the forces over the processors in my clone.
+  for (int i=0; i<FDeque.size(); i++) {
+    PathData.IntraComm.AllSum(FDeque[i], FTmp);
+    FDeque[i] = FShort;
+  }
+
+  Fmean = 0.0;
+  /// Compute mean force
+  int n=0;
+  for (int k=0; k<FDeque.size(); k++)
+    for (int ptcl=0; ptcl<N; ptcl++) 
+      for (int dim=0; dim<NDIM; dim++) {
+	Fmean(n) += FDeque[k](ptcl)[dim];
+	n++;
+      }
+  Fmean = (1.0/(double)FDeque.size())*Fmean;
+
+  
+  CoVar = 0.0;
+  /// Now, calculate the covariance and 
+  for (int k=0; k<FDeque.size(); k++) {
+    int i=0;
+    for (int ptcl1=0; ptcl1<N; ptcl1++) 
+      for (int dim1=0; dim1<NDIM; dim1++) {
+	int j=0;
+	for (int ptcl2=0; ptcl2<N; ptcl2++)
+	  for (int dim2=0; dim2<NDIM; dim2++) {
+	    CoVar(i,j) += 
+	      (FDeque[k](ptcl1)[dim1] * FDeque[k](ptcl2)[dim2]);
+	    j++;
+	  }
+	i++;
+      }
+  }
+  CoVar = (1.0/(double)(FDeque.size()-1))*CoVar;
+  /// Subtract <F(i)><F(j)>
+  for (int i=0; i<CoVar.rows(); i++)
+    for (int j=0; j<CoVar.cols(); j++)
+      CoVar(i,j) -= Fmean(i)*Fmean(j);
+
+  /// Compute the average autocorrelation time for the diagaonal
+  /// elements only.
+  Array<double,1> x(FDeque.size());
+  double tmpMean, tmpVar, tmpKappa;
+  kappa = 0.0;
+  for (int ptcl=0; ptcl<N; ptcl++)
+    for (int dim=0; dim<NDIM; dim++) {
+      for (int k=0; k<x.size(); k++) 
+	x(k) = FDeque[k](ptcl)(dim);
+      Stats (x, tmpMean, tmpVar, tmpKappa);
+      kappa += tmpKappa;
+    }
+  kappa /= (double)(N*NDIM);
+
+  if (PathData.IntraComm.MyProc()==0)
+    cerr << "CloneNum = " << PathData.GetCloneNum()
+	 << " and kappa = " << kappa << endl;
+
+  /// Multiply covariance by kappa
+  CoVar *= kappa;
+
+  /// Now we have the covariance matrix for the clone.  We must sum
+  /// over all the clones and divide by the number
+  if (PathData.IntraComm.MyProc() == 0) {
+    int numClones = PathData.GetNumClones();
+
+    PathData.InterComm.AllSum(Fmean, FallSum);
+    Fmean = 1.0/(double)numClones *FallSum;
+
+    PathData.InterComm.AllSum(CoVar, A);
+    double beta = PathData.Path.tau * PathData.Path.TotalNumSlices;
+    A *= 0.5*TimeStep*beta/(Mass*(double)(numClones*numClones));
+    /// Now calculate eigenvalues and eigenvectors
+    SymmEigenPairs (A, A.extent(0), Lambda, L);
+  }
+  /// Now, broadcast eigenvectors and values to all processors
+  PathData.IntraComm.Broadcast(0, L);
+  PathData.IntraComm.Broadcast(0, Lambda);
+  PathData.IntraComm.Broadcast(0, Fmean);
+
+  /// Transpose eigenvectors
+  for (int i=0; i<L.rows(); i++)
+    for (int j=0; j<L.cols(); j++)
+      Ltrans(i,j) = L(j,i);
+
+  /// Now empty out the force deque for next time
+  FDeque.clear();
+
+}
+
+
+
+
+/// Friction sets
+void
+LangevinMoveClass::CalcFriction()
+{
+  if (NumFs < 20) {
+    Lambda = 1.0e-5;
+    L      = 0.0;
+    Ltrans = 0.0;
+    for (int i=0; i<L.extent(0); i++) {
+      L(i,i) = 1.0;
+      Ltrans(i,i) = 1.0;
+    }
+  }
+  else {
+    double ninv = 1.0/NumFs;
+    double beta = PathData.Path.tau * PathData.Path.TotalNumSlices;
+    double norm = 0.5*beta*Mass;
+    for (int i=0; i<A.extent(0); i++)
+      for (int j=0; j<A.extent(1); j++)
+	A(i,j) = (ninv*(FF(i,j) - ninv*Fmean(i)*Fmean(j)));
+    SymmEigenPairs(A, A.extent(0), Lambda, L);
+    for (int i=0; i<L.extent(0); i++)
+      for (int j=0; j<L.extent(1); i++)
+	Ltrans(i,j) = L(j,i);
+  }
 }
 
 
 void
 LangevinMoveClass::LDStep()
 {
+  CalcCovariance();
   /// Write out positions and velocities
   TimeVar.Write(Time);
   for (int i=0; i<R.size(); i++) {
@@ -68,6 +245,22 @@ LangevinMoveClass::LDStep()
     FShortSum(i) = zero;
     FLongSum(i) = zero;
   }
+
+  /// Accumulate statistics for covariance matrices.
+  for (int i=0; i<FF.extent(0); i++) {
+    int m1 = i/NDIM;
+    int m2 = i%NDIM;
+    Fmean(i) += OldFShort(m1)[m2] + OldFLong(m1)[m2];
+    for (int j=0; j<FF.extent(1); j++) {
+      int n1 = j/NDIM;
+      int n2 = j%NDIM;
+      FF(i,j) += ( (OldFShort(m1)[m2] + OldFLong(m1)[m2]) * 
+		   (OldFShort(n1)[n2] + OldFLong(n1)[n2]) );
+    }
+  }
+  NumFs++;
+
+
   // Put x(t+2dt) into the Path so we can start accumulating forces
   // for the next step
   int first = PathData.Path.Species(LDSpecies).FirstPtcl;
@@ -113,26 +306,38 @@ LangevinMoveClass::Read(IOSectionClass &in)
 {
   string speciesStr;
 
-  assert(in.ReadVar("Mass", Mass));
-  assert (in.ReadVar("TimeStep", TimeStep));
+  assert (in.ReadVar("Mass",          Mass));
+  assert (in.ReadVar("TimeStep",      TimeStep));
   assert (in.ReadVar("NumEquilSteps", NumEquilSteps));
   assert (in.ReadVar("NumAccumSteps", NumAccumSteps));
-  assert (in.ReadVar("Species", speciesStr));
+  assert (in.ReadVar("Species",       speciesStr));
 
   LDSpecies = PathData.Path.SpeciesNum(speciesStr);
   assert (LDSpecies != -1);
   SpeciesClass &species = PathData.Path.Species(LDSpecies);
   int numPtcls = species.NumParticles;
-  V.resize(numPtcls);
-  R.resize(numPtcls);
-  FShortSum.resize(numPtcls);
-  FLongSum.resize(numPtcls);
-  FShortTmp.resize(numPtcls);
-  FLongTmp.resize(numPtcls);
-  OldFShort.resize(numPtcls);
-  OldFLong.resize(numPtcls);
-  Particles.resize(numPtcls);
+  V.resize         (numPtcls);
+  R.resize         (numPtcls);
+  FShort.resize    (numPtcls);
+  FLong.resize     (numPtcls);
+  FTmp.resize      (numPtcls);
+  FShortSum.resize (numPtcls);
+  FLongSum.resize  (numPtcls);
+  FShortTmp.resize (numPtcls);
+  FLongTmp.resize  (numPtcls);
+  OldFShort.resize (numPtcls);
+  OldFLong.resize  (numPtcls);
+  Particles.resize (numPtcls);
   WriteArray.resize(numPtcls,NDIM);
+  Fmean.resize  (NDIM*numPtcls);
+  FallSum.resize(NDIM*numPtcls);
+  FF.resize     (NDIM*numPtcls, NDIM*numPtcls);
+  A.resize      (NDIM*numPtcls, NDIM*numPtcls);
+  CoVar.resize  (NDIM*numPtcls, NDIM*numPtcls);
+  L.resize      (NDIM*numPtcls, NDIM*numPtcls);
+  Ltrans.resize (NDIM*numPtcls, NDIM*numPtcls);
+  Lambda.resize (NDIM*numPtcls);
+  NumFs = 0;
 
   dVec zero(0.0);
   for (int i=0; i<numPtcls; i++) {
@@ -167,7 +372,7 @@ void LangevinMoveClass::InitVelocities()
   }
   // Now, remove center-of-mass velocity
   double Esum = 0.0;
-  Vsum = Vsum/(double)V.size();
+  Vsum = (1.0/(double)V.size())*Vsum;
   for (int i=0; i<V.size(); i++) {
     V(i) -= Vsum;
     Esum    += 0.5 * Mass * dot(V(i), V(i));
@@ -179,7 +384,7 @@ void LangevinMoveClass::InitVelocities()
   for (int i=0; i<V.size(); i++)
     V(i) = norm * V(i);
 
-  /// Double-check that we did this write
+  /// Double-check that we did this right
   for (int i=0; i<NDIM; i++)
     Vsum[i] = 0.0;
   Esum = 0.0;
--- src/Moves/LangevinMove.h
+++ src/Moves/LangevinMove.h
@@ -2,12 +2,41 @@
 #define LANGEVIN_MOVE_H
 
 #include "MoveBase.h"
+#include <deque>
 
 class LangevinMoveClass : public MoveClass
 {
 protected:
   Array<dVec,1> V, R, OldFShort, OldFLong, 
     FShortSum, FLongSum, FShortTmp, FLongTmp;
+
+  ///////////////////////////
+  // Friction calculations //
+  ///////////////////////////
+  /// These stores a deque of the force evaluations during the
+  /// accumlation stage of the present ion move.
+  Array<dVec,1> FShort, FLong, FTmp;
+  deque<Array<dVec,1> > FDeque;
+  /// This stores the current friction coefficient matrix
+  Array<double,2> A, CoVar;
+  /// L stores the eigenvectors of A.  Ltran is its transpose
+  Array<double,2> L, Ltrans;
+  /// This stores the eigenvalues of A;
+  Array<double,1> Lambda;
+  /// FF stores <F_i F_j>.  This is used in computing the covariance
+  /// matrix for the forces, which in turn is used to compute friction
+  /// coefficents.  
+  Array<double,2> FF;
+  /// This stores the long-averaged means
+  Array<double,1> Fmean, FallSum;
+  /// Kappa is the autocorrelation time for the force calculation
+  double kappa;
+  /// This stores the number of means we have summed.
+  int NumFs;
+  void CalcFriction();
+  void CalcCovariance();
+
+
   /// The species we are doing Langevin dynamics on
   int LDSpecies;
   /// This holds the list of particles with which we're doing dynamics
