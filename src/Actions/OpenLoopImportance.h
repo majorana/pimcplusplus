ece0ce8b8af89271fb10c7105cf7b7cb2253d67d
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-03-03 05:35:35 +0000

    Made importance function much saner

--- src/Actions/OpenLoopImportance.h
+++ src/Actions/OpenLoopImportance.h
@@ -3,17 +3,24 @@
 
 #include "ActionBase.h"
 
-typedef enum {NOIMP,DISTIMP,DISPXIMP,CONSTSHIFT,TUNEDFUNCTION,RETUNEDFUNCTION} SampleChoice;
+typedef enum {NOIMP,DISTIMP,DISPXIMP,CONSTSHIFT,TUNEDFUNCTION,RETUNEDFUNCTION, POLYNOMIAL,EXPONENTIAL} SampleChoice;
+
+typedef enum {MIN_IMAGE_DISP,MIN_IMAGE_DIST,DISP,DIST} Xvalue;
 
 class OpenLoopImportanceClass : public ActionBaseClass
 {
 public:
+  Array<double,1> Polynom;
+  double a;
+  double alpha;
+  double s;
   void Read (IOSectionClass &in);
   double SingleAction (int slice1, int slice2, 
 		       const Array<int,1> &activeParticles, int level);
   double d_dBeta (int slice1, int slice2, int level);
   OpenLoopImportanceClass (PathDataClass &pathData);
   SampleChoice ImpChoice;
+  Xvalue Xis;
   double Shift;
 };
 
