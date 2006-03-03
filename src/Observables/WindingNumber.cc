cb7988404d8fce4c1ecd8c3cbc843f7439bac039
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-03-03 05:29:36 +0000

    I think I fixed the winding number but still not verified fully

--- src/Observables/WindingNumber.cc
+++ src/Observables/WindingNumber.cc
@@ -8,23 +8,31 @@
 ///a specific species
 void WindingNumberClass::Accumulate()
 {
+  NumSamples++;
+ //Move the join to the end so we don't have to worry about permutations
+  PathData.MoveJoin(PathData.NumTimeSlices()-1);
+
   TotalDisp=0.0;
+  TempDisp=0.0;
   int numLinks=PathData.Path.NumTimeSlices()-1;
   for (int ptcl=0;ptcl<PathData.Path.NumParticles();ptcl++){
     for (int slice=0;slice<numLinks;slice++) {
-      NumSamples++;
       dVec disp;
       disp=PathData.Path.Velocity(slice,slice+1,ptcl);
-      TotalDisp(ptcl) =TotalDisp(ptcl)+ disp;
+      //      TotalDisp(ptcl) =TotalDisp(ptcl)+ disp*PathData.Path.tau;
+      TotalDisp(0) =TotalDisp(0)+ disp;
     }
   }
   PathData.Path.Communicator.Sum(TotalDisp,TempDisp);
+  cerr<<"My tempDisp is "<<TempDisp(0)<<endl;
+  TempDisp=TempDisp; //*PathData.Path.tau;
   if (PathData.Path.Communicator.MyProc()==0) {  
-    for (int ptcl=0;ptcl<PathData.Path.NumParticles();ptcl++){
+    //    for (int ptcl=0;ptcl<PathData.Path.NumParticles();ptcl++){
       for (int dim=0;dim<NDIM;dim++){
-	TotalW2[dim] =TotalW2[dim]+TempDisp(ptcl)[dim]*TempDisp(ptcl)[dim];
+	//	TotalW2[dim] =TotalW2[dim]+TempDisp(ptcl)[dim]*TempDisp(ptcl)[dim];
+	TotalW2[dim] =TotalW2[dim]+TempDisp(0)[dim]*TempDisp(0)[dim];
       }
-    }
+      //    }
   }
   //  for (dim=0;dim<NDIM;dim++){
   //    allPtclDisp[dim]=(allPtclDisp[dim]*allPtclDisp[dim]);
@@ -46,9 +54,13 @@ void WindingNumberClass::WriteBlock()
       IOSection.WriteVar("Type",string("Vector"));
     }
   Array<double,1> dummy(3);
+  double beta=PathData.Path.tau*(PathData.Path.NumTimeSlices()-1);
+  double norm=(double)NumSamples*(2*PathData.Path.Species(0).lambda*
+				  beta*PathData.Path.NumParticles());
   for (int dim=0;dim<NDIM;dim++)
-    dummy(dim)=TotalW2[dim];
+    dummy(dim)=TotalW2[dim]/norm;
   WNVar.Write(dummy);
   NumSamples = 0;
+  TotalW2=0.0;
 }
 
