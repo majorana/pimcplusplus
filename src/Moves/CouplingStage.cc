4ee01c64eceac67882330e31f99d9e28879a1536
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-03-04 22:51:18 +0000

    Coupling Move changed in a number of ways

--- src/Moves/CouplingStage.cc
+++ src/Moves/CouplingStage.cc
@@ -40,51 +40,73 @@ void CouplingStageClass::ChooseTimeSlices(int &slice1,int &slice2)
 }
 
 
-double CouplingStageClass::Sample(int &slice1,int &slice2, 
-	      Array<int,1> &activeParticles)
-{
-  //slice1=0;
-  //    slice2=PathData.NumTimeSlices()-1;
-  //  activeParticles.resize(PathData.NumParticles());
-  //  for (int ptcl=0;ptcl<activeParticles.size();ptcl++)
-  //    activeParticles(ptcl)=ptcl;
-  if (PathData.Path.ExistsCoupling==1)
-    PathData.Path.ExistsCoupling=0;
-  else
-    PathData.Path.ExistsCoupling=1;
-  return 1.0;
+// double CouplingStageClass::Sample(int &slice1,int &slice2, 
+// 	      Array<int,1> &activeParticles)
+// {
+//   //slice1=0;
+//   //    slice2=PathData.NumTimeSlices()-1;
+//   //  activeParticles.resize(PathData.NumParticles());
+//   //  for (int ptcl=0;ptcl<activeParticles.size();ptcl++)
+//   //    activeParticles(ptcl)=ptcl;
+//   //  if (PathData.Path.ExistsCoupling==1)
+//   //    PathData.Path.ExistsCoupling=0;
+//   //  else
+//   //    PathData.Path.ExistsCoupling=1;
+//   //  return 1.0;
   
 
 
+//   ///For free energy
+//   slice1=0;
+//   slice2=PathData.NumTimeSlices()-1;
+//     activeParticles.resize(1);
+//     activeParticles(0)=PathData.Path.Species(1).FirstPtcl;
+//     //    cerr<<"My lambda is currently"<<PathData.Path.ExistsCoupling<<endl;
+//     //    int myProc=PathData.Path.Communicator.MyProc();
+//     int myProc=PathData.InterComm.MyProc();
+//     double lowVal=((double)(myProc))/100.0;
+//     double highVal=((double)(myProc))/100.0+1.0/100.0;
+//     //    cerr<<PathData.Path.Communicator.MyProc()<<" "<<"lowVal is "<<lowVal<<"and highVal is "<<highVal<<endl;
+//     //    //    if (abs(PathData.Path.ExistsCoupling-lowVal)<1e-5){
+//     //    //      PathData.Path.ExistsCoupling=highVal;
+// //       //      cerr<<"Trying to switch from "<<PathData.Path.ExistsCoupling<<" to "<<highVal<<endl;
+// //    }
+// //    else{
+// //      PathData.Path.ExistsCoupling=lowVal;
+// //       //      cerr<<"Trying to switch from "<<PathData.Path.ExistsCoupling<<" to "<<lowVal<<endl;
+// //    }
+//     double oldCoupling=PathData.Path.ExistsCoupling;
+//     if (PathData.Random.Local()>0.5)
+//       PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling+PathData.Path.Random.Local()*0.1;
+//     else
+//       PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling-PathData.Path.Random.Local()*0.1;
+//     if (PathData.Path.ExistsCoupling>1.0)
+//       PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling-1.0;
+//     if (PathData.Path.ExistsCoupling<0.0)
+//       PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling+1.0;
+//     //    cerr<<"My lambda is currently trying to be "<<PathData.Path.ExistsCoupling<<endl;
+//     return oldCoupling/PathData.Path.ExistsCoupling;
+//       //    return 1.0;
+// }
+
+
+
+double CouplingStageClass::Sample(int &slice1,int &slice2, 
+	      Array<int,1> &activeParticles)
+{
+  int myProc=PathData.InterComm.MyProc();
+  //  cerr<<"Inside coupling stage "<<myProc<<endl;
   ///For free energy
   slice1=0;
   slice2=PathData.NumTimeSlices()-1;
-    activeParticles.resize(1);
-    activeParticles(0)=PathData.Path.Species(1).FirstPtcl;
-    //    cerr<<"My lambda is currently"<<PathData.Path.ExistsCoupling<<endl;
-    //    int myProc=PathData.Path.Communicator.MyProc();
-    int myProc=PathData.InterComm.MyProc();
-    double lowVal=((double)(myProc))/100.0;
-    double highVal=((double)(myProc))/100.0+1.0/100.0;
-    //    cerr<<PathData.Path.Communicator.MyProc()<<" "<<"lowVal is "<<lowVal<<"and highVal is "<<highVal<<endl;
-    //    //    if (abs(PathData.Path.ExistsCoupling-lowVal)<1e-5){
-    //    //      PathData.Path.ExistsCoupling=highVal;
-//       //      cerr<<"Trying to switch from "<<PathData.Path.ExistsCoupling<<" to "<<highVal<<endl;
-//    }
-//    else{
-//      PathData.Path.ExistsCoupling=lowVal;
-//       //      cerr<<"Trying to switch from "<<PathData.Path.ExistsCoupling<<" to "<<lowVal<<endl;
-//    }
-    double oldCoupling=PathData.Path.ExistsCoupling;
-    if (PathData.Random.Local()>0.5)
-      PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling+PathData.Path.Random.Local()*0.1;
-    else
-      PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling-PathData.Path.Random.Local()*0.1;
-    if (PathData.Path.ExistsCoupling>1.0)
-      PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling-1.0;
-    if (PathData.Path.ExistsCoupling<0.0)
-      PathData.Path.ExistsCoupling=PathData.Path.ExistsCoupling+1.0;
-    //    cerr<<"My lambda is currently trying to be "<<PathData.Path.ExistsCoupling<<endl;
-    return oldCoupling/PathData.Path.ExistsCoupling;
-      //    return 1.0;
+  activeParticles.resize(1);
+  activeParticles(0)=PathData.Path.Species(1).FirstPtcl;
+  double lowVal=((double)(myProc))/100.0;
+  double highVal=((double)(myProc))/100.0+1.0/100.0;
+  if (abs(PathData.Path.ExistsCoupling-lowVal)<1e-5)
+    PathData.Path.ExistsCoupling=highVal;
+  else
+    PathData.Path.ExistsCoupling=lowVal;
+  //  cerr<<"Leaving coupling stage "<<myProc<<endl;
+  return 1.0;
 }
