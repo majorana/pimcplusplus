54a5f89d78da5a958df74b62a6f8ebfa64e89c89
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-03-04 23:24:05 +0000

    Changed initblock so it takes the slices you are working on. Required to get the Order N to work and added a switch for open loops

--- src/Moves/NoPermuteClass.cc
+++ src/Moves/NoPermuteClass.cc
@@ -12,19 +12,30 @@ bool NoPermuteStageClass::Attempt (int &slice1, int &slice2,
   // If I'm being called the first time in the move, choose a particle
   if (activeParticles(0) == -1) {
     activeParticles.resize (1);
+    //HACK! HACK! HACK! HACK!
     if (PathData.Path.OpenPaths && slice1<PathData.Path.OpenLink &&
-	PathData.Path.OpenLink<slice2){
+	PathData.Path.OpenLink<slice2 && 1==2){
       do {
 	activeParticles(0) = ChooseParticle();
       } while (activeParticles(0)==PathData.Path.OpenPtcl);
     }
     else activeParticles(0)=ChooseParticle();
-
+ 
   }
   // In any case, always accept
   return true;
 }
 
+void NoPermuteStageClass::InitBlock(int &slice1,int &slice2)
+{
+  //  cerr<<"In init block"<<endl;
+  if (PathData.Path.OrderN){
+    for (int slice=slice1;slice<=slice2;slice++)
+      PathData.Path.Cell.BinParticles(slice);
+  }
+  //  cerr<<"out of init block"<<endl;
+
+}
 int NoPermuteStageClass::ChooseParticle()
 {
   int myPtcl=(PathData.Path.Random.LocalInt 
