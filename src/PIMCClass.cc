999aca08252257b530944d11fc81df21ff27fde5
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-03-05 00:24:59 +0000

    Worm grow addition

--- src/PIMCClass.cc
+++ src/PIMCClass.cc
@@ -230,6 +230,8 @@ void PIMCClass::ReadMoves(IOSectionClass &in)
       move = new BisectionSphereBlockClass(PathData,OutFile);
     else if (moveType=="CenterDroplet")
       move = new CenterDropletClass(PathData,OutFile);
+    else if (moveType=="GrowWorm")
+      move = new WormGrowMoveClass(PathData,OutFile);
     else if (moveType=="OpenEnd")
       move = new OpenEndMoveClass(PathData,OutFile);
     else if (moveType=="RefSlice")
