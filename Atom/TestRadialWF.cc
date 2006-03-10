cddb99a508364f5ff6008abcefd39545f4f0719b
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2006-03-10 17:12:41 +0000

    Added test to ToppHopfield potential.

--- Atom/TestRadialWF.cc
+++ Atom/TestRadialWF.cc
@@ -5,10 +5,32 @@ void TestRadialWF1()
   CoulombPot pot;
   pot.Z1Z2 = -1.0;
 
-  OptimalGrid grid(1.0, 50.0);
+  OptimalGrid grid(1.0, 80.0);
   RadialWF wf;
-  wf.l = 1;
-  wf.n = 2;
+  wf.l = 0;
+  wf.n = 1;
+  wf.Energy = -0.25;
+  wf.Occupancy = 1.0;
+  wf.SetPotential (&pot);
+  wf.SetGrid (&grid);
+  
+  wf.Solve (1.0e-14);
+  fprintf (stderr, "Energy = %1.12f\n", wf.Energy);
+}
+
+void TestRadialWF2()
+{
+  ToppHopfieldPot pot;
+  pot.Z = 1.0;
+  pot.a = 1.22441639097900;
+  pot.b = -0.17904097875311;
+  pot.rc = 3.0;
+  pot.V0 = 0.179;
+
+  OptimalGrid grid(1.0, 100.0);
+  RadialWF wf;
+  wf.l = 0;
+  wf.n = 1;
   wf.Energy = -0.25;
   wf.Occupancy = 1.0;
   wf.SetPotential (&pot);
@@ -23,4 +45,5 @@ void TestRadialWF1()
 main()
 {
   TestRadialWF1();
+  TestRadialWF2();
 }
