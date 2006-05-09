4aa14c3810496b335dec4a46eb3aa4347393177d
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-05-09 16:27:08 +0000

    Added writing of move and observable names.

--- src/Observables/Time.cc
+++ src/Observables/Time.cc
@@ -18,23 +18,39 @@ void MCTimeClass::WriteBlock()
     MoveTime=0;
     ObservableTime=0;
     TotalTime=0;
-    cerr<<"My moves size is "<<MoveTime.size()<<endl;
-    cerr<<"My observable size is "<<ObservableTime.size()<<endl;
+    Array<string,1> moveNames       (Moves.size());
+    Array<string,1> observableNames (Observables.size());
+    list<MoveClass*>::iterator moveIter;
+    int i=0;
+    for (moveIter=Moves.begin();moveIter!=Moves.end();moveIter++) {
+      moveNames(i) = ((*moveIter)->Name);
+      i++;
+    }
+    i = 0;
+    list<ObservableClass*>::iterator observableIter;
+    for (observableIter=Observables.begin();
+	 observableIter!=Observables.end();observableIter++) {
+      observableNames(i) = ((*observableIter)->Name);
+      i++;
+    }
+    IOSection.WriteVar("MoveNames", moveNames);
+    IOSection.WriteVar("ObserveNames", observableNames);
   }
   TotalTime+=(double)(clock()-StartTime)/(double)CLOCKS_PER_SEC;
   StartTime=clock();
   TotalTimeVar.Write(TotalTime);
   list<MoveClass*>::iterator moveIter;
-  int counter=0;
+  int i=0;
   for (moveIter=Moves.begin();moveIter!=Moves.end();moveIter++){
-    MoveTime(counter)=((*moveIter)->TimeSpent)/TotalTime;
-    counter++;
+    MoveTime(i)=((*moveIter)->TimeSpent)/TotalTime;
+    i++;
   }
-  counter = 0;
+  i = 0;
   list<ObservableClass*>::iterator observeIter; 
-  for (observeIter=Observables.begin();observeIter!=Observables.end();observeIter++){
-    ObservableTime(counter)=((*observeIter)->TimeSpent)/TotalTime;
-    counter++;
+  for (observeIter=Observables.begin();
+       observeIter!=Observables.end();observeIter++){
+    ObservableTime(i)=((*observeIter)->TimeSpent)/TotalTime;
+    i++;
   }
   
   MoveTimeVar.Write(MoveTime);
@@ -65,11 +81,11 @@ void MCTimeClass::WriteInfo()
 //   Array<string,1> observeString;
 //   movesString.resize(Moves.size());
 //   observeString.resize(Observables.size());
-//   for (int counter=0;counter<Moves.size();counter++){
-//     movesString(counter)=Moves(counter)->Name;
+//   for (int i=0;i<Moves.size();i++){
+//     movesString(i)=Moves(i)->Name;
 //   }
-//   for (int counter=0;counter<Observables.size();counter++){
-//     observeString(counter)=Observables(counter)->Name;
+//   for (int i=0;i<Observables.size();i++){
+//     observeString(i)=Observables(i)->Name;
 //   }
   //  IOSection.WriteVar("Move Names",movesString);
   //  IOSection.WriteVar("Observable Names",observeString);
