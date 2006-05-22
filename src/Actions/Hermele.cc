855880afc9c5f1ba9e836b00620de02e9ad05dd5
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-05-22 18:31:59 +0000

    Changes in Josephson Junction

--- src/Actions/Hermele.cc
+++ src/Actions/Hermele.cc
@@ -25,42 +25,60 @@ void HermeleClass::Read(IOSectionClass& in)
 
 void HermeleClass::Phi2Omega()
 {
-  //  cerr<<"INto function"<<endl;
   int N=PathData.Path.NumTimeSlices()-1;
   for (int slice=0;slice<N;slice++){
     inArray[slice][0]=Path(slice,0)[0];
     inArray[slice][1]=0.0;
   }
-  //  cerr<<"Filled up the slice arrawy"<<endl;
   fftw_execute(p);
-  //  cerr<<"executed"<<endl;
   for (int counter=0;counter<N;counter++){
     out[counter][0]=out[counter][0]*PathData.Path.tau;
     out[counter][1]=out[counter][1]*PathData.Path.tau;
   }
-  //  cerr<<"dividing out the counter"<<endl;
-
 }
 
 double HermeleClass::ComputeFourierAction()
 {
   double T=1.0/(PathData.Path.tau*(PathData.Path.NumTimeSlices()-1));
-  double total=0.0;
-  double prefactor=(2*c*K_s*T)/(M_PI*M_PI);
   Phi2Omega();
+  double J=5.0;
+  double total=0.0;
   for (int slice=1;slice<PathData.Path.NumTimeSlices()-1;slice++){
     double omega_n=2.0*M_PI*slice*T;
-    total+=1.0/log((omega_n*omega_n+omega_c2)/(omega_n*omega_n))*
-      (out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1]);
-    //    cerr<<total<<" "<<1.0/log((omega_n*omega_n+omega_c2)/(omega_n*omega_n))<<" "
-    //	<<(out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1])<<" "<<T<<" "
-    //	<<omega_n<<" "<<((omega_n*omega_n+omega_c2)/(omega_n*omega_n))<<endl;
+    total+=T/2.0*(1.0/J+0.0*M_PI*M_PI/(4.0*c*K_s)*log((omega_n*omega_n+omega_c2)/(omega_n*omega_n)))*omega_n*omega_n*(out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1]);
   }
-  //  for (int slice=0;slice<PathData.Path.NumTimeSlices()-1;slice++){
-  //    cerr<<out[slice][0]<<" "<<out[slice][1]<<endl;
-  //  }
-  return prefactor*total;
+ return total;
 }
+
+
+
+// double HermeleClass::ComputeFourierAction()
+// {
+//   double T=1.0/(PathData.Path.tau*(PathData.Path.NumTimeSlices()-1));
+//   double total=0.0;
+//   double prefactor=(2*c*K_s*T)/(M_PI*M_PI);
+//   Phi2Omega();
+//   double J=5.0;
+//   for (int slice=1;slice<PathData.Path.NumTimeSlices()-1;slice++){
+//     double omega_n=2.0*M_PI*slice*T;
+//     //    //    //    total+=1.0/log((omega_n*omega_n+omega_c2)/(omega_n*omega_n))*
+//     //    //    //      (out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1]);
+
+    
+//     //    total+=-T/2.0*(1.0/J+0.0*M_PI*M_PI/(4.0*c*K_s)*log((omega_n*omega_n+omega_c2)/(omega_n*omega_n)))*omega_n*omega_n*(out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1]);
+//     total+=T/2.0*(1.0/J+0.0*M_PI*M_PI/(4.0*c*K_s)*log((omega_n*omega_n+omega_c2)/(omega_n*omega_n)))*omega_n*omega_n*(out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1]);
+
+//     //    cerr<<total<<" "<<1.0/log((omega_n*omega_n+omega_c2)/(omega_n*omega_n))<<" "
+//     //	<<(out[slice][0]*out[slice][0]+out[slice][1]*out[slice][1])<<" "<<T<<" "
+//     //	<<omega_n<<" "<<((omega_n*omega_n+omega_c2)/(omega_n*omega_n))<<endl;
+//   }
+//   //  for (int slice=0;slice<PathData.Path.NumTimeSlices()-1;slice++){
+//   //    cerr<<out[slice][0]<<" "<<out[slice][1]<<endl;
+//   //  }
+//   ////// return prefactor*total;
+//  return total;
+// }
+
 
 
 HermeleClass::HermeleClass(PathDataClass &pathData) : 
@@ -72,43 +90,56 @@ HermeleClass::HermeleClass(PathDataClass &pathData) :
 
 double 
 HermeleClass::SingleAction (int slice1, int slice2,
-			       const Array<int,1> &changedParticles,
-			       int level)
-{ 
-//   ifstream infile;
-//   infile.open("myPath");
-//   for (int slice=0;slice<PathData.Path.NumTimeSlices();slice++)
-//     infile>>Path(slice,0)[0];
-//   infile.close();
-//   slice1=0;
-//   slice2=PathData.Path.NumTimeSlices()-2;
-//   level=0;
-  double levelTau=Path.tau* (1<<level);
-  double totalU=0.0;
-  int skip = 1<<level;
-  PathClass &Path=PathData.Path;
-  //  cerr<<"My total U starts at "<<totalU<<endl;
-  //  double J=0.1;
-  double J=5.0;
-  for (int slice=slice1;slice<slice2;slice+=skip){
-    totalU -= (J*cos(Path(slice,0)[0])*levelTau/2.0+J*cos(Path(slice+skip,0)[0])*levelTau/2.0);
-    //    totalU -= (2.0*(Path(slice,0)[0]*Path(slice,0)[0]/2.0)
-    //	       *levelTau/2.0+
-    //	       2.0*(Path(slice+skip,0)[0]*Path(slice+skip,0)[0]/2.0)
-    //	       *levelTau/2.0);
-    //    cerr<<"ACTION: "<<slice<<" "<<slice+skip<<" "<<totalU<<" "<<Path(slice,0)[0]<<" "<<Path(slice+skip,0)[0]<<endl;
-    
-  }
+			    const Array<int,1> &changedParticles,
+			    int level)
+{
   
-  double totalPotential=ComputeFourierAction();
-  //  cerr<<"Start paths"<<endl;
-  //  for (int slice=0;slice<PathData.Path.NumTimeSlices();slice++)
-  //    cerr<<Path(slice,0)[0]<<endl;
-  //  cerr<<"End paths "<<totalU<<" "<<totalPotential<<endl;
-  //  cerr<<"My totalU is "<<totalU<<endl;
-  //  cerr<<totalU<<" "<<totalPotential<<endl;
-  return totalU+totalPotential;
+
+
 }
+
+
+// double 
+// HermeleClass::SingleAction (int slice1, int slice2,
+// 			       const Array<int,1> &changedParticles,
+// 			       int level)
+// { 
+// //   ifstream infile;
+// //   infile.open("myPath");
+// //   for (int slice=0;slice<PathData.Path.NumTimeSlices();slice++)
+// //     infile>>Path(slice,0)[0];
+// //   infile.close();
+// //   slice1=0;
+// //   slice2=PathData.Path.NumTimeSlices()-2;
+// //   level=0;
+//   double levelTau=Path.tau* (1<<level);
+//   double totalU=0.0;
+//   int skip = 1<<level;
+//   PathClass &Path=PathData.Path;
+//   //  cerr<<"My total U starts at "<<totalU<<endl;
+//   //  double J=0.1;
+//   double J=5.0;
+//   for (int slice=slice1;slice<slice2;slice+=skip){
+//     //    totalU -= (J*cos(Path(slice,0)[0])*levelTau/2.0+J*cos(Path(slice+skip,0)[0])*levelTau/2.0);
+//     totalU -= (0.1*cos(2*M_PI*Path(slice,0)[0])*levelTau/2.0+0.1*cos(2*M_PI*Path(slice+skip,0)[0])*levelTau/2.0);
+//     //    totalU -= (2.0*(Path(slice,0)[0]*Path(slice,0)[0]/2.0)
+//     //	       *levelTau/2.0+
+//     //	       2.0*(Path(slice+skip,0)[0]*Path(slice+skip,0)[0]/2.0)
+//     //	       *levelTau/2.0);
+//     //    cerr<<"ACTION: "<<slice<<" "<<slice+skip<<" "<<totalU<<" "<<Path(slice,0)[0]<<" "<<Path(slice+skip,0)[0]<<endl;
+    
+//   }
+  
+//   double totalPotential=ComputeFourierAction();
+//   //  cerr<<"Start paths"<<endl;
+//   //  for (int slice=0;slice<PathData.Path.NumTimeSlices();slice++)
+//   //    cerr<<Path(slice,0)[0]<<endl;
+//   //  cerr<<"End paths "<<totalU<<" "<<totalPotential<<endl;
+//   //  cerr<<"My totalU is "<<totalU<<endl;
+//   //  cerr<<totalU<<" "<<totalPotential<<endl;
+//   //  return totalU+totalPotential;
+//   return totalPotential;
+// }
 
 
 
