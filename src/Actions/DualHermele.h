f09616c183ac078b317f9fdfe1bec234ce2d77b9
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-05-22 20:23:56 +0000

    Ken is adding this for Bryan.

---
+++ src/Actions/DualHermele.h
@@ -0,0 +1,41 @@
+#ifndef DUAL_HERMELE_CLASS_H
+#define DUAL_HERMELE_CLASS_H
+
+#include "ActionBase.h"
+#include <Common/PairAction/PAFit.h>
+#include <fftw3.h>
+
+/// The JosephsonClass is an action class which stores the part of
+/// the potential pair action that is summed in real space.  If the
+/// potential is short range, it contains the whole potential action.
+/// This action, in general, contains off diagaonal contributions.
+class DualHermeleClass : public ActionBaseClass
+{
+protected:
+  int TotalTime;
+public:
+  double alpha;
+  void Read (IOSectionClass &in);
+  double SingleAction (int slice1, int slice2, 
+		       const Array<int,1> &activeParticles, int level);
+  double d_dBeta (int slice1, int slice2, int level);
+  double J;
+  //  fftw_complex *inArray, *out;
+  //  fftw_plan p;
+  //  void Phi2Omega();
+  //  double ComputeFourierAction();
+  double c;
+  double K_s;
+  double omega_c2;
+
+  DualHermeleClass (PathDataClass &pathData);
+  //  ~DualHermeleClass()
+  //  {
+    //    fftw_destroy_plan(p);
+    //    fftw_free(inArray);
+    //    fftw_free(out);
+  //  }
+
+};
+
+#endif
