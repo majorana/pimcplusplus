3ca84f650b941be8792a93fdda12fd13a5db585e
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-05-22 19:03:20 +0000

    Added worm moves

---
+++ src/Moves/WormSwap.h
@@ -0,0 +1,36 @@
+#ifndef WORM_SWAP_MOVE_H
+#define WORM_SWAP_MOVE_H
+
+// #include "WormStage.h"
+#include "MultiStage.h"
+#include "../PathDataClass.h"
+
+
+
+/// This is the displace move, which attempts to pick up a whole
+/// single-particle path and displace it by a random amount.
+class WormSwapMoveClass : public MultiStageClass
+{
+private:
+  /// This is the standard distribution of the displacement gaussian
+  double Sigma;
+  //  WormStageClass WormSwapStage;
+public:
+  ///Specifies how big of a chunk to change while making swap. May
+/// eventually change to be more flexible
+  int NumLevels;
+  void ChooseTimeSlices();
+  // Read the parameters from the input file
+  void Read (IOSectionClass &in);
+  // Actually attempts the move and accepts or rejects
+  //  void MakeMove();
+  WormSwapMoveClass (PathDataClass &pathData, IOSectionClass &outSection) :
+    MultiStageClass(pathData, outSection)
+    //    WormSwapStage(pathData, outSection)
+  {
+    DumpFreq = 20;
+  }
+};
+
+
+#endif
