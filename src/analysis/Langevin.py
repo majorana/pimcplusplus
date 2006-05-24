a2a3e323025f78010de724a426f3f9e38aca63e9
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-05-24 21:11:00 +0000

    Now plots band energies in detailed doc.

--- src/analysis/Langevin.py
+++ src/analysis/Langevin.py
@@ -7,6 +7,7 @@ import stats
 
 def ProcessLangevin (infiles, summaryDoc, detailedDoc, StartCut, beta):
     V = infiles.ReadVar("V")[0]
+    bands = infiles.ReadVar("BandEnergies")
     numPtcls = V.shape[1]
     KE = 0.5*numarray.sum(numarray.sum(V*V, 2),1)
     numSteps = len(KE)
@@ -22,6 +23,16 @@ def ProcessLangevin (infiles, summaryDoc, detailedDoc, StartCut, beta):
     img = Image(imageName)
     summaryDoc.append(Heading(2, "Langevin dynamics"))
     summaryDoc.append(img)
+
+#    for i in range(0,len(bands)):
+#        bands[i] = numarray.transpose(bands[i])
+    baseName = "BandEnergies"
+    bandPage = BuildScalarTracePage(bands, baseName, "BandEnergies", StartCut)
+    myFrame=IFrame()
+    myFrame.src=bandPage
+    myFrame.width="100%"
+    myFrame.height="375"
+    detailedDoc.append(myFrame)
     
     
     
