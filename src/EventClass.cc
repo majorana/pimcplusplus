32445d3c118f6619700e1a1499f0e022d7411815
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-06-24 19:22:18 +0000

    Adding forgotten file.

---
+++ src/EventClass.cc
@@ -0,0 +1,9 @@
+#include "PathDataClass.h"
+#include "EventClass.h"
+
+EventClass::EventClass(PathDataClass &pathData, IOSectionClass& out) : 
+  PathData(pathData), IOSection(out), TimeSpent(0.0), TimesCalled(0),
+  Path(pathData.Path)
+{
+    // do nothing else for now
+}
