b1ea1c3b2b67f828c877d258ed15c9f76be1a49a
esler <esler@94938c44-ffe3-0310-b2d4-86f7a523709d>
2006-12-13 02:27:41 +0000

    Added POV export of planes.

---
+++ src/Visual/WFExport.cc
@@ -0,0 +1,312 @@
+#include "WFExport.h"
+#include "WFVis.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <revel.h>
+#include <Common/Splines/CubicSpline.h>
+
+using namespace IO;
+
+WFExportClass::WFExportClass(WFVisualClass &visual) :
+    Visual(visual), Width(2000), Height(2000),
+    BaseNameChooser("Export filename", Gtk::FILE_CHOOSER_ACTION_SAVE),
+    WidthLabel("Width:"), HeightLabel("Height:")
+{
+  BaseNameFrame.set_label("Base filename");
+  BaseNameFrame.add (BaseNameHBox);
+  BaseNameHBox.pack_start(BaseNameEntry, Gtk::PACK_SHRINK, 5);
+  BaseNameHBox.pack_start(BaseNameBrowseButton, Gtk::PACK_SHRINK);
+  BaseNameBrowseButton.set_label("Browse");
+  MainVBox.pack_start (BaseNameFrame, Gtk::PACK_SHRINK, 5);
+  
+  SizeFrame.set_label ("Size");
+  SizeFrame.add (SizeBox);
+  WidthButton.set_range(1.0, 3000.0);
+  HeightButton.set_range(1.0, 3000.0);
+  WidthButton.set_increments (10.0, 100.0);
+  HeightButton.set_increments (10.0, 100.0);
+  WidthButton.set_value (700.0);
+  HeightButton.set_value (700.0);
+  WidthButton.set_digits(0);
+  HeightButton.set_digits(0);
+  WidthBox.pack_start (WidthLabel, Gtk::PACK_SHRINK,5);
+  WidthBox.pack_start (WidthButton, Gtk::PACK_SHRINK);
+  HeightBox.pack_start (HeightLabel, Gtk::PACK_SHRINK,5);
+  HeightBox.pack_start (HeightButton, Gtk::PACK_SHRINK);
+  WidthHeightBox.pack_start (WidthBox, Gtk::PACK_SHRINK, 5);
+  WidthHeightBox.pack_start (HeightBox, Gtk::PACK_SHRINK, 5);
+  SizeBox.pack_start (WidthHeightBox, Gtk::PACK_SHRINK);
+  SizeBox.pack_start (RatioButton,    Gtk::PACK_SHRINK);
+  RatioButton.set_label ("Fixed ratio");
+  
+  TypeFrame.set_label("Rendering Type");
+  TypeFrame.add(TypeBox);
+  TypeBox.pack_start(TypeCombo, Gtk::PACK_SHRINK);
+  TypeCombo.insert_text(0, "Offscreen GL");
+  TypeCombo.insert_text(1, "POVray");
+  TypeCombo.set_active (0);
+  ExportButton.set_label ("Export");
+  CancelButton.set_label ("Cancel");
+  
+  StillMovieFrame.set_label("Still/Movie");
+  StillMovieFrame.add (StillMovieBox);
+  StillMovieBox.pack_start (StillButton, Gtk::PACK_SHRINK, 5);
+  StillMovieBox.pack_start (MovieButton, Gtk::PACK_SHRINK, 5);
+  Gtk::RadioButtonGroup group = StillButton.get_group();
+  MovieButton.set_group (group);
+  StillButton.set_label("Current Frame (png)");
+  MovieButton.set_label("Multi-frame movie (MPEG4)");
+  
+  MovieParamFrame.set_label("Movie parameters");
+  MovieParamFrame.add (MovieParamBox);
+  MovieParamBox.pack_start (FirstLastBox, Gtk::PACK_SHRINK, 5);
+  MovieParamBox.pack_start (InterpBox, Gtk::PACK_SHRINK, 5);
+  FirstFrameLabel.set_text ("First frame:");
+  FirstFrameBox.pack_start (FirstFrameLabel,  Gtk::PACK_SHRINK, 5);
+  FirstFrameBox.pack_start (FirstFrameButton, Gtk::PACK_SHRINK, 5);
+  LastFrameLabel.set_text  ("Last frame:");
+  LastFrameBox.pack_start  (LastFrameLabel,   Gtk::PACK_SHRINK, 5);
+  LastFrameBox.pack_start  (LastFrameButton,  Gtk::PACK_SHRINK, 5);
+  FirstLastBox.pack_start (FirstFrameBox, Gtk::PACK_EXPAND_PADDING);
+  FirstLastBox.pack_start (LastFrameBox, Gtk::PACK_EXPAND_PADDING);
+  InterpFactorLabel.set_text ("Interpolation factor:");
+  InterpFactorBox.pack_start (InterpFactorLabel,  Gtk::PACK_SHRINK, 5);
+  InterpFactorBox.pack_start (InterpFactorButton, Gtk::PACK_SHRINK, 5);
+  InterpBox.pack_start (InterpFactorBox, Gtk::PACK_EXPAND_PADDING);
+  
+  MovieParamFrame.set_sensitive(false);
+  
+  ButtonBox.pack_start(ExportButton,    Gtk::PACK_SHRINK, 5);
+  ButtonBox.pack_start(CancelButton,    Gtk::PACK_SHRINK, 5);
+  MainVBox.pack_start (SizeFrame,       Gtk::PACK_SHRINK, 5);
+  MainVBox.pack_start (TypeFrame,       Gtk::PACK_SHRINK, 5);
+  MainVBox.pack_start (StillMovieFrame, Gtk::PACK_SHRINK, 5);
+  MainVBox.pack_start (MovieParamFrame, Gtk::PACK_SHRINK, 5);
+  MainVBox.pack_start (ButtonBox,       Gtk::PACK_SHRINK, 10);
+  add (MainVBox);
+  set_title ("Export");
+  // Set signal handlers
+  ExportButton.signal_clicked().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnExportButton));
+  CancelButton.signal_clicked().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnCancelButton));
+  BaseNameBrowseButton.signal_clicked().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnBrowseButton));
+  RatioButton.signal_toggled().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnRatioToggle));
+  WidthButton.signal_value_changed().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnWidthAdjust));
+  HeightButton.signal_value_changed().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnHeightAdjust));
+  StillButton.signal_toggled().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnStillMovie));
+  BaseNameChooser.signal_selection_changed().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnChooserChange));
+  BaseNameEntry.signal_activate().connect
+    (sigc::mem_fun(*this, &WFExportClass::OnEntryChange));
+}
+
+
+void WFExportClass::SetupWidgets()
+{
+//   FirstFrameButton.set_digits(0);
+//   FirstFrameButton.set_range(1.0, Visual.PathArray.extent(0)+1);
+//   FirstFrameButton.set_increments (1.0, 10.0);
+//   LastFrameButton.set_digits(0);
+//   LastFrameButton.set_range (1.0, Visual.PathArray.extent(0)+1);
+//   LastFrameButton.set_increments (1.0, 10.0);
+//   InterpFactorButton.set_range(1.0, 500.0);
+//   InterpFactorButton.set_digits(0);
+//   InterpFactorButton.set_increments (1.0, 10.0);
+}  
+
+void WFExportClass::InitGLStuff()
+{
+//   glShadeModel(GL_SMOOTH);
+//   glEnable (GL_LIGHTING);
+//   glEnable (GL_LINE_SMOOTH);
+//   glEnable (GL_POLYGON_SMOOTH);
+
+  static GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+  static GLfloat light_ambient[] = {0.2, 0.2, 0.2, 1.0};
+  static GLfloat light_specular[]= {1.0, 1.0, 1.0, 1.0};
+  static GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_DEPTH_TEST);
+  glLightModeli(GL_LIGHT_MODEL_COLOR_CONTROL, GL_SEPARATE_SPECULAR_COLOR);
+  glEnable (GL_MULTISAMPLE);
+  glEnable (GL_COLOR_MATERIAL);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  
+
+  glViewport(0, 0, Width, Height);
+  
+}
+
+
+void WFExportClass::MakePixmap()
+{
+  GdkPixmap.clear();
+  GLConfig.clear();
+
+  GLConfig = Gdk::GL::Config::create (Gdk::GL::MODE_RGB   |
+				      Gdk::GL::MODE_DEPTH |
+				      Gdk::GL::MODE_SINGLE);
+  if (!GLConfig) {
+    cerr << "Cannot find a visual capable of OpenGL.\n";
+    return;
+  }
+
+  GdkPixmap = Gdk::Pixmap::create (Visual.get_window(),
+				   Width, Height, GLConfig->get_depth());
+  
+  Glib::RefPtr<Gdk::GL::Pixmap> GLPixmap = 
+    Gdk::GL::ext(GdkPixmap).set_gl_capability (GLConfig);
+
+  GLContext = Gdk::GL::Context::create (GLPixmap, false);
+
+  GLPixmap->make_current(GLContext);
+  GLPixmap->gl_begin(GLContext);
+
+  // Render here
+  InitGLStuff();
+  Visual.DrawFrame (true);
+  Visual.PathVis.GLRender();
+
+  glFlush();
+  GLPixmap->gl_end();
+  GLPixmap->wait_gl();
+}
+
+void WFExportClass::Export (string filename)
+{
+//   GdkPixmap.clear();
+//   GLConfig.clear();
+
+//   GLConfig = Gdk::GL::Config::create (Gdk::GL::MODE_RGB   |
+// 				      Gdk::GL::MODE_DEPTH |
+// 				      Gdk::GL::MODE_SINGLE);
+//   if (!GLConfig) {
+//     cerr << "Cannot find a visual capable of OpenGL.\n";
+//     return;
+//   }
+
+//   GdkPixmap = Gdk::Pixmap::create (Visual.get_window(),
+// 				   Width, Height, GLConfig->get_depth());
+  
+//   Glib::RefPtr<Gdk::GL::Pixmap> GLPixmap = 
+//     Gdk::GL::ext(GdkPixmap).set_gl_capability (GLConfig);
+
+//   GLContext = Gdk::GL::Context::create (GLPixmap, false);
+
+//   GLPixmap->make_current(GLContext);
+//   GLPixmap->gl_begin(GLContext);
+
+//   // Render here
+//   InitGLStuff();
+//   Visual.MakeFrame ((int)floor(Visual.FrameAdjust.get_value()));
+//   Visual.PathVis.GLRender();
+
+//   glFlush();
+//   GLPixmap->gl_end();
+//   GLPixmap->wait_gl();
+
+  MakePixmap();
+  Glib::RefPtr<Gdk::Drawable> drawable =  GdkPixmap;
+  Glib::RefPtr<Gdk::Drawable> drawable2 =  GdkPixmap;
+
+  cerr << "drawable = " << drawable << endl;
+
+  Glib::RefPtr<Gdk::Pixbuf> Pbuf = Gdk::Pixbuf::create
+    (drawable, GdkPixmap->get_colormap(), 0, 0, 0, 0, Width, Height);
+
+  if (drawable == drawable2)
+    cerr << "no change\n";
+			
+  Pbuf->save(filename, Glib::ustring("png"));
+}
+
+
+
+void
+WFExportClass::ExportPOV(string basename)
+{
+  string filename = basename;
+  filename.append(".pov");
+  Visual.PathVis.POVRender (filename);
+}
+
+
+
+void
+WFExportClass::OnExportButton()
+{
+  Width  = (int)round(WidthButton.get_value());
+  Height = (int)round(HeightButton.get_value());
+  //  string basename = BaseNameChooser.get_filename();
+  string basename = BaseNameEntry.get_text();
+  if (TypeCombo.get_active_text() == "POVray") 
+    ExportPOV(basename);
+  else {
+    basename.append(".png");
+    Export(basename);
+  }
+}
+
+void
+WFExportClass::OnCancelButton()
+{
+  hide();
+}
+
+void
+WFExportClass::OnBrowseButton()
+{
+  BaseNameChooser.run();
+}
+
+void 
+WFExportClass::OnHeightAdjust()
+{
+  if (RatioButton.get_active()) 
+    WidthButton.set_value(HeightButton.get_value()*Ratio);
+}
+
+void 
+WFExportClass::OnWidthAdjust()
+{
+  if (RatioButton.get_active()) 
+    HeightButton.set_value(WidthButton.get_value()/Ratio);
+}
+
+void 
+WFExportClass::OnRatioToggle()
+{
+  if (RatioButton.get_active())
+    Ratio = WidthButton.get_value()/HeightButton.get_value();
+}
+
+void
+WFExportClass::OnStillMovie()
+{
+  bool do_movie = !StillButton.get_active();
+  MovieParamFrame.set_sensitive(do_movie);
+}
+
+void
+WFExportClass::OnChooserChange()
+{
+  BaseNameEntry.set_text(BaseNameChooser.get_filename());
+}
+
+void
+WFExportClass::OnEntryChange()
+{
+  BaseNameChooser.select_filename(BaseNameEntry.get_text());
+}
