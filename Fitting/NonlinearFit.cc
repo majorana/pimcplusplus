a5994a9cd9fda9ae576874f4328707632b239faa
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-02-01 21:34:50 +0000

    Works now.

--- Fitting/NonlinearFit.cc
+++ Fitting/NonlinearFit.cc
@@ -1,4 +1,5 @@
 #include "NonlinearFit.h"
+#include "../MatrixOps/MatrixOps.h"
 
 template<int M, typename ModelType> double
 NonlinearFitClass<M,ModelType>::Chi2(const Array<double,1> &x, 
@@ -9,6 +10,7 @@ NonlinearFitClass<M,ModelType>::Chi2(const Array<double,1> &x,
   int N = x.size();
   assert (y.size()     == N);
   assert (sigma.size() == N);
+  Model.SetParams(params);
 
   double chi2 = 0;
   for (int i=0; i<N; i++) {
@@ -24,6 +26,7 @@ NonlinearFitClass<M,ModelType>::CalcAlphaBeta (const Array<double,1> &x,
 					       const Array<double,1> &sigma,
 					       TinyVector<double,M> params)
 {
+  Model.SetParams(params);
   int N = x.size();
   assert (y.size()     == N);
   assert (sigma.size() == N);
@@ -51,6 +54,40 @@ NonlinearFitClass<M,ModelType>::CalcAlphaBeta (const Array<double,1> &x,
 
 
 template<int M, typename ModelType> void
+NonlinearFitClass<M,ModelType>::Solve()
+{
+  for (int i=0; i<M; i++) {
+    dParams[i] = 0.0;
+    for (int j=0; j<M; j++)
+      AlphaInv(i,j) = Alpha(i,j);
+  }
+
+  GJInverse(AlphaInv);
+
+  for (int i=0; i<M; i++)
+    for (int j=0; j<M; j++)
+      dParams[i] += AlphaInv(i,j)*Beta(j);
+
+  // Do simple Gaussian elimination
+//   double dInv = 1.0/Alpha(0,0);
+//   for (int col=0; col<M; col++)
+//     Alpha(0,col) *= dInv;
+//   Beta(0) *= dInv;
+//   for (int row=1; row<M; row++) {
+//     for (int col=0; col<row; col++) {
+//       Beta(row) -= Alpha(row,col)*Beta(col);
+//       for (int k=0;
+//     double diag = Alpha(row,row);
+//     double diagInv = 1.0/diag;
+//     for (int col=0; col<M; col++)
+//     Beta(row) *= diagInv;
+//     for (int col=row+1; col<
+
+
+}
+
+
+template<int M, typename ModelType> void
 NonlinearFitClass<M,ModelType>::Fit (const Array<double,1> &x,
 				     const Array<double,1> &y,
 				     const Array<double,1> &sigma,
@@ -64,8 +101,20 @@ NonlinearFitClass<M,ModelType>::Fit (const Array<double,1> &x,
     CalcAlphaBeta (x, y, sigma, params);
     for (int i=0; i<M; i++)
       Alpha(i,i) *= (1.0+lambda);
-    
+    Solve();
+    done = true;
+    for (int i=0; i<M; i++)
+      if ((dParams[i]/params[i]) > 1.0e-5)
+	done = false;
+    if (!done) {
+      double chiNew = Chi2 (x, y, sigma, params + dParams);
+      if (chiNew < chiNow) {
+	Lambda *= 5.0;
+	params = params + dParams;
+      }
+      else {
+	Lambda *= 0.2;
+      }
+    }
   }
-
-
 }
--- Fitting/NonlinearFit.h
+++ Fitting/NonlinearFit.h
@@ -1,8 +1,10 @@
 #ifndef NONLINEAR_FIT_H
 #define NONLINEAR_FIT_H
 
+#include "../Blitz.h"
 #include <blitz/tinyvec.h>
 #include <blitz/array.h>
+#include "../MatrixOps/MatrixOps.h"
 
 using namespace blitz;
 
@@ -12,14 +14,17 @@ class NonlinearFitClass
 {
 private:
   ModelType &Model;
-  TinyVector<double,M> Params, Beta;
+  TinyVector<double,M> Params, Beta, dParams;
   TinyMatrix<double,M,M> Alpha;
+  Array<double,2> AlphaInv;
   double Chi2 (const Array<double,1> &x, const Array<double,1> &y,
 	       const Array<double,1> &y, TinyVector<double,M> params);
   void CalcAlphaBeta(const Array<double,1> &x, 
 		     const Array<double,1> &y,
 		     const Array<double,1> &sigma,
 		     TinyVector<double,M> params);
+  void Solve ();
+
 public:
   void Fit(const Array<double,1> &x, const Array<double,1> &y,
 	   const Array<double,1> &sigma, TinyVector<double,M> &params);
@@ -27,8 +32,136 @@ public:
   NonlinearFitClass(ModelType model) :
     Model(model)
   {
-    
+    AlphaInv.resize(M,M);
   };
 };
+
+
+template<int M, typename ModelType> double
+NonlinearFitClass<M,ModelType>::Chi2(const Array<double,1> &x, 
+				     const Array<double,1> &y,
+				     const Array<double,1> &sigma,
+				     TinyVector<double,M> params)
+{
+  int N = x.size();
+  assert (y.size()     == N);
+  assert (sigma.size() == N);
+  Model.SetParams(params);
+
+  double chi2 = 0;
+  for (int i=0; i<N; i++) {
+    double val = Model (x(i));
+    chi2 += (val-y(i))*(val-y(i))/(sigma(i)*sigma(i));
+  }
+  return chi2;
+}
+
+template<int M, typename ModelType> void
+NonlinearFitClass<M,ModelType>::CalcAlphaBeta (const Array<double,1> &x, 
+					       const Array<double,1> &y,
+					       const Array<double,1> &sigma,
+					       TinyVector<double,M> params)
+{
+  Model.SetParams(params);
+  int N = x.size();
+  assert (y.size()     == N);
+  assert (sigma.size() == N);
+  
+  for (int k=0; k<M; k++) {
+    Beta[k] = 0.0;
+    for (int l=0; l<M; l++)
+      Alpha(k,l) = 0.0;
+  }
+
+  for (int i=0; i<N; i++) {
+    double val = Model(x(i));
+    TinyVector<double,M> grad = Model.Grad(x(i));
+    for (int k=0; k<M; k++)
+      Beta[k] += grad[k]*(y(i)-val)/(sigma(i)*sigma(i));
+  }
+  for (int i=0; i<N; i++) {
+    double val = Model(x(i));
+    TinyVector<double,M> grad = Model.Grad(x(i));
+    for (int k=0; k<M; k++)
+      for (int l=0; l<M; l++)
+	Alpha(k,l) += grad[k]*grad[l]/(sigma(i)*sigma(i));
+  }
+  cerr << "Alpha = " << Alpha << endl;
+  cerr << "Beta  = " << Beta  << endl;
+}
+
+
+template<int M, typename ModelType> void
+NonlinearFitClass<M,ModelType>::Solve()
+{
+  for (int i=0; i<M; i++) {
+    dParams[i] = 0.0;
+    for (int j=0; j<M; j++)
+      AlphaInv(i,j) = Alpha(i,j);
+  }
+
+  GJInverse(AlphaInv);
+  TinyMatrix<double,M,M> id;
+  id = 0.0;
+  for (int i=0; i<M; i++)
+    for (int j=0; j<M; j++)
+      for (int k=0; k<M; k++) 
+	id(i,j) += AlphaInv(i,k)*Alpha(k,j);
+
+  for (int i=0; i<M; i++)
+    for (int j=0; j<M; j++)
+      dParams[i] += AlphaInv(i,j)*Beta(j);
+  //  cerr << "dParams = " << dParams << endl;
+
+  // Do simple Gaussian elimination
+//   double dInv = 1.0/Alpha(0,0);
+//   for (int col=0; col<M; col++)
+//     Alpha(0,col) *= dInv;
+//   Beta(0) *= dInv;
+//   for (int row=1; row<M; row++) {
+//     for (int col=0; col<row; col++) {
+//       Beta(row) -= Alpha(row,col)*Beta(col);
+//       for (int k=0;
+//     double diag = Alpha(row,row);
+//     double diagInv = 1.0/diag;
+//     for (int col=0; col<M; col++)
+//     Beta(row) *= diagInv;
+//     for (int col=row+1; col<
+
+
+}
+
+
+template<int M, typename ModelType> void
+NonlinearFitClass<M,ModelType>::Fit (const Array<double,1> &x,
+				     const Array<double,1> &y,
+				     const Array<double,1> &sigma,
+				     TinyVector<double,M> &params)
+{
+  double chiNow = Chi2 (x, y, sigma, params);
+  double lambda = 0.0001;
+  
+  bool done = false;
+  while (!done) {
+    CalcAlphaBeta (x, y, sigma, params);
+    for (int i=0; i<M; i++)
+      Alpha(i,i) *= (1.0+lambda);
+    Solve();
+    done = true;
+    for (int i=0; i<M; i++)
+      if ((dParams[i]/params[i]) > 1.0e-5)
+	done = false;
+    if (!done) {
+      double chiNew = Chi2 (x, y, sigma, params + dParams);
+      if (chiNew < chiNow) {
+	lambda *= 0.2;
+	params = params + dParams;
+      }
+      else {
+	lambda *= 5.0;
+      }
+    }
+  }
+}
 
 #endif
