bd112a81726b9964d3f70eebed243e3d3cd9a630
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-03-15 18:39:10 +0000

    Computation of first and second derivatives are correct, now.

--- Splines/TestBspline.c
+++ Splines/TestBspline.c
@@ -10,14 +10,14 @@ Test_1d_s()
   grid.num = 11;
   float data[] = { 3.0, -4.0, 2.0, 1.0, -2.0, 0.0, 3.0, 2.0, 0.5, 1.0, 3.0 };
   BCtype_s bc;
-  bc.lCode = DERIV1; bc.lVal = 10.0;
-  bc.rCode = DERIV1; bc.rVal = -10.0;
+  bc.lCode = DERIV2; bc.lVal = 10.0;
+  bc.rCode = DERIV2; bc.rVal = -10.0;
   
   UBspline_1d_s *spline = (UBspline_1d_s*) create_UBspline_1d_s (grid, bc, data);
   for (double x=1.0; x<=3.00001; x+=0.001) {
-    float val;
-    eval_UBspline_1d_s (spline, x, &val);
-    fprintf (stdout, "%1.5f %20.14f\n", x, val);
+    float val, grad, lapl;
+    eval_UBspline_1d_s_vgl (spline, x, &val, &grad, &lapl);
+    fprintf (stdout, "%1.5f %20.14f %20.14f %20.14f\n", x, val, grad, lapl);
   }
 
 }
