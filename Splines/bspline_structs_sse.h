4955d1bac8e3aaf089d76eb46900c3cc795b021a
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-03-16 21:30:07 +0000

    Adding missing files.

---
+++ Splines/bspline_structs_sse.h
@@ -0,0 +1,74 @@
+#ifndef BSPLINE_STRUCTS_SSE_H
+#define BSPLINE_STRUCTS_SSE_H
+
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <pmmintrin.h>
+
+///////////////////////////
+// Single precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  float* restrict coefs;
+  int coefs_size;
+  Ugrid x_grid;
+  BCtype_s xBC;
+  __m128 tx;
+} UBspline_1d_s;
+
+typedef struct
+{
+  spline_code code;
+  float *coefs;
+  int x_stride;
+  Ugrid coefs_size, x_grid, y_grid;
+  BCtype_s xBC, yBC;
+  __m128 tx, ty;
+} UBspline_2d_s;
+
+typedef struct
+{
+  spline_code code;
+  float *coefs;
+  int coefs_size, x_stride, y_stride;
+  Ugrid x_grid, y_grid, z_grid;
+  BCtype_s xBC, yBC, zBC;
+  __m128 _tpx, _tpy, _tpz;
+  __m128 _delta_inv;
+} UBspline_3d_s;
+
+
+///////////////////////////
+// Double precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size;
+  Ugrid x_grid;
+  BCtype_d xBC;
+} UBspline_1d_d;
+
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size, x_stride;
+  Ugrid x_grid, y_grid;
+  BCtype_d xBC, yBC;
+} UBspline_2d_d;
+
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size, x_stride, y_stride;
+  Ugrid x_grid, y_grid, z_grid;
+  BCtype_d xBC, yBC, zBC;
+} UBspline_3d_d;
+
+
+#endif
---
+++ Splines/bspline_structs_std.h
@@ -0,0 +1,71 @@
+#ifndef BSPLINE_STRUCTS_STD_H
+#define BSPLINE_STRUCTS_STD_H
+
+///////////////////////////
+// Single precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  float* restrict coefs;
+  int coefs_size;
+  Ugrid x_grid;
+  BCtype_s xBC;
+  float tx[4];
+} UBspline_1d_s;
+
+typedef struct
+{
+  spline_code code;
+  float *coefs;
+  int coefs_size, x_stride;
+  Ugrid x_grid, y_grid;
+  BCtype_s xBC, yBC;
+  float tx[4], ty[4];
+} UBspline_2d_s;
+
+typedef struct
+{
+  spline_code code;
+  float *coefs;
+  int coefs_size, x_stride, y_stride;
+  Ugrid x_grid, y_grid, z_grid;
+  BCtype_s xBC, yBC, zBC;
+  float tx[4], ty[4], tz[4];
+  int csize;
+
+} UBspline_3d_s;
+
+
+///////////////////////////
+// Double precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size;
+  Ugrid x_grid;
+  BCtype_d xBC;
+} UBspline_1d_d;
+
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size, x_stride;
+  Ugrid x_grid, y_grid;
+  BCtype_d xBC, yBC;
+} UBspline_2d_d;
+
+typedef struct
+{
+  spline_code code;
+  double *coefs;
+  int coefs_size, x_stride, y_stride;
+  Ugrid x_grid, y_grid, zgrid;
+  BCtype_d xBC, yBC, zBC;
+} UBspline_3d_d;
+
+
+#endif
