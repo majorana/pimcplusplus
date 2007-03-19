c3a3282e62b5a3b51f8b6199b34b7e13412caca2
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-03-19 18:28:58 +0000

    Change trailing _f in name to be _s, to be consistent with LAPACK naming convensions.

--- Splines/bspline.h
+++ Splines/bspline.h
@@ -64,10 +64,10 @@ typedef struct
 ////////////////////////////////////////////////////////////
 #ifdef __SSE3__
 #include "bspline_structs_sse.h"
-#include "bspline_eval_sse_f.h"
+#include "bspline_eval_sse_s.h"
 #else
 #include "bspline_structs_std.h"
-#include "bspline_eval_std_f.h"
+#include "bspline_eval_std_s.h"
 #endif
 
 
