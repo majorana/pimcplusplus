97e3f4652a5f33069a680dd17c83bd091fce1245
bclark <bclark@94938c44-ffe3-0310-b2d4-86f7a523709d>
2007-05-04 14:19:32 +0000

    Changed shiftmoveclass to do random shifts because I am testing a move that requires this to ensure detailed balance

--- src/Moves/MetaMoves.cc
+++ src/Moves/MetaMoves.cc
@@ -44,20 +44,21 @@ void ShiftMoveClass::MakeMove()
   PathData.Path.SliceRange(PathData.Path.Communicator.NumProcs()-1,
 			   slice1, slice2);
 
-  /// Shift between 7/16 and 1/2 of the maximum slices
-  int maxSlices = (slice2-slice1)>>1;
-  int minSlices = (7*(slice2-slice1))>>4;
-  if ((maxSlices-minSlices)<5)
-    minSlices = max(0, maxSlices - 5);
-  
-  //  int numTimeSlicesToShift =
-  //  PathData.Path.Random.CommonInt(maxSlices);
-  // There is no point in shifting more than maxSlices/2
-  // if (numTimeSlicesToShift > (maxSlices>>1))
-  //   numTimeSlicesToShift -= (maxSlices>>1);
+//   /// Shift between 7/16 and 1/2 of the maximum slices
+//   int maxSlices = (slice2-slice1)>>1;
+//   int minSlices = (7*(slice2-slice1))>>4;
+//   if ((maxSlices-minSlices)<5)
+//     minSlices = max(0, maxSlices - 5);
 
-  int numTimeSlicesToShift = 
-    minSlices + PathData.Path.Random.CommonInt(maxSlices-minSlices+1);
+//   int numTimeSlicesToShift = 
+//     minSlices + PathData.Path.Random.CommonInt(maxSlices-minSlices+1);
+  
+  int numTimeSlicesToShift =
+    PathData.Path.Random.CommonInt(maxSlices);
+//   //  There is no point in shifting more than maxSlices/2
+//   if (numTimeSlicesToShift > (maxSlices>>1))
+//     numTimeSlicesToShift -= (maxSlices>>1);
+
 
   PathData.MoveJoin(0);
   PathData.ShiftData(numTimeSlicesToShift);
