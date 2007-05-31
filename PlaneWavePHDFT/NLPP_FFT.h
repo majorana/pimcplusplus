0ddc2f4cadf8e94f070148b43bd96859b30ac0e3
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-05-31 14:57:12 +0000

    Starting to add code to implement NLPPs.

---
+++ PlaneWavePHDFT/NLPP_FFT.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////
+// Copyright (C) 2003-2006 Bryan Clark and Kenneth Esler   //
+//                                                         //
+// This program is free software; you can redistribute it  //
+// and/or modify it under the terms of the GNU General     //
+// Public License as published by the Free Software        //
+// Foundation; either version 2 of the License, or         //
+// (at your option) any later version.  This program is    //
+// distributed in the hope that it will be useful, but     //
+// WITHOUT ANY WARRANTY; without even the implied warranty //
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. //  
+// See the GNU General Public License for more details.    //
+// For more information, please see the PIMC++ Home Page:  //
+//           http://pathintegrals.info                     //
+/////////////////////////////////////////////////////////////
+
+#ifndef NLPP_FFT_H
+#define NLPP_FFT_H
+
+#include "HamiltonianBase.h"
+
+// Implements the King-Smith method for applying nonlocal
+// pseudopotentials in real space.  See PRB 44, 13063.
+
+class NLPP_FFTClass : public VionBase
+{
+private:
+  kSpacePH LocalPot_k;
+  zVec Vc;
+  FFTBox      &cFFT;
+  void SetupkPotentials();
+  void SetuprPotentials();
+public:
+  void Setup();
+  void SetIons (const Array<Vec3, 1>& rions);
+  void Vmatrix (Array<complex<double>,2> &vmat);
+  void Setk(Vec3 k);
+
+  void Apply (const zVec &c, zVec &Hc);
+  void Apply (const zVec &c, zVec &Hc,
+	      Array<double,3> &VHXC);
+  NLPP_FFTClass (NLPPClass &nlpp, GVecsClass &gvecs, FFTBox &fft) :
+    VionBase (gvecs), kPH(ph), cFFT(fft)
+  {
+
+  }
+};
