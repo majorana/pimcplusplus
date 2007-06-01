024e8b7edba9f02f30a3c70c116605e3b2ddd1e1
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-06-01 20:28:27 +0000

    Verified that the local part is working correctly for the NLPP_FFT.

---
+++ PlaneWavePHDFT/NLPP_FFT.cc
@@ -0,0 +1,173 @@
+/////////////////////////////////////////////////////////////
+// Copyright (C) 2003-2006 Bryan Clark and Kenneth Esler   //
+//                                                         //
+// This program is free software; you can redistribute it  //
+// and/or modify it under the terms of the GNU General     //
+// Public License as published by the Free Software        //
+// Foundation; either version 2 of the License, or         //
+// (at your option) any later version.  This program is    //
+// distributed in the hope that it will be useful, but     //
+// WITHOUT ANY WARRANTY; without even the implied warranty //
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. //  
+// See the GNU General Public License for more details.    //
+// For more information, please see the PIMC++ Home Page:  //
+//           http://pathintegrals.info                     //
+/////////////////////////////////////////////////////////////
+
+#include "NLPP_FFT.h"
+
+void
+NLPP_FFTClass::Vmatrix (Array<complex<double>,2> &vmat)
+{
+  cerr << "NLPP_FFTClass::Vmatrix is not implemented.\n";
+  abort();
+}
+
+
+void 
+NLPP_FFTClass::SetupkPotentials()
+{
+  // First, create Vlocal from the local potential of the NLPP
+  Vlocal.Spline = NLPP.GetLocalSpline();
+  Vouter.Z1Z2   = -NLPP.GetValenceCharge();
+  Vlocal.Vouter = &Vouter;
+
+  // Now, compute the tail coefficients of the local potential
+  kPH.CalcTailCoefs (30.0, 60.0);
+
+  // Compute local potential in reciprocal space
+  double volInv = 1.0/GVecs.GetBoxVol();
+  // Setup V and F tensors in k-space
+  //  double gMag, lastMag2;
+  double gMag, lastMag2;
+  lastMag2 = -1.0;
+  double a, bPerp, bPar, V;
+  int numCalls = 0;
+  for (int i=0; i<GVecs.DeltaSize(); i++) {
+    double gMag2 = dot(GVecs.DeltaG(i), GVecs.DeltaG(i));
+    if (fabs(gMag2-lastMag2) > 1.0e-12) {
+      lastMag2 = gMag2;
+      gMag  = sqrt(lastMag2);
+      kPH.GetVals(gMag, a, bPerp, bPar, V);
+      numCalls++;
+    }
+    if (gMag2 > 1.0e-10)
+      VG(i) = volInv * V;
+    else {
+      VG(i) = 0.0;
+      VG0 = volInv * V;
+    }
+  }
+}
+
+void
+NLPP_FFTClass::SetuprPotentials()
+{
+  cFFT.kBox = complex<FFT_FLOAT>(0.0, 0.0);
+  for (int i=0; i<GVecs.DeltaSize(); i++) {
+    Int3 I = GVecs.DeltaI(i);
+    cFFT.kBox(I)   = StructureFactor(i)*VG(i);
+  }
+  cFFT.k2r();
+  Vr = cFFT.rBox;
+}
+
+void
+NLPP_FFTClass::SetIons(const Array<Vec3,1> &rions)
+{
+  // Calculate the structure factor
+  VionBase::SetIons(rions);
+  if (IsSetup)
+    SetuprPotentials();
+}
+
+
+
+void
+NLPP_FFTClass::Setup()
+{
+  int nx, ny, nz;
+  cFFT.GetDims(nx,ny,nz);
+  Vr.resize(nx,ny,nz);
+  Vc.resize(GVecs.size());
+  VG.resize(GVecs.DeltaSize());
+  SetupkPotentials();
+  SetuprPotentials();
+
+  // Compute the Kleinmain-Bylander projectors:
+
+
+  IsSetup = true;
+}
+
+void
+NLPP_FFTClass::Setk (Vec3 k)
+{
+  kPoint = k;
+  
+  int nx, ny, nz;
+  cFFT.GetDims(nx,ny,nz);
+  Vr.resize(nx,ny,nz);
+  Vc.resize(GVecs.size());
+  VG.resize(GVecs.DeltaSize());
+
+  SetupkPotentials();
+  SetIons(Rions);
+}
+
+
+void 
+NLPP_FFTClass::Apply (const zVec &c, zVec &Hc)
+{
+  if (!IsSetup)
+    Setup();
+  int nx, ny, nz;
+  cFFT.GetDims(nx, ny, nz);
+
+  ////////////////////
+  // Potential part //
+  ////////////////////
+  // Transform c into real space
+  cFFT.PutkVec (c);
+  cFFT.k2r();
+  // Multiply by V
+  cFFT.rBox *= Vr;
+  // Transform back
+  cFFT.r2k();
+
+  // Get vector
+  cFFT.GetkVec (Vc);
+  for (int i=0; i<GVecs.size(); i++)
+    Hc(i) += Vc(i);
+}
+
+void 
+NLPP_FFTClass::Apply (const zVec &c, zVec &Hc,
+			 Array<double,3> &VHXC)
+{
+  if (!IsSetup)
+    Setup();
+  int nx, ny, nz;
+  cFFT.GetDims(nx, ny, nz);
+
+  ////////////////////
+  // Potential part //
+  ////////////////////
+  // Transform c into real space
+  cFFT.PutkVec (c);
+  cFFT.k2r();
+  // Multiply by V
+  //  cFFT.rBox *= (Vr+VHXC);
+  for (int ix=0; ix<nx; ix++)
+    for (int iy=0; iy<ny; iy++)
+      for (int iz=0; iz<nz; iz++)
+	cFFT.rBox(ix,iy,iz) *= (Vr(ix,iy,iz)+VHXC(ix,iy,iz));
+
+  // Transform back
+  cFFT.r2k();
+
+  // Get vector
+  cFFT.GetkVec (Vc);
+  for (int i=0; i<GVecs.size(); i++)
+    Hc(i) += Vc(i);
+}
