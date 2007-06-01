49a1eea62a5cbff553a8f6bc61898df367622bcc
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-06-01 03:02:33 +0000

    Forgot to add this file hours ago.

---
+++ PH/PlotNLPP.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+from pylab import *
+
+zeta_r = load ('zeta_r.dat');
+zeta_q = load ('zeta_q.dat');
+chi_r  = load ('chi_r.dat');
+chi_q  = load ('chi_q.dat');
+
+plot (zeta_r[:,0], zeta_r[:,1], 'b',\
+      chi_r[:,0], chi_r[:,1], 'b--',\
+      zeta_r[:,0], zeta_r[:,2], 'r',\
+      chi_r[:,0], chi_r[:,2], 'r--')
+
+figure(2);
+plot (zeta_q[:,0], zeta_q[:,1], 'b',\
+      chi_q[:,0], chi_q[:,1], 'b--',\
+      zeta_q[:,0], zeta_q[:,2], 'r',\
+      chi_q[:,0], chi_q[:,2], 'r--')
+
+show()
