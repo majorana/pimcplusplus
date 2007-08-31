b5486b44fb23d203dea9ecbbde40410dbf013bf1
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2007-08-31 18:19:49 +0000

    Added functions to GVecsClass.

--- PlaneWavePHDFT/GVecs.cc
+++ PlaneWavePHDFT/GVecs.cc
@@ -422,3 +422,188 @@ GVecsClass::Set (Mat3 &lattice, Array<Vec3,1> &gvecs, double fftFactor)
 //   }
     
 }
+
+
+
+void
+GVecsClass::Set (Mat3 &lattice, Array<Vec3,1> &gvecs, Int3 boxSize)
+{
+  Lattice = lattice;
+  Vec3 a[3], b[3];
+  a[0] = lattice(0,0), lattice(0,1), lattice(0,2);
+  a[1] = lattice(1,0), lattice(1,1), lattice(1,2);
+  a[2] = lattice(2,0), lattice(2,1), lattice(2,2);
+  double det_a = dot(cross(a[0],a[1]),a[2]);
+  double detInv = 1.0/det_a;
+  double vol = fabs(det_a);
+  double volInv = 1.0/vol;
+  b[0] = 2.0*M_PI*detInv * cross(a[1], a[2]);  
+  b[1] = 2.0*M_PI*detInv * cross(a[2], a[0]);
+  b[2] = 2.0*M_PI*detInv * cross(a[0], a[1]);
+  LatticeInv(0,0) =  (Lattice(1,1)*Lattice(2,2) - Lattice(2,1)*Lattice(1,2));
+  LatticeInv(1,0) = -(Lattice(1,0)*Lattice(2,2) - Lattice(1,2)*Lattice(2,0));
+  LatticeInv(2,0) =  (Lattice(1,0)*Lattice(2,1) - Lattice(1,1)*Lattice(2,0));
+  LatticeInv(0,1) = -(Lattice(0,1)*Lattice(2,2) - Lattice(0,2)*Lattice(2,1));
+  LatticeInv(1,1) =  (Lattice(0,0)*Lattice(2,2) - Lattice(0,2)*Lattice(2,0));
+  LatticeInv(2,1) = -(Lattice(0,0)*Lattice(2,1) - Lattice(0,1)*Lattice(2,0));
+  LatticeInv(0,2) =  (Lattice(0,1)*Lattice(1,2) - Lattice(0,2)*Lattice(1,1));
+  LatticeInv(1,2) = -(Lattice(0,0)*Lattice(1,2) - Lattice(0,2)*Lattice(1,0));
+  LatticeInv(2,2) =  (Lattice(0,0)*Lattice(1,1) - Lattice(0,1)*Lattice(1,0));
+  LatticeInv   = (1.0/det_a) * LatticeInv;
+  RecipLattice = 2.0*M_PI * LatticeInv;
+  /// Make sure we've computed the inverse lattice properly
+  Mat3 ident = LatticeInv * Lattice;
+
+  for (int i=0; i<3; i++)
+    for (int j=0; j<3; j++) 
+      if (i==j) assert (fabs(ident(i,j)-1.0) < 1.0e-12);
+      else      assert (fabs(ident(i,j))      < 1.0e-12);
+
+  GVecs.resize(gvecs.size());
+  Indices.resize(gvecs.size());
+  Multipliers.resize(gvecs.size());
+  GVecs = gvecs;
+  
+  for (int i=0; i<GVecs.size(); i++) 
+    Indices(i) = GetIndex(a, b, GVecs(i));
+  Multipliers = Indices;
+  
+  Nx = boxSize[0];
+  Ny = boxSize[1];
+  Nz = boxSize[2];
+
+  Int3 maxIndex(0,0,0), minIndex(0,0,0);
+  double maxG2 = 0.0;
+  for (int i=0; i<Indices.size(); i++) {
+    for (int j=0; j<3; j++) {
+      if (Indices(i)[j] < minIndex[j])
+	minIndex[j] = Indices(i)[j];
+      if (Indices(i)[j] > maxIndex[j])
+	maxIndex[j] = Indices(i)[j];
+    }
+    if (dot (GVecs(i), GVecs(i)) > maxG2)
+      maxG2 = dot (GVecs(i), GVecs(i));
+  }
+
+  /// Adjust indices for FFT box
+  for (int i=0; i<Indices.size(); i++) {
+    Indices(i)[0] = (Indices(i)[0]+Nx)%Nx;
+    Indices(i)[1] = (Indices(i)[1]+Ny)%Ny;
+    Indices(i)[2] = (Indices(i)[2]+Nz)%Nz;
+  }
+    
+
+
+  kCut = sqrt (maxG2);
+  Int3 iDiff;
+  Vec3 gDiff;
+  int numDiff = 0;
+  // Construct difference vectors
+  for (int ix=2*minIndex[0]; ix<=2*maxIndex[0]; ix++) {
+    iDiff[0] = (ix+Nx)%Nx;
+    for (int iy=2*minIndex[1]; iy<=2*maxIndex[1]; iy++) {
+      iDiff[1] = (iy+Ny)%Ny;
+      for (int iz=2*minIndex[2]; iz<=2*maxIndex[2]; iz++) {
+	iDiff[2] = (iz+Nz)%Nz;
+	gDiff   = (double)ix*b[0]+(double)iy*b[1]+(double)iz*b[2];
+	if (dot (gDiff, gDiff) <= 4.0000001*maxG2)
+	  numDiff++;
+      }
+    }
+  }
+  GDiff.resize(numDiff);
+  GDiffInv2.resize(numDiff);
+  IDiff.resize(numDiff);
+
+  // Now actually store
+  numDiff = 0;
+  for (int ix=2*minIndex[0]; ix<=2*maxIndex[0]; ix++) {
+    iDiff[0] = (ix+Nx)%Nx;
+    for (int iy=2*minIndex[1]; iy<=2*maxIndex[1]; iy++) {
+      iDiff[1] = (iy+Ny)%Ny;
+      for (int iz=2*minIndex[2]; iz<=2*maxIndex[2]; iz++) {
+	iDiff[2] = (iz+Nz)%Nz;
+	gDiff   = (double)ix*b[0]+(double)iy*b[1]+(double)iz*b[2];
+	if (dot (gDiff, gDiff) <= maxG2) {
+	  GDiff(numDiff) = gDiff;
+	  IDiff(numDiff) = iDiff;
+	  GDiffInv2(numDiff) = 1.0/dot(gDiff, gDiff);
+	  numDiff++;
+	}
+      }
+    }
+  }    
+}
+
+
+Int3
+GVecsClass::GetFFTBoxSize (Mat3 &lattice, Array<Vec3,1> &gvecs, 
+			   double fftFactor)
+{
+  Mat3 latticeInv;
+  Vec3 a[3], b[3];
+  a[0] = lattice(0,0), lattice(0,1), lattice(0,2);
+  a[1] = lattice(1,0), lattice(1,1), lattice(1,2);
+  a[2] = lattice(2,0), lattice(2,1), lattice(2,2);
+  double det_a = dot(cross(a[0],a[1]),a[2]);
+  double detInv = 1.0/det_a;
+  double vol = fabs(det_a);
+  double volInv = 1.0/vol;
+  b[0] = 2.0*M_PI*detInv * cross(a[1], a[2]);  
+  b[1] = 2.0*M_PI*detInv * cross(a[2], a[0]);
+  b[2] = 2.0*M_PI*detInv * cross(a[0], a[1]);
+  latticeInv(0,0) =  (lattice(1,1)*lattice(2,2) - lattice(2,1)*lattice(1,2));
+  latticeInv(1,0) = -(lattice(1,0)*lattice(2,2) - lattice(1,2)*lattice(2,0));
+  latticeInv(2,0) =  (lattice(1,0)*lattice(2,1) - lattice(1,1)*lattice(2,0));
+  latticeInv(0,1) = -(lattice(0,1)*lattice(2,2) - lattice(0,2)*lattice(2,1));
+  latticeInv(1,1) =  (lattice(0,0)*lattice(2,2) - lattice(0,2)*lattice(2,0));
+  latticeInv(2,1) = -(lattice(0,0)*lattice(2,1) - lattice(0,1)*lattice(2,0));
+  latticeInv(0,2) =  (lattice(0,1)*lattice(1,2) - lattice(0,2)*lattice(1,1));
+  latticeInv(1,2) = -(lattice(0,0)*lattice(1,2) - lattice(0,2)*lattice(1,0));
+  latticeInv(2,2) =  (lattice(0,0)*lattice(1,1) - lattice(0,1)*lattice(1,0));
+  latticeInv   = (1.0/det_a) * latticeInv;
+  /// Make sure we've computed the inverse lattice properly
+  Mat3 ident = latticeInv * lattice;
+
+  for (int i=0; i<3; i++)
+    for (int j=0; j<3; j++) 
+      if (i==j) assert (fabs(ident(i,j)-1.0) < 1.0e-12);
+      else      assert (fabs(ident(i,j))      < 1.0e-12);
+
+  Array<Vec3,1> indices;
+  for (int i=0; i<GVecs.size(); i++) 
+    indices(i) = GetIndex(a, b, GVecs(i));
+
+  /// Find FFT box size;
+  Int3 maxIndex(0,0,0), minIndex(0,0,0);
+  double maxG2 = 0.0;
+  for (int i=0; i<indices.size(); i++) {
+    for (int j=0; j<3; j++) {
+      if (indices(i)[j] < minIndex[j])
+	minIndex[j] = indices(i)[j];
+      if (indices(i)[j] > maxIndex[j])
+	maxIndex[j] = indices(i)[j];
+    }
+    if (dot (GVecs(i), GVecs(i)) > maxG2)
+      maxG2 = dot (GVecs(i), GVecs(i));
+  }
+  kCut = sqrt (maxG2);
+//   cerr << "minIndex = " << minIndex << endl;
+//   cerr << "maxIndex = " << maxIndex << endl;
+//   cerr << "maxG2 = " << maxG2 << endl;
+  int nx = 2*(maxIndex[0] - minIndex[0] + 1);
+  int ny = 2*(maxIndex[1] - minIndex[1] + 1);
+  int nz = 2*(maxIndex[2] - minIndex[2] + 1);
+
+  // This is to increase the resolution of the FFT box, for spline
+  // representation 
+  nx = (int)ceil(fftFactor * nx);
+  ny = (int)ceil(fftFactor * ny);
+  nz = (int)ceil(fftFactor * nz);
+
+  if ((nx%2)==1) nx++;
+  if ((ny%2)==1) ny++;
+  if ((nz%2)==1) nz++;
+
+  return Int3 (nx, ny, nz);
+}
