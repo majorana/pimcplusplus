8575791cb8ac75193af62d32294d188a5fb6cc0d
esler <esler@09d36d52-ffe3-0310-9f69-ed4471c772d2>
2008-05-20 19:20:03 +0000

    Added more functions to Birch fit.

--- Fitting/BirchEOS.h
+++ Fitting/BirchEOS.h
@@ -21,7 +21,11 @@ public:
   inline double PressureFD(double V);
   inline void SetParams (TinyVector<double,N+2> params);
   inline TinyVector<double,N+2> GetParams ();
+  inline double GetB0();
+  inline double GetB(double V);
+  inline double GetB0FD();
   inline double GetB0p();
+  inline double GetB0pFD();
   inline double K_T (double V);
 
   BirchEOSClass() : third(1.0/3.0), Atomic2GPa(29421.01)
@@ -57,7 +61,7 @@ inline double
 BirchEOSClass<N>::operator()(double V)
 {
   double x = cbrt(V/V0);
-  double t = x*x-1;
+  double t = x*x-1.0;
   double t_np2 = t*t;
   double E = E0;
   for (int n=0; n<N; n++) {
@@ -104,8 +108,54 @@ BirchEOSClass<N>::PressureFD(double V)
 
 template<int N>
 inline double
+BirchEOSClass<N>::GetB0()
+{
+  return Atomic2GPa*8.0*bn[0]/(9.0*V0);
+}
+
+template<int N>
+inline double
+BirchEOSClass<N>::GetB(double V)
+{
+  double x = cbrt(V/V0);
+  double t = x*x - 1.0;
+  double prefact = 2.0/(9.0*V)*(t + 1.0);
+  double sum = 0.0;
+  double t_n = 1.0;
+  for (int n=0; n<(N-1); n++) {
+    sum += (double)(n+2)*((double)(2*n+2) + (double)(2*n+7)*t)*bn[n]*t_n;
+    t_n *= t;
+  }
+  return Atomic2GPa*prefact*sum;
+}
+
+template<int N>
+inline double
+BirchEOSClass<N>::GetB0FD()
+{
+  double eps = 1.0e-7;
+  double Pplus = Pressure(V0+eps);
+  double Pminus = Pressure(V0-eps);
+  return -V0 * (Pplus-Pminus)/(2.0*eps);
+}
+
+template<int N>
+inline double
 BirchEOSClass<N>::GetB0p()
 {
+  return 4.0 + (16.0*bn[1])/(9.0*V0*GetB0());
+}
+
+template<int N>
+inline double
+BirchEOSClass<N>::GetB0pFD()
+{
+  double eps = 1.0e-7;
+  double Bplus = GetB(V0+eps);
+  double Bminus = GetB(V-eps);
+  double dB_dV = (Bplus - Bminus) / (2.0*eps);
+  double B0 = GetB(V0);
+  return (-(V/B0*dB_dV);
 }
 
 
@@ -115,18 +165,20 @@ BirchEOSClass<N>::Grad (double V)
 {
   TinyVector<double,N+2> grad;
   grad[1] = 1.0;
-  // Find grad[1]
   double x = cbrt(V/V0);
   double t = x*x - 1.0;
+
+  // Find grad[0]
+  grad[0] = 0.0;
   double dt_dV0 = -2.0/3.0*x*x/V0;
   double t_np1 = t;
-  grad[0] = 0.0;
   for (int n=0; n<N; n++) {
     grad[0] += bn[n] * (double)(n+2) * t_np1;
     t_np1 *= t;
   }
   grad[0] *= dt_dV0;
   
+  // Find the rest
   double t_np2 = t*t;
   for (int n=0; n<N; n++) {
     grad[n+2] = t_np2;
--- Fitting/TestBirch.cc
+++ Fitting/TestBirch.cc
@@ -2,9 +2,9 @@
 
 int main()
 {
-  BirchEOSClass<2> birch;
-  TinyVector<double, 4> params(-50.0, 40.0, 3.0, 2.1);
-  TinyVector<double, 4> grad, gradFD;
+  BirchEOSClass<4> birch;
+  TinyVector<double, 6> params(-50.0, 40.0, 3.0, 2000.1, 5, 0.8);
+  TinyVector<double, 6> grad, gradFD;
 
 
   birch.SetParams(params);
@@ -18,7 +18,20 @@ int main()
   grad = birch.Grad(41.0);
   gradFD = birch.GradFD(41.0);
   cerr << "grad   = " << grad << endl;
-  cerr << "gradFD = " << gradFD << endl;
+  cerr << "gradFD = " << gradFD << endl << endl;
 
+  double B0 = birch.GetB0();
+  double B  = birch.GetB(params[0]);
+  double B0FD= birch.GetB0FD();
+
+  cerr << "B0(V0) = " << B << endl;
+  cerr << "B0     = " << B0   << endl;
+  cerr << "B0FD   = " << B0FD << endl << endl;
+
+  double B0p = birch.GetB0p();
+  double B0pFD = birch.GetB0p();
+  
+  cerr << "B0p   = " << B0p << endl;
+  cerr << "B0pFD = " << B0p << endl;
 
 }
