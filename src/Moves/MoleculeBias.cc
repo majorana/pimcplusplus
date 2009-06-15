c770752f823e1d60c0286c0d9529c906ee7a7b91
gergely <gergely@94938c44-ffe3-0310-b2d4-86f7a523709d>
2009-06-15 21:55:14 +0000

    updates to force bias

--- src/Moves/MoleculeBias.cc
+++ src/Moves/MoleculeBias.cc
@@ -87,15 +87,20 @@ double MoleculeForceBiasMove::Sample(int &slice1,int &slice2, Array<int,1> &acti
 	  for(int activeMolIndex=0; activeMolIndex<MoveList.size(); activeMolIndex++){
       // need to generate move
       // Generate a random unit vector to specify point of reflection
-      dVec A;
-      for (int x = 0; x < 3; x++)
-        A(x) = (PathData.Path.Random.Local()-0.5);
-      A = Normalize(A);
+      dVec axis;
+      double x = 2 * (PathData.Path.Random.Local()-0.5);
+      double A = sqrt(1 - x*x);
+      double y = 2 * A * (PathData.Path.Random.Local()-0.5);
+      if((PathData.Path.Random.Local()-0.5)<0)
+        y *= -1;
+      double z = sqrt(1 - x*x - y*y);
+      if((PathData.Path.Random.Local()-0.5)<0)
+        z *= -1;
+      //assert(abs(x*x + y*y + z*z - 1) < 1e-5);
+      axis(0) = x; axis(1) = y; axis(2) = z;
+      //axis = Normalize(axis);
       // Generate an angle and a displacement vector
       double theta = 2*(PathData.Path.Random.Local()-0.5)*Theta;
-      double x = 2*(PathData.Path.Random.Local()-0.5)*Step;
-      double y = 2*(PathData.Path.Random.Local()-0.5)*Step;
-      double z = 2*(PathData.Path.Random.Local()-0.5)*Step;
       //double V = step*step*step*4*M_PI*angle_step*2*M_PI; // need probability that a move is generated = 1/V
       sumF(0) = 0;
       sumF(1) = 0;
@@ -111,20 +116,23 @@ double MoleculeForceBiasMove::Sample(int &slice1,int &slice2, Array<int,1> &acti
         coord -= O;
         dVec F = MolAction->Force(slice,ptcl); // calculate force
         sumF += F;
-        dVec lever = CalcLever(A,coord);
+        dVec lever = CalcLever(axis,coord);
         sumN += cross(lever,F);
       }   
 
+      double dx = 2*(PathData.Path.Random.Local()-0.5)*Step;
+      double dy = 2*(PathData.Path.Random.Local()-0.5)*Step;
+      double dz = 2*(PathData.Path.Random.Local()-0.5)*Step;
       dVec dR;
-      dR(0) = x;
-      dR(1) = y;
-      dR(2) = z;
+      dR(0) = dx;
+      dR(1) = dy;
+      dR(2) = dz;
 
       double Pprop = 1.0;
       if(doTrans)
-        Pprop *= exp(Tau*dot(sumF,dR));
+        Pprop *= exp(Tau*dot(sumF,dR)/sqrt(dot(sumF,sumF)*dot(dR,dR)));
       if(doRot)
-        Pprop *= exp(Tau*dot(sumN,A)*theta);
+        Pprop *= exp(Tau*dot(sumN,axis)*theta/sqrt(dot(sumN,sumN)));
 
       double T = Pprop;  // Transition probability
       if (Pprop > 1)
@@ -141,7 +149,7 @@ double MoleculeForceBiasMove::Sample(int &slice1,int &slice2, Array<int,1> &acti
         }
         // do rotation
         if(doRot){  
-          RotateMol(slice,PathData.Mol.MembersOf(activeMol), A, theta);
+          RotateMol(slice,PathData.Mol.MembersOf(activeMol), axis, theta);
         }
 
         dVec Orev = PathData.Path(slice,activeMol);
@@ -158,14 +166,14 @@ double MoleculeForceBiasMove::Sample(int &slice1,int &slice2, Array<int,1> &acti
           coord -= Orev;
           dVec F = MolAction->Force(slice,ptcl); // calculate force
           sumFrev += F;
-          dVec lever = CalcLever(A,coord);
+          dVec lever = CalcLever(axis,coord);
           sumNrev += cross(lever,F);
         }   
         double PpropRev = 1.0;
         if(doTrans)
-          PpropRev *= exp(Tau*dot(sumFrev,(-1*dR)));
+          PpropRev *= exp(Tau*dot(sumFrev,(-1*dR))/sqrt(dot(sumFrev,sumFrev)*dot(dR,dR)));
         if(doRot)
-          PpropRev *= exp(Tau*dot(sumNrev,A)*(-theta));  //Reverse Transition probability
+          PpropRev *= exp(Tau*dot(sumNrev,axis)*(-theta)/sqrt(dot(sumNrev,sumNrev)));  //Reverse Transition probability
         double Trev = PpropRev;
         if (PpropRev > 1)
           Trev = 1.0;
